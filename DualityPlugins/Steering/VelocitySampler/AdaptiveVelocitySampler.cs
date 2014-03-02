0993c3a10c20c867cac5ab5ef79277e4eade324b
Adam <mail@fetzenet.de>
2014-03-02 13:24:18 +0100

    Steering Merge #CHANGE: Split VelocitySampler into multiple files. #CHANGE: Applied coding guidelines.

---
+++ DualityPlugins/Steering/VelocitySampler/AdaptiveVelocitySampler.cs
@@ -0,0 +1,57 @@
+﻿using Duality;
+using OpenTK;
+using System;
+using System.Collections.Generic;
+
+namespace Duality.Plugins.Steering
+{
+	/// <summary>
+	/// Samples velocities based on the velocity the agent chose. The sampling
+	/// density is higher velocities close to the last best velocity.
+	/// This reduces samples needed massively compared to <see cref="BruteForceVelocitySampler"/>
+	/// but can potentially lead to undesired behavior.
+	/// </summary>
+	[Serializable]
+	public class AdaptiveVelocitySampler : IVelocitySampler
+	{
+		private int layerCount				= 5;
+		private int outerLayerSampleCount	= 11;
+
+		[NonSerialized] private int currentSampleIdx = 0;
+
+		public void Reset()
+		{
+			this.currentSampleIdx = 0;
+		}
+		public Vector2 GetCurrentSample(Agent agent)
+		{
+			int commonSampleCount = this.layerCount * this.outerLayerSampleCount;
+			Vector2 oldVelocity = agent.BestVel / agent.Characteristics.MaxSpeed;
+			
+			if (this.currentSampleIdx >= commonSampleCount + 1)
+				return Vector2.Zero;
+			if (this.currentSampleIdx >= commonSampleCount)
+				return oldVelocity;
+
+			int layerIdx = this.currentSampleIdx % this.layerCount;
+			int directionIdx = (this.currentSampleIdx / this.layerCount) % this.outerLayerSampleCount;
+
+
+			float undistortedSpeedFactor = (float)(layerIdx + 1) / this.layerCount;
+			float undistortedAngle = MathF.Lerp(-1.0f, 1.0f, ((float)directionIdx / this.outerLayerSampleCount));
+
+			float speedFactor = undistortedSpeedFactor;
+			float angle = MathF.Atan2(oldVelocity.Y, oldVelocity.X) + MathF.Pow(Math.Abs(undistortedAngle), 0.8f) * undistortedAngle * MathF.RadAngle180;
+				
+			return new Vector2(MathF.Cos(angle) * speedFactor, MathF.Sin(angle) * speedFactor);
+		}
+		public bool SetCurrentCost(float cost)
+		{
+			this.currentSampleIdx++;
+			if (this.currentSampleIdx <= this.layerCount * this.outerLayerSampleCount)
+				return true;
+			else
+				return false;
+		}
+	}
+}
