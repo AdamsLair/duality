d587699dea2b3979fbeea65228aae1f07d5c16d0
fish <physhh@gmail.com>
2014-03-03 02:24:53 +0100

    Characteristics documentation

--- DualityPlugins/Steering/Characteristics/AdvancedAgentCharacteristics.cs
+++ DualityPlugins/Steering/Characteristics/AdvancedAgentCharacteristics.cs
@@ -4,6 +4,12 @@ using System;
 
 namespace Duality.Plugins.Steering
 {
+	/// <summary>
+	/// Implementation of <see cref="IAgentCharacteristics"/> which offers parameters
+	/// which are closely related to the algorithms used. Those might be hard to 
+	/// configure if one doens't have background knownledge about the implementation. 
+	/// In that case you should use <see cref="DefaultAgentCharacteristics"/>.
+	/// </summary>
 	[Serializable]
 	public class AdvancedAgentCharacteristics : IAgentCharacteristics
 	{
@@ -20,39 +26,58 @@ namespace Duality.Plugins.Steering
 		private float toiFactor = DEFAULT_TOI_FACTOR;
 		private float toiExponent = DEFAULT_TOI_EXPONENT;
 
+		/// <summary>
+		/// [GET/SET] Factor between 0 and 1 which defines how much velocity-consistency should be weighted
+		/// </summary>
 		public float VelocityPreservationFactor
 		{
 			get { return this.velocityPreservationFactor; }
 			set { this.velocityPreservationFactor = value; }
 		}
+		/// <summary>
+		/// [GET/SET] Factor between 0 and 1 which defines how much the difference from the optimal direction should be weighted
+		/// </summary>
 		public float DirectionFactor
 		{
 			get { return this.directionFactor; }
 			set { this.directionFactor = value; }
 		}
+		/// <summary>
+		/// [GET/SET] Factor between 0 and 1 which defines how much the difference from the optimal speed should be weighted
+		/// </summary>
 		public float SpeedFactor
 		{
 			get { return this.speedFactor; }
 			set { this.speedFactor = value; }
 		}
+		/// <summary>
+		/// [GET/SET] Factor between 0 and 1 which defines how much the time of impact with other obstacles should be weighted
+		/// </summary>
 		public float ToiFactor
 		{
 			get { return this.toiFactor; }
 			set { this.toiFactor = value; }
 		}
+		/// <summary>
+		/// [GET/SET] Value between 0 and infinity which defines the exponent applied to the TOI-penality. Can be used to
+		/// make the agent avoid other obstacles earlier/later.
+		/// </summary>
 		public float ToiExponent
 		{
 			get { return this.toiExponent; }
 			set { this.toiExponent = value; }
 		}
-		public float MaxSpeed
-		{
-			get { return this.prefSpeed; }
-		}
+		/// <summary>
+		/// [GET/SET] The speed which the agent should use if possible.
+		/// </summary>
 		public float PreferredSpeed
 		{
 			get { return this.prefSpeed; }
 			set { this.prefSpeed = value; }
+		}
+		public float MaxSpeed
+		{
+			get { return this.prefSpeed; }
 		}
 
 		public float CalculateVelocityCost(Agent agent, Vector2 sampleVelocity, float toiPenality)
--- DualityPlugins/Steering/Characteristics/DefaultAgentCharacteristics.cs
+++ DualityPlugins/Steering/Characteristics/DefaultAgentCharacteristics.cs
@@ -7,6 +7,12 @@ using OpenTK;
 
 namespace Duality.Plugins.Steering
 {
+	/// <summary>
+	/// Implementation of <see cref="IAgentCharacteristics"/> which offers parameters
+	/// which are intuitve for end users. The purpose of this implementation is to 
+	/// hide implementation details as much as possible. If you need more control you can
+	/// either use <see cref="AdvancedAgentCharacteristics"/> or use a custom implementation.
+	/// </summary>
 	[Serializable]
 	public class DefaultAgentCharacteristics : IAgentCharacteristics
 	{
@@ -50,8 +56,7 @@ namespace Duality.Plugins.Steering
 				// ... and use it as toi exponent. This will lead to a toi-cost-function which is very steep close to one 
 				// => the agent will avoid other agents in the last moment
 				// => if aggressiveness is 0 the opposit happens - the agent avoids other agents very early
-				// the function we use to calculate is basically: c * log(1 - x) / log(0.5) | where x is the aggressiveness-factor
-				this.baseImpl.ToiExponent = AdvancedAgentCharacteristics.DEFAULT_TOI_EXPONENT * MathF.Log(1.0f - MathF.Lerp(0.0001f, 0.9999f, this.aggressiveness)) / MathF.Log(0.5f);
+				this.baseImpl.ToiExponent = AdvancedAgentCharacteristics.DEFAULT_TOI_EXPONENT * (1f / (1f - MathF.Lerp(0.01f, 0.99f, this.aggressiveness)) - 1f);
 
 				this.updateBaseImplParams = false;
 			}
