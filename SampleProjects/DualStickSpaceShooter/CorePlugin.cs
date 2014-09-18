9509c924a120a4eb2b71b44a4e3a5a9210197dcb
Adam <mail@fetzenet.de>
2014-09-18 22:41:31 +0200

    Set up Example Game #ADD: Added DualStickSpaceShooter example game project for issue 117

---
+++ SampleProjects/DualStickSpaceShooter/CorePlugin.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Duality;
+
+namespace DualStickSpaceShooter
+{
+    public class DualStickSpaceShooterCorePlugin : CorePlugin {}
+}
