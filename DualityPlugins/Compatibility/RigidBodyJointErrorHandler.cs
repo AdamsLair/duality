a8358bfd964c36b2ed685dfb832d6763ec49ede4
Adam <mail@fetzenet.de>
2014-10-31 18:59:50 +0100

    Bugfixes, Compatibility #FIX: Fixed usability bugs in the editors PropertyGrid. #FIX: Fixed broken backwards compatibility in RigidBody joints for old data.

---
+++ DualityPlugins/Compatibility/RigidBodyJointErrorHandler.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+using Duality;
+using Duality.Components.Physics;
+using Duality.Serialization;
+
+namespace Duality.Plugins.Compatibility
+{
+	public class RigidBodyJointErrorHandler : SerializeErrorHandler
+	{
+		public override void HandleError(SerializeError error)
+		{
+			AssignFieldError fieldError = error as AssignFieldError;
+			if (fieldError != null && fieldError.TargetObject is JointInfo)
+			{
+				RigidBody target = fieldError.FieldValue as RigidBody;
+				JointInfo joint = fieldError.TargetObject as JointInfo;
+
+				bool other = false;
+				FieldInfo field = null;
+				if (fieldError.FieldName == "colA")
+				{
+					field = fieldError.TargetObjectType.Fields.FirstOrDefault(f => f.Name == "parentBody");
+				}
+				else if (fieldError.FieldName == "colB")
+				{
+					field = fieldError.TargetObjectType.Fields.FirstOrDefault(f => f.Name == "otherBody");
+					other = true;
+				}
+
+				if (field != null)
+				{
+					if (other)
+					{
+						FieldInfo jointsField = typeof(RigidBody).GetField("joints", BindingFlags.Instance | BindingFlags.NonPublic);
+						IList<JointInfo> jointList = jointsField.GetValue(target) as IList<JointInfo>;
+						jointList.Remove(joint);
+					}
+
+					field.SetValue(fieldError.TargetObject, fieldError.FieldValue);
+					fieldError.AssignSuccess = true;
+				}
+			}
+
+			return;
+		}
+	}
+}
