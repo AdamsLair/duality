c4aff412ca8597854a5a195aaed1d167436eda51
Adam <mail@fetzenet.de>
2014-11-02 22:30:04 +0100

    Example Project #CHANGE: Loading all content initially to prevent on-demand-loading and stutter.

--- SampleProjects/DualStickSpaceShooter/CorePlugin.cs
+++ SampleProjects/DualStickSpaceShooter/CorePlugin.cs
@@ -7,5 +7,30 @@ using Duality;
 
 namespace DualStickSpaceShooter
 {
-    public class DualStickSpaceShooterCorePlugin : CorePlugin {}
+    public class DualStickSpaceShooterCorePlugin : CorePlugin
+	{
+		private bool contentLoaded = false;
+		protected override void OnBeforeUpdate()
+		{
+			base.OnBeforeUpdate();
+
+			// Load all available content so we don't need on-demand loading during runtime.
+			// It's probably not a good idea for content-rich games, consider having a per-level
+			// loading screen instead, or something similar.
+			if (!this.contentLoaded)
+			{
+				Log.Game.Write("Loading game content...");
+				Log.Game.PushIndent();
+				{
+					List<ContentRef<Resource>> availableContent = ContentProvider.GetAvailableContent<Resource>();
+					foreach (ContentRef<Resource> resourceReference in availableContent)
+					{
+						resourceReference.MakeAvailable();
+					}
+				}
+				Log.Game.PopIndent();
+				this.contentLoaded = true;
+			}
+		}
+	}
 }
