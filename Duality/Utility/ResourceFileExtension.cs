e18aa0b0b84969d5528167359c5480e553394a5d
darthnihil <rbrunet09@yahoo.ca>
2014-11-24 12:42:56 -0500

    Creation of ResourceFileExtension.cs
    The creation of the ResourceFileExtension class as a static class allows
    the global variables inside to be called from anywhere in the solution
    due to the fact that it doesn’t need any instantiation.

---
+++ Duality/Utility/ResourceFileExtension.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Duality.Utility
+{
+    public static class ResourceFileExtension {
+        
+    }
+}
