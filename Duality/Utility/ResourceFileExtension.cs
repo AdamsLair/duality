5afed88f0d5eeafb7b21993fbcc942db994e99da
darthnihil <rbrunet09@yahoo.ca>
2014-11-24 13:01:51 -0500

    Creation of every global variables
    Without changing the variables inside each resource class, the same
    variable is copied over into the ResourceFileExtension.cs. Furthermore,
    the FileExtension “.res” from the parent class Resources will also be
    moved into the static class.

--- Duality/Utility/ResourceFileExtension.cs
+++ Duality/Utility/ResourceFileExtension.cs
@@ -5,7 +5,62 @@ using System.Text;
 
 namespace Duality.Utility
 {
-    public static class ResourceFileExtension {
-        
+    public static class ResourceFileExtension
+    {
+
+        public const string FileExt = ".res";
+
+        /// <summary>
+        /// An AudioData resources file extension.
+        /// </summary>
+        public const string AudioDataFileExtension = ".AudioData" + FileExt;
+        /// <summary>
+        /// A DrawTechnique resources file extension.
+        /// </summary>
+        public const string DrawTechniqueFileExtension = ".DrawTechnique" + FileExt;
+        /// <summary>
+        /// A Font resources file extension.
+        /// </summary>
+        public const string FontFileExtension = ".Font" + FileExt;
+        /// <summary>
+        /// A FragmentShader resources file extension.
+        /// </summary>
+        public const string FragmentShaderFileExtension = ".FragmentShader" + FileExt;
+        /// <summary>
+        /// A Material resources file extension.
+        /// </summary>
+        public const string MaterialFileExtension = ".Material" + FileExt;
+        /// <summary>
+        /// A Pixmap resources file extension.
+        /// </summary>
+        public const string PixmapFileExtension = ".Pixmap" + FileExt;
+        /// <summary>
+        /// A Prefab resources file extension.
+        /// </summary>
+        public const string PrefabFileExtension = ".Prefab" + FileExt;
+        /// <summary>
+        /// A RenderTarget resources file extension.
+        /// </summary>
+        public const string RenderTargetFileExtension = ".RenderTarget" + FileExt;
+        /// <summary>
+        /// A Scene resources file extension.
+        /// </summary>
+        public const string SceneFileExtension = ".Scene" + FileExt;
+        /// <summary>
+        /// A ShaderProgram resources file extension.
+        /// </summary>
+        public const string ShaderProgramFileExtension = ".ShaderProgram" + FileExt;
+        /// <summary>
+        /// A Sound resources file extension.
+        /// </summary>
+        public const string SoundFileExtension = ".Sound" + FileExt;
+        /// <summary>
+        /// A Texture resources file extension.
+        /// </summary>
+        public const string TextureFileExtension = ".Texture" + FileExt;
+        /// <summary>
+        /// A VertexShader resources file extension.
+        /// </summary>
+        public const string VertexShaderFileExtension = ".VertexShader" + FileExt;
     }
 }
