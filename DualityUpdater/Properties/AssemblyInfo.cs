c1e3f772cac2b5dff61c89d38c6f6fba32b84541
Fedja Adam <mail@fetzenet.de>
2014-11-29 11:13:07 +0100

    Package Update #CHANGE: Pushed AdamsLair.Duality 1.3.1 #CHANGE: Pushed AdamsLair.Duality.Editor 1.1.1 #CHANGE: Pushed AdamsLair.Duality.Samples.DualStickSpaceShooter 1.0.0

--- DualityUpdater/Properties/AssemblyInfo.cs
+++ DualityUpdater/Properties/AssemblyInfo.cs
@@ -32,4 +32,4 @@ using System.Runtime.InteropServices;
 // Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.5")]
+[assembly: AssemblyVersion("1.0.6")]
