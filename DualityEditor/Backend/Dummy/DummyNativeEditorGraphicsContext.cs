04e2878434ea6728da19c55e32883cd16561f29f
Adam <adam@adamslair.net>
2015-06-09 22:44:02 +0200

    Bugfixes #FIX: The editor no longer crashes when running on the dummy editor backend with an actual graphics backend that requires a context to be created.

--- DualityEditor/Backend/Dummy/DummyNativeEditorGraphicsContext.cs
+++ DualityEditor/Backend/Dummy/DummyNativeEditorGraphicsContext.cs
@@ -9,11 +9,31 @@ namespace Duality.Editor.Backend.Dummy
 {
 	internal class DummyNativeEditorGraphicsContext : INativeEditorGraphicsContext
 	{
+		private INativeWindow dummyWindow = null;
+
+		public DummyNativeEditorGraphicsContext()
+		{
+			// Since the graphics backend probably needs some kind of graphics context, create an invisible dummy window.
+			this.dummyWindow = DualityApp.GraphicsBackend.CreateWindow(new WindowOptions
+			{
+				Width = 640,
+				Height = 480,
+				ScreenMode = ScreenMode.Window,
+				RefreshMode = RefreshMode.NoSync
+			});
+		}
 		INativeRenderableSite INativeEditorGraphicsContext.CreateRenderableSite()
 		{
 			return new DummyNativeRenderableSite();
 		}
 		void INativeEditorGraphicsContext.PerformBufferSwap() { }
-		void IDisposable.Dispose() { }
+		void IDisposable.Dispose()
+		{
+			if (this.dummyWindow != null)
+			{
+				this.dummyWindow.Dispose();
+				this.dummyWindow = null;
+			}
+		}
 	}
 }
