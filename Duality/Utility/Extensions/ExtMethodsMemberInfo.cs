59893b34c731ba537411c6549465f25552d0f1b5
Adam <adam@adamslair.net>
2015-06-12 17:28:19 +0200

    MemberInfo Equivalence #ADD: Added a unit test for MemberInfo.IsEquivalent. #CHANGE: Replaced MemberInfo.IsEquivalent implementation with a PCL-friendly one that doesn't rely on MetadataTokens.

--- Duality/Utility/Extensions/ExtMethodsMemberInfo.cs
+++ Duality/Utility/Extensions/ExtMethodsMemberInfo.cs
@@ -18,35 +18,86 @@ namespace Duality
 		{
 			if (first == second)
 				return true;
- 
+
+			// Early-out the basic stuff
 			if (first.DeclaringType != second.DeclaringType)
 				return false;
- 
-			// Methods on arrays do not have metadata tokens but their ReflectedType
-			// always equals their DeclaringType
-			if (first.DeclaringType != null && first.DeclaringType.IsArray)
+			if (first.Module != second.Module)
 				return false;
- 
-			if (first.MetadataToken != second.MetadataToken || first.Module != second.Module)
+			if (first.Name != second.Name)
 				return false;
- 
-			if (first is MethodInfo)
+
+			// Check if they're both the same kind of member
+			if (first is FieldInfo)
 			{
-				MethodInfo lhsMethod = first as MethodInfo;
- 
-				if (lhsMethod.IsGenericMethod)
+				if (!(second is FieldInfo))
+					return false;
+			}
+			else if (first is PropertyInfo)
+			{
+				if (!(second is PropertyInfo))
+					return false;
+			}
+			else if (first is MethodInfo)
+			{
+				if (!(second is MethodInfo))
+					return false;
+			}
+			else if (first is ConstructorInfo)
+			{
+				if (!(second is ConstructorInfo))
+					return false;
+			}
+			else if (first is EventInfo)
+			{
+				if (!(second is EventInfo))
+					return false;
+			}
+			else if (first is TypeInfo)
+			{
+				if (!(second is TypeInfo))
+					return false;
+			}
+
+			if (first is MethodBase)
+			{
+				MethodBase firstMethod = first as MethodBase;
+				MethodBase secondMethod = second as MethodBase;
+
+				// If its a generic method, check its generic Type parameters
+				if (firstMethod.IsGenericMethod)
 				{
-					MethodInfo rhsMethod = second as MethodInfo;
- 
-					Type[] lhsGenArgs = lhsMethod.GetGenericArguments();
-					Type[] rhsGenArgs = rhsMethod.GetGenericArguments();
-					for (int i = 0; i < rhsGenArgs.Length; i++)
+					if (!secondMethod.IsGenericMethod)
+						return false;
+
+					Type[] firstGenArgs = firstMethod.GetGenericArguments();
+					Type[] secondGenArgs = secondMethod.GetGenericArguments();
+					for (int i = 0; i < secondGenArgs.Length; i++)
 					{
-						if (lhsGenArgs[i] != rhsGenArgs[i])
+						if (firstGenArgs[i] != secondGenArgs[i])
 							return false;
 					}
 				}
+
+				// Check its parameters
+				ParameterInfo[] firstParams = firstMethod.GetParameters();
+				ParameterInfo[] secondParams = secondMethod.GetParameters();
+				if (firstParams.Length != secondParams.Length)
+					return false;
+
+				for (int i = 0; i < firstParams.Length; i++)
+				{
+					if (firstParams[i].ParameterType != secondParams[i].ParameterType)
+						return false;
+					if (firstParams[i].IsOut != secondParams[i].IsOut)
+						return false;
+					if (firstParams[i].IsIn != secondParams[i].IsIn)
+						return false;
+					if (firstParams[i].IsRetval != secondParams[i].IsRetval)
+						return false;
+				}
 			}
+
 			return true;
 		}
 	}
---
+++ UnitTesting/DualityTests/Utility/ExtMethodsMemberInfoTest.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Linq;
+
+using Duality;
+
+using NUnit.Framework;
+
+namespace Duality.Tests.Utility
+{
+	[TestFixture]
+	public class ExtMethodsMemberInfo
+	{
+		[Test] public void IsEquivalentFields()
+		{
+			Type typeBase = typeof(MemberInfoEquivalenceBase);
+			Type typeDerived = typeof(MemberInfoEquivalence);
+
+			MemberInfo memberBase = typeBase.GetRuntimeFields().First(m => m.Name == "SomeField");
+			MemberInfo memberDerived = typeDerived.GetRuntimeFields().First(m => m.Name == "SomeField");
+			MemberInfo memberOther = typeDerived.GetRuntimeFields().First(m => m.Name == "SomeField2");
+
+			AssertEquivalence(memberBase, memberDerived);
+			AssertNoEquivalence(memberOther, memberBase);
+			AssertNoEquivalence(memberOther, memberDerived);
+		}
+		[Test] public void IsEquivalentProperties()
+		{
+			Type typeBase = typeof(MemberInfoEquivalenceBase);
+			Type typeDerived = typeof(MemberInfoEquivalence);
+
+			MemberInfo memberBase = typeBase.GetRuntimeProperties().First(m => m.Name == "SomeProperty");
+			MemberInfo memberDerived = typeDerived.GetRuntimeProperties().First(m => m.Name == "SomeProperty");
+			MemberInfo memberOther = typeDerived.GetRuntimeProperties().First(m => m.Name == "SomeProperty2");
+
+			AssertEquivalence(memberBase, memberDerived);
+			AssertNoEquivalence(memberOther, memberBase);
+			AssertNoEquivalence(memberOther, memberDerived);
+		}
+		[Test] public void IsEquivalentEvents()
+		{
+			Type typeBase = typeof(MemberInfoEquivalenceBase);
+			Type typeDerived = typeof(MemberInfoEquivalence);
+
+			MemberInfo memberBase = typeBase.GetRuntimeEvents().First(m => m.Name == "SomeEvent");
+			MemberInfo memberDerived = typeDerived.GetRuntimeEvents().First(m => m.Name == "SomeEvent");
+			MemberInfo memberOther = typeDerived.GetRuntimeEvents().First(m => m.Name == "SomeEvent2");
+			
+			AssertEquivalence(memberBase, memberDerived);
+			AssertNoEquivalence(memberOther, memberBase);
+			AssertNoEquivalence(memberOther, memberDerived);
+		}
+		[Test] public void IsEquivalentMethods()
+		{
+			Type typeBase = typeof(MemberInfoEquivalenceBase);
+			Type typeDerived = typeof(MemberInfoEquivalence);
+
+			MemberInfo memberBase = typeBase.GetRuntimeMethods().First(m => m.Name == "SomeMethod");
+			MemberInfo memberDerived = typeDerived.GetRuntimeMethods().First(m => m.Name == "SomeMethod");
+			MemberInfo memberOther = typeDerived.GetRuntimeMethods().First(m => m.Name == "SomeMethod2");
+			
+			AssertEquivalence(memberBase, memberDerived);
+			AssertNoEquivalence(memberOther, memberBase);
+			AssertNoEquivalence(memberOther, memberDerived);
+
+			MethodInfo memberGenericBase = typeBase.GetRuntimeMethods().First(m => m.Name == "SomeGenericMethod");
+			MethodInfo memberGenericDerived = typeDerived.GetRuntimeMethods().First(m => m.Name == "SomeGenericMethod");
+			MethodInfo memberGenericOther = typeDerived.GetRuntimeMethods().First(m => m.Name == "SomeGenericMethod2");
+			
+			AssertEquivalence(memberGenericBase, memberGenericDerived);
+			AssertNoEquivalence(memberGenericOther, memberGenericBase);
+			AssertNoEquivalence(memberGenericOther, memberGenericDerived);
+
+			AssertEquivalence(
+				memberGenericBase.MakeGenericMethod(typeof(int), typeof(float)), 
+				memberGenericDerived.MakeGenericMethod(typeof(int), typeof(float)));
+			AssertNoEquivalence(
+				memberGenericBase.MakeGenericMethod(typeof(int), typeof(float)), 
+				memberGenericDerived.MakeGenericMethod(typeof(int), typeof(string)));
+			AssertNoEquivalence(
+				memberGenericOther.MakeGenericMethod(typeof(int), typeof(float)), 
+				memberGenericBase.MakeGenericMethod(typeof(int), typeof(float)));
+			AssertNoEquivalence(
+				memberGenericOther.MakeGenericMethod(typeof(int), typeof(float)), 
+				memberGenericDerived.MakeGenericMethod(typeof(int), typeof(float)));
+		}
+
+		private void AssertEquivalence(MemberInfo memberBase, MemberInfo memberEqual)
+		{
+			Assert.IsTrue(memberEqual.IsEquivalent(memberBase));
+			Assert.IsTrue(memberBase.IsEquivalent(memberEqual));
+		}
+		private void AssertNoEquivalence(MemberInfo memberBase, MemberInfo memberEqual)
+		{
+			Assert.IsFalse(memberEqual.IsEquivalent(memberBase));
+			Assert.IsFalse(memberBase.IsEquivalent(memberEqual));
+		}
+		
+		private class MemberInfoEquivalenceBase
+		{
+			public int SomeField;
+			public int SomeField2;
+			public int SomeProperty
+			{
+				get { return this.SomeField; }
+			}
+			public int SomeProperty2
+			{
+				get { return this.SomeField2; }
+			}
+			public void SomeMethod(int value) { }
+			public void SomeMethod2(int value) { }
+			public void SomeGenericMethod<T,U>(T value, U value2) { }
+			public void SomeGenericMethod2<T,U>(T value, U value2) { }
+			public event EventHandler SomeEvent;
+			public event EventHandler SomeEvent2;
+		}
+		private class MemberInfoEquivalence : MemberInfoEquivalenceBase { }
+	}
+}
