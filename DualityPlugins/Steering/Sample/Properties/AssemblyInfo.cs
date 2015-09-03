421334b15296b908e0e37643ce13f9fdbd32f4cb
Adam <adam@adamslair.net>
2015-09-03 19:46:22 +0200

    Package Update #CHANGE: Updated all packages to v2.0.

--- DualityPlugins/Steering/Sample/Properties/AssemblyInfo.cs
+++ DualityPlugins/Steering/Sample/Properties/AssemblyInfo.cs
@@ -32,4 +32,4 @@ using System.Runtime.InteropServices;
 // Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.2")]
+[assembly: AssemblyVersion("2.0.0")]
