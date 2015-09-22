a3dfeb59810a388d8c21b0364221a345ca8be83e
Adam <adam@adamslair.net>
2015-09-22 10:42:52 +0200

    OpenAL Loader Bugfixes #FIX: The audio library loader now searches both working directory and executing directory for fallback libraries to use.

--- Backend/DefaultOpenTK/Backend/Audio/AudioLibraryLoader.cs
+++ Backend/DefaultOpenTK/Backend/Audio/AudioLibraryLoader.cs
@@ -3,16 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.IO;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 namespace Duality.Backend.DefaultOpenTK
 {
 	internal static class AudioLibraryLoader
 	{
-		private static readonly string SourceFileName32 = Path.Combine(DualityApp.PluginDirectory, "OpenALSoft32.dll");
-		private static readonly string SourceFileName64 = Path.Combine(DualityApp.PluginDirectory, "OpenALSoft64.dll");
-		private const string TargetFileName = "OpenAL32.dll";
-
 		public static void LoadAudioLibrary()
 		{
 			// Not running on Windows? External libraries will be handled by Mono / OpenTK anyway.
@@ -23,24 +20,37 @@ namespace Duality.Backend.DefaultOpenTK
 				Environment.OSVersion.Platform == PlatformID.WinCE;
 			if (!isWindows) return;
 
+			// Determine working data
+			Assembly execAssembly = Assembly.GetEntryAssembly() ?? typeof(DualityApp).Assembly;
+			string execAssemblyDir = Path.GetFullPath(Path.GetDirectoryName(execAssembly.Location));
+			string sourceFileName32 = Path.Combine(DualityApp.PluginDirectory, "OpenALSoft32.dll");
+			string sourceFileName64 = Path.Combine(DualityApp.PluginDirectory, "OpenALSoft64.dll");
+			string targetFileName = "OpenAL32.dll";
+
+			// Determine the location of fallback libraries
+			string sourceFilePath32 = sourceFileName32;
+			string sourceFilePath64 = sourceFileName64;
+			if (!File.Exists(sourceFilePath32)) sourceFilePath32 = Path.Combine(execAssemblyDir, sourceFileName32);
+			if (!File.Exists(sourceFilePath64)) sourceFilePath64 = Path.Combine(execAssemblyDir, sourceFileName64);
+
 			// No fallback libraries present? Nothing to do here.
-			if (!File.Exists(SourceFileName64) && !File.Exists(SourceFileName32)) return;
+			if (!File.Exists(sourceFilePath64) && !File.Exists(sourceFilePath32)) return;
 
 			bool openALInstalled = 
-				CheckOpenALDriverPath(Path.Combine(Environment.SystemDirectory, TargetFileName)) ||
-				CheckOpenALDriverPath(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.System), TargetFileName)) ||
-				CheckOpenALDriverPath(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.SystemX86), TargetFileName)) ||
-				CheckOpenALDriverPath(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Windows), TargetFileName));
+				CheckOpenALDriverPath(Path.Combine(Environment.SystemDirectory, targetFileName)) ||
+				CheckOpenALDriverPath(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.System), targetFileName)) ||
+				CheckOpenALDriverPath(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.SystemX86), targetFileName)) ||
+				CheckOpenALDriverPath(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Windows), targetFileName));
 
 			// An OpenAL driver is installed. Uninstall the software fallback.
 			if (openALInstalled)
 			{
-				UninstallSoftwareFallback();
+				UninstallSoftwareFallback(targetFileName);
 			}
 			// No OpenAL driver found? Install the software fallback.
 			else
 			{
-				InstallSoftwareFallback();
+				InstallSoftwareFallback(sourceFilePath32, sourceFilePath64, targetFileName);
 			}
 		}
 		public static void UnloadAudioLibrary()
@@ -52,7 +62,6 @@ namespace Duality.Backend.DefaultOpenTK
 			if (File.Exists(path))
 			{
 				Log.Core.Write("OpenAL Drivers installed in {0}", path);
-				UninstallSoftwareFallback();
 				return true;
 			}
 			else
@@ -61,34 +70,34 @@ namespace Duality.Backend.DefaultOpenTK
 			}
 		}
 		[System.Diagnostics.DebuggerNonUserCode]
-		private static void InstallSoftwareFallback()
+		private static void InstallSoftwareFallback(string sourceFilePath32, string sourceFilePath64, string targetFilePath)
 		{
 			Log.Core.Write("OpenAL Drivers not found. Using {0} software fallback.", Environment.Is64BitProcess ? "64 Bit" : "32 Bit");
 			if (Environment.Is64BitProcess)
 			{
-				if (!File.Exists(SourceFileName64)) return;
+				if (!File.Exists(sourceFilePath64)) return;
 				try
 				{
-					File.Copy(SourceFileName64, TargetFileName, true);
+					File.Copy(sourceFilePath64, targetFilePath, true);
 				}
 				catch (Exception) {}
 			}
 			else
 			{
-				if (!File.Exists(SourceFileName32)) return;
+				if (!File.Exists(sourceFilePath32)) return;
 				try
 				{
-					File.Copy(SourceFileName32, TargetFileName, true);
+					File.Copy(sourceFilePath32, targetFilePath, true);
 				}
 				catch (Exception) {}
 			}
 		}
 		[System.Diagnostics.DebuggerNonUserCode]
-		private static void UninstallSoftwareFallback()
+		private static void UninstallSoftwareFallback(string targetFilePath)
 		{
 			try
 			{
-				if (File.Exists(TargetFileName)) File.Delete(TargetFileName);
+				if (File.Exists(targetFilePath)) File.Delete(targetFilePath);
 			}
 			catch (Exception) {}
 		}
