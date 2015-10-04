7f75297305b6a07deb9dd2d9ef193a14a81fa8a2
Adam <adam@adamslair.net>
2015-10-04 16:02:22 +0200

    Log Message Tweaks #CHANGE: The "Loading Resource" log no longer uses three dots at the end of the line, so console highlighting won't trigger here.

--- Duality/ContentProvider.cs
+++ Duality/ContentProvider.cs
@@ -443,7 +443,7 @@ namespace Duality
 		{
 			if (string.IsNullOrEmpty(path) || IsDefaultContentPath(path) || !FileOp.Exists(path)) return null;
 
-			Log.Core.Write("Loading Resource '{0}'...", path);
+			Log.Core.Write("Loading Resource '{0}'", path);
 			Log.Core.PushIndent();
 
 			// Load the Resource and register it
