f5072179d61ebfb177a2f29ae748db1c038bfb78
SirePi <yamailbox@gmail.com>
2015-10-29 12:21:52 +0100

    Space and tabs

--- SampleProjects/FlapOrDie/Controllers/MainMenuController.cs
+++ SampleProjects/FlapOrDie/Controllers/MainMenuController.cs
@@ -10,17 +10,17 @@ using Duality.Resources;
 
 namespace FlapOrDie.Controllers
 {
-    [RequiredComponent(typeof(Camera))]
-    public class MainMenuController : Component, ICmpUpdatable
-    {
-        void ICmpUpdatable.OnUpdate()
-        {
-            if(DualityApp.Keyboard.KeyHit(Key.Escape))
-            {
-                DualityApp.Terminate();
-            }
-            if(DualityApp.Keyboard.KeyHit(Key.Space))
-            {
+	[RequiredComponent(typeof(Camera))]
+	public class MainMenuController : Component, ICmpUpdatable
+	{
+		void ICmpUpdatable.OnUpdate()
+		{
+			if (DualityApp.Keyboard.KeyHit(Key.Escape))
+			{
+				DualityApp.Terminate();
+			}
+			if (DualityApp.Keyboard.KeyHit(Key.Space))
+			{
 				//preloading materials and sounds
 				foreach (ContentRef<Material> m in ContentProvider.GetAvailableContent<Material>())
 				{
@@ -31,11 +31,11 @@ namespace FlapOrDie.Controllers
 					s.MakeAvailable();
 				}
 
-                ContentRef<Scene> gameScene = ContentProvider.RequestContent<Scene>(@"Data\GameScene.Scene.res");
-                gameScene.Res.FindComponent<GameController>().Reset();
+				ContentRef<Scene> gameScene = ContentProvider.RequestContent<Scene>(@"Data\GameScene.Scene.res");
+				gameScene.Res.FindComponent<GameController>().Reset();
 
-                Scene.SwitchTo(gameScene);
-            }
-        }
-    }
+				Scene.SwitchTo(gameScene);
+			}
+		}
+	}
 }
