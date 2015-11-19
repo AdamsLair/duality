650d1bf476818163b05e27d4f3563cc677b69489
Adam <adam@adamslair.net>
2015-11-19 14:58:40 +0100

    Tileset Visual Input #ADD: Added a first visual input definition for Tilesets. #CHANGE: Tilesets now need to be "compiled" in order to prepare their optimized target data.

--- DualityPlugins/Tilemaps/Tileset.cs
+++ DualityPlugins/Tilemaps/Tileset.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 
+using Duality.Cloning;
 using Duality.Resources;
 using Duality.Editor;
 using Duality.Plugins.Tilemaps.Properties;
@@ -17,5 +18,52 @@ namespace Duality.Plugins.Tilemaps
 	[EditorHintImage(TilemapsResNames.ImageTileset)]
 	public class Tileset : Resource
 	{
+		private static readonly TilesetVisualInput[] EmptyVisualInputLayers = new TilesetVisualInput[0];
+
+		private TilesetVisualInput[] visualInputLayers = EmptyVisualInputLayers;
+		[DontSerialize] private bool compiled          = false;
+
+		/// <summary>
+		/// The different layers of <see cref="TilesetVisualInput"/>, which compose the look of all the tiles
+		/// that are defined in this <see cref="Tileset"/>.
+		/// </summary>
+		public IList<TilesetVisualInput> VisualInputLayers
+		{
+			get { return this.visualInputLayers; }
+		}
+		/// <summary>
+		/// [GET] Whether this <see cref="Tileset"/> has been compiled yet or not.
+		/// </summary>
+		[EditorHintFlags(MemberFlags.Invisible)]
+		public bool Compiled
+		{
+			get { return this.compiled; }
+		}
+
+		/// <summary>
+		/// Compiles the <see cref="Tileset"/> using the specified source data, in order to
+		/// generate optimized target data for rendering and collision detection.
+		/// </summary>
+		public void Compile()
+		{
+			this.compiled = true;
+		}
+
+		protected override void OnLoaded()
+		{
+			this.Compile();
+			base.OnLoaded();
+		}
+		protected override void OnDisposing(bool manually)
+		{
+			base.OnDisposing(manually);
+			this.compiled = false;
+		}
+		protected override void OnCopyDataTo(object target, ICloneOperation operation)
+		{
+			base.OnCopyDataTo(target, operation);
+			Tileset targetTileset = target as Tileset;
+			if (this.compiled) targetTileset.Compile();
+		}
 	}
 }
