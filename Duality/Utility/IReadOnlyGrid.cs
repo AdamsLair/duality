7ae768340e1cc42f388e8a4aeb26f2d7f8dba3ea
Adam <adam@adamslair.net>
2015-11-23 14:17:40 +0100

    Added IReadOnlyGrid<T> #ADD: Added a new IReadOnlyGrid<T> interface and implemented it in Grid<T>.

---
+++ Duality/Utility/IReadOnlyGrid.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+
+namespace Duality
+{
+	/// <summary>
+	/// Represents a readonly interface for two-dimensional grid-aligned data.
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	public interface IReadOnlyGrid<out T> : IEnumerable<T>
+	{
+		/// <summary>
+		/// [GET] The grids width.
+		/// </summary>
+		int Width { get; }
+		/// <summary>
+		/// [GET] The grids height.
+		/// </summary>
+		int Height { get; }
+
+		/// <summary>
+		/// [GET] Accesses a grid element at the specified position.
+		/// </summary>
+		/// <param name="x"></param>
+		/// <param name="y"></param>
+		/// <returns></returns>
+		T this[int x, int y] { get; }
+	}
+}
