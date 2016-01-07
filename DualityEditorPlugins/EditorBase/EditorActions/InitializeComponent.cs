a27c108bdefd449c72012164bf71f2c2fd18b3f4
deanljohnson <dean@deanljohnson.com>
2016-01-07 04:31:56 -0800

    Added the EditorAction classes themselves

---
+++ DualityEditorPlugins/EditorBase/EditorActions/InitializeComponent.cs
@@ -0,0 +1,10 @@
+﻿namespace Duality.Editor.Plugins.Base.EditorActions
+{
+    public abstract class InitializeComponent<T> : EditorSingleAction<T> where T : Component
+    {
+        public override bool MatchesContext(string context)
+        {
+            return context == DualityEditorApp.ActionContextInitializeComponent;
+        }
+    }
+}
---
+++ DualityEditorPlugins/EditorBase/EditorActions/InitializeRigidBody.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+using Duality.Components.Physics;
+
+namespace Duality.Editor.Plugins.Base.EditorActions
+{
+    public class InitializeRigidBody : InitializeComponent<RigidBody>
+    {
+        public override bool CanPerformOn(RigidBody body)
+        {
+            return base.CanPerformOn(body) && !body.Shapes.Any();
+        }
+
+        public override void Perform(RigidBody body)
+        {
+            if (body.Shapes.Any()) return;
+
+            // Add a default shape when creating a new RigidBody in the editor
+            body.AddShape(new CircleShapeInfo(128.0f, Vector2.Zero, 1.0f));
+        }
+    }
+}
