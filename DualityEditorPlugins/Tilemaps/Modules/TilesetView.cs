4954157a6e06702e42d281defe1b9325d80440a4
Adam <adam@adamslair.net>
2016-01-14 12:44:35 +0100

    TilesetView Selection #ADD: Added rectangular user selection to the TilesetView Control.

--- DualityEditorPlugins/Tilemaps/Modules/TilesetView.cs
+++ DualityEditorPlugins/Tilemaps/Modules/TilesetView.cs
@@ -35,6 +35,9 @@ namespace Duality.Editor.Plugins.Tilemaps
 		private Size                spacing                = new Size(2, 2);
 		private int                 hoverIndex             = -1;
 		private bool                globalEventsSubscribed = false;
+		private Rectangle           selectedArea           = Rectangle.Empty;
+		private Point               actionBeginTilePos     = Point.Empty;
+		private bool                isUserSelecting        = false;
 
 
 		public ContentRef<Tileset> TargetTileset
@@ -91,6 +94,18 @@ namespace Duality.Editor.Plugins.Tilemaps
 				}
 			}
 		}
+		public Rectangle SelectedArea
+		{
+			get { return this.selectedArea; }
+			set
+			{
+				if (this.selectedArea != value)
+				{
+					this.selectedArea = value;
+					this.Invalidate();
+				}
+			}
+		}
 
 
 		public TilesetView()
@@ -117,6 +132,14 @@ namespace Duality.Editor.Plugins.Tilemaps
 			base.Dispose(disposing);
 		}
 		
+		/// <summary>
+		/// Determines the tile index at the specified pixel / view coordinate.
+		/// </summary>
+		/// <param name="x"></param>
+		/// <param name="y"></param>
+		/// <param name="scrolled"></param>
+		/// <param name="allowNearest"></param>
+		/// <returns></returns>
 		public int PickTileIndexAt(int x, int y, bool scrolled = true, bool allowNearest = false)
 		{
 			if (scrolled)
@@ -173,6 +196,12 @@ namespace Duality.Editor.Plugins.Tilemaps
 
 			return modelIndex;
 		}
+		/// <summary>
+		/// Determines the pixel / view coordinate of the specified tile index.
+		/// </summary>
+		/// <param name="tileIndex"></param>
+		/// <param name="scrolled"></param>
+		/// <returns></returns>
 		public Point GetTileIndexLocation(int tileIndex, bool scrolled = true)
 		{
 			Point result = this.ClientRectangle.Location;
@@ -206,6 +235,28 @@ namespace Duality.Editor.Plugins.Tilemaps
 				result.Y += this.AutoScrollPosition.Y;
 			}
 			return result;
+		}
+
+		/// <summary>
+		/// Converts a 2D tile (not pixel) coordinate into the associated tile index.
+		/// </summary>
+		/// <param name="tileX"></param>
+		/// <param name="tileY"></param>
+		/// <returns></returns>
+		public int GetTileIndex(int tileX, int tileY)
+		{
+			return tileX + tileY * this.tileCount.X;
+		}
+		/// <summary>
+		/// Converts a tile index into a 2D tile (not pixel) coordinate.
+		/// </summary>
+		/// <param name="tileIndex"></param>
+		/// <returns></returns>
+		public Point GetTilePos(int tileIndex)
+		{
+			return new Point(
+				tileIndex % this.tileCount.X,
+				tileIndex / this.tileCount.X);
 		}
 		
 		private void UpdateContentStats()
@@ -363,6 +414,7 @@ namespace Duality.Editor.Plugins.Tilemaps
 			base.OnEnabledChanged(e);
 			this.Invalidate();
 		}
+
 		protected override void OnPaint(PaintEventArgs e)
 		{
 			base.OnPaint(e);
@@ -452,27 +504,116 @@ namespace Duality.Editor.Plugins.Tilemaps
 					this.tileSize.Height + 1);
 			}
 
+			// Draw selection indicators
+			if (this.Enabled && !this.selectedArea.IsEmpty)
+			{
+				int startIndex = this.GetTileIndex(this.selectedArea.X, this.selectedArea.Y);
+				Point startPos = this.GetTileIndexLocation(startIndex);
+
+				// "Grey out" everything except the current selection a little bit
+				Region oldClip = e.Graphics.Clip;
+				e.Graphics.SetClip(new Rectangle(
+					startPos.X - 1, 
+					startPos.Y - 1, 
+					this.selectedArea.Width * (this.tileSize.Width + 1), 
+					this.selectedArea.Height * (this.tileSize.Height + 1)), 
+					System.Drawing.Drawing2D.CombineMode.Exclude);
+				e.Graphics.FillRectangle(new SolidBrush(Color.FromArgb(64, this.BackColor)), this.ClientRectangle);
+				e.Graphics.Clip = oldClip;
+
+				// Draw the selected tile area
+				e.Graphics.DrawRectangle(
+					new Pen(this.BackColor), 
+					startPos.X - 1, 
+					startPos.Y - 1, 
+					this.selectedArea.Width * (this.tileSize.Width + 1), 
+					this.selectedArea.Height * (this.tileSize.Height + 1));
+				e.Graphics.DrawRectangle(
+					new Pen(this.ForeColor), 
+					startPos.X - 2, 
+					startPos.Y - 2, 
+					this.selectedArea.Width * (this.tileSize.Width + 1) + 2, 
+					this.selectedArea.Height * (this.tileSize.Height + 1) + 2);
+				e.Graphics.DrawRectangle(
+					new Pen(this.BackColor), 
+					startPos.X - 3, 
+					startPos.Y - 3, 
+					this.selectedArea.Width * (this.tileSize.Width + 1) + 4, 
+					this.selectedArea.Height * (this.tileSize.Height + 1) + 4);
+				e.Graphics.DrawRectangle(
+					new Pen(Color.FromArgb(128, this.BackColor)), 
+					startPos.X - 4, 
+					startPos.Y - 4, 
+					this.selectedArea.Width * (this.tileSize.Width + 1) + 6, 
+					this.selectedArea.Height * (this.tileSize.Height + 1) + 6);
+			}
+
 			// Overlay with disabled plate
 			if (!this.Enabled)
 			{
 				e.Graphics.FillRectangle(new SolidBrush(Color.FromArgb(128, this.BackColor)), this.ClientRectangle);
 			}
 		}
-		protected override void OnLeave(EventArgs e)
+		protected override void OnMouseLeave(EventArgs e)
 		{
-			base.OnLeave(e);
+			base.OnMouseLeave(e);
 			this.hoverIndex = -1;
 			this.Invalidate();
+		}
+		protected override void OnMouseDown(MouseEventArgs e)
+		{
+			base.OnMouseDown(e);
+			if (e.Button == MouseButtons.Left)
+			{
+				int tileIndex = this.PickTileIndexAt(e.X, e.Y);
+				if (tileIndex != -1)
+				{
+					this.hoverIndex = -1;
+					this.actionBeginTilePos = this.GetTilePos(tileIndex);
+					this.isUserSelecting = true;
+					this.SelectedArea = new Rectangle(this.actionBeginTilePos.X, this.actionBeginTilePos.Y, 1, 1);
+				}
+			}
+		}
+		protected override void OnMouseUp(MouseEventArgs e)
+		{
+			base.OnMouseUp(e);
+			if (!this.isUserSelecting)
+			{
+				this.SelectedArea = Rectangle.Empty;
+			}
+			this.actionBeginTilePos = Point.Empty;
+			this.isUserSelecting = false;
 		}
 		protected override void OnMouseMove(MouseEventArgs e)
 		{
 			base.OnMouseMove(e);
-			int oldHoverIndex = this.hoverIndex;
-
-			this.hoverIndex = this.PickTileIndexAt(e.X, e.Y);
-
-			if (oldHoverIndex != this.hoverIndex)
-				this.Invalidate();
+			int tileIndex = this.PickTileIndexAt(e.X, e.Y);
+			if (this.isUserSelecting)
+			{
+				if (tileIndex != -1)
+				{
+					Point tilePos = this.GetTilePos(tileIndex);
+					Point selectionTopLeft = new Point(
+						Math.Min(this.actionBeginTilePos.X, tilePos.X), 
+						Math.Min(this.actionBeginTilePos.Y, tilePos.Y));
+					Point selectionBottomRight = new Point(
+						Math.Max(this.actionBeginTilePos.X, tilePos.X), 
+						Math.Max(this.actionBeginTilePos.Y, tilePos.Y));
+					this.SelectedArea = new Rectangle(
+						selectionTopLeft.X,
+						selectionTopLeft.Y,
+						selectionBottomRight.X - selectionTopLeft.X + 1,
+						selectionBottomRight.Y - selectionTopLeft.Y + 1);
+				}
+			}
+			else
+			{
+				int oldHoverIndex = this.hoverIndex;
+				this.hoverIndex = tileIndex;
+				if (oldHoverIndex != this.hoverIndex)
+					this.Invalidate();
+			}
 		}
 		protected override void OnMouseClick(MouseEventArgs e)
 		{
