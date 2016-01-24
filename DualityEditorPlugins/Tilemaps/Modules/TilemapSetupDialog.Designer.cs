b6a1118751ed6c879ff4bfd16c22be6bf9e2aa21
Adam <adam@adamslair.net>
2016-01-24 17:58:58 +0100

    Created TilemapSetupDialog Form #ADD: Added an empty TilemapSetupDialog form to be populated and implemented later.

---
+++ DualityEditorPlugins/Tilemaps/Modules/TilemapSetupDialog.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace Duality.Editor.Plugins.Tilemaps
+{
+	partial class TilemapSetupDialog
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.panelBottomBack = new System.Windows.Forms.Panel();
+			this.buttonOk = new System.Windows.Forms.Button();
+			this.buttonCancel = new System.Windows.Forms.Button();
+			this.panelBottomBack.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// panelBottomBack
+			// 
+			this.panelBottomBack.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.panelBottomBack.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(196)))), ((int)(((byte)(196)))), ((int)(((byte)(196)))));
+			this.panelBottomBack.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.panelBottomBack.Controls.Add(this.buttonOk);
+			this.panelBottomBack.Controls.Add(this.buttonCancel);
+			this.panelBottomBack.Location = new System.Drawing.Point(-1, 180);
+			this.panelBottomBack.Name = "panelBottomBack";
+			this.panelBottomBack.Size = new System.Drawing.Size(355, 58);
+			this.panelBottomBack.TabIndex = 6;
+			// 
+			// buttonOk
+			// 
+			this.buttonOk.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.buttonOk.Location = new System.Drawing.Point(183, 7);
+			this.buttonOk.Name = "buttonOk";
+			this.buttonOk.Size = new System.Drawing.Size(75, 23);
+			this.buttonOk.TabIndex = 3;
+			this.buttonOk.Text = "Ok";
+			this.buttonOk.UseVisualStyleBackColor = true;
+			// 
+			// buttonCancel
+			// 
+			this.buttonCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.buttonCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+			this.buttonCancel.Location = new System.Drawing.Point(264, 7);
+			this.buttonCancel.Name = "buttonCancel";
+			this.buttonCancel.Size = new System.Drawing.Size(75, 23);
+			this.buttonCancel.TabIndex = 2;
+			this.buttonCancel.Text = "Cancel";
+			this.buttonCancel.UseVisualStyleBackColor = true;
+			// 
+			// TilemapSetupDialog
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(212)))), ((int)(((byte)(212)))), ((int)(((byte)(212)))));
+			this.ClientSize = new System.Drawing.Size(351, 220);
+			this.Controls.Add(this.panelBottomBack);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.Name = "TilemapSetupDialog";
+			this.ShowIcon = false;
+			this.ShowInTaskbar = false;
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+			this.Text = "Tilemap Setup";
+			this.panelBottomBack.ResumeLayout(false);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Panel panelBottomBack;
+		private System.Windows.Forms.Button buttonOk;
+		private System.Windows.Forms.Button buttonCancel;
+
+	}
+}
