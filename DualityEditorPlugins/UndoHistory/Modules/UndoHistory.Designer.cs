ecd2e435acff2adc594f64ebb561ac6a75f8031e
Gareth Brading <gareth.brading@gmail.com>
2016-02-27 01:01:14 +0000

    Undo History Modules for issue #296 Implemented as an editor plugin, but unfortunately had to expose UndoRedoManager.actionIndex and actionStack

---
+++ DualityEditorPlugins/UndoHistory/Modules/UndoHistory.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace Duality.Editor.Plugins.UndoHistory.Modules
+{
+	partial class UndoHistory
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UndoHistory));
+            this.undoHistoryList = new Duality.Editor.Plugins.UndoHistory.Modules.UndoHistoryList();
+            this.SuspendLayout();
+            // 
+            // undoHistoryList
+            // 
+            this.undoHistoryList.AutoScroll = true;
+            this.undoHistoryList.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(196)))), ((int)(((byte)(196)))), ((int)(((byte)(196)))));
+            this.undoHistoryList.BaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(162)))), ((int)(((byte)(162)))), ((int)(((byte)(162)))));
+            this.undoHistoryList.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.undoHistoryList.Font = new System.Drawing.Font("Lucida Console", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.undoHistoryList.Location = new System.Drawing.Point(0, 0);
+            this.undoHistoryList.Name = "undoHistoryList";
+            this.undoHistoryList.ScrollOffset = 0;
+            this.undoHistoryList.SelectedEntry = null;
+            this.undoHistoryList.Size = new System.Drawing.Size(683, 195);
+            this.undoHistoryList.TabIndex = 3;
+            // 
+            // UndoHistory
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(683, 195);
+            this.Controls.Add(this.undoHistoryList);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "UndoHistory";
+            this.ShowHint = WeifenLuo.WinFormsUI.Docking.DockState.DockBottomAutoHide;
+            this.Text = "Undo/Redo...";
+            this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+        private UndoHistoryList undoHistoryList;
+	}
+}
---
+++ DualityEditorPlugins/UndoHistory/UndoHistoryPlugin.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using AdamsLair.WinForms.ItemModels;
+using Duality;
+using Duality.Editor.Forms;
+using Duality.Editor.Plugins.UndoHistory.Properties;
+using Duality.Editor.Properties;
+using WeifenLuo.WinFormsUI.Docking;
+
+namespace Duality.Editor.Plugins.UndoHistory
+{
+	public class UndoHistoryPlugin : EditorPlugin
+	{
+		private	Modules.UndoHistory	undoHistory		= null;
+		private	bool	isLoading	= false;
+
+
+		public override string Id
+		{
+			get { return "UndoHistory"; }
+		}
+
+
+		protected override IDockContent DeserializeDockContent(Type dockContentType)
+		{
+			this.isLoading = true;
+			IDockContent result;
+			if (dockContentType == typeof(Modules.UndoHistory))
+				result = this.RequestUndoHistory();
+			else
+				result = base.DeserializeDockContent(dockContentType);
+			this.isLoading = false;
+			return result;
+		}
+		
+		protected override void InitPlugin(MainForm main)
+		{
+			base.InitPlugin(main);
+			
+			// Request menu
+			MenuModelItem viewItem = main.MainMenu.RequestItem(GeneralRes.MenuName_Edit);
+			viewItem.AddItem(new MenuModelItem
+			{
+                Name = UndoHistoryRes.MenuItemName_UndoHistory,
+				Icon = UndoHistoryResCache.IconUndoHistory,
+				ActionHandler = this.menuItemUndoHistory_Click
+			});
+		}
+		
+		public Modules.UndoHistory RequestUndoHistory()
+		{
+			if (this.undoHistory == null || this.undoHistory.IsDisposed)
+			{
+				this.undoHistory = new Modules.UndoHistory();
+				this.undoHistory.FormClosed += delegate(object sender, FormClosedEventArgs e) { this.undoHistory = null; };
+			}
+
+			if (!this.isLoading)
+			{
+				this.undoHistory.Show(DualityEditorApp.MainForm.MainDockPanel);
+				if (this.undoHistory.Pane != null)
+				{
+					this.undoHistory.Pane.Activate();
+					this.undoHistory.Focus();
+				}
+			}
+
+			return this.undoHistory;
+		}
+
+		private void menuItemUndoHistory_Click(object sender, EventArgs e)
+		{
+			this.RequestUndoHistory();
+		}
+	}
+}
