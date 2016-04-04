b9a5efcaac34f90d2a5fe8834f8edd64f6b65c6e
mfep <serfozo.lorinc@live.com>
2016-04-04 17:45:50 +0200

    style

--- Duality/Resources/RenderTarget.cs
+++ Duality/Resources/RenderTarget.cs
@@ -211,8 +211,10 @@ namespace Duality.Resources
 		/// </summary>
 		protected void SetupNativeRes()
 		{
-			foreach (var target in this.targets.Where(t => t != null).Res()) {
-				if (target.PixelWidth == 0 || target.PixelHeight == 0) {
+			foreach (var target in this.targets.Where(t => t != null).Res())
+			{
+				if (target.PixelWidth == 0 || target.PixelHeight == 0)
+				{
 					Log.Core.WriteError("Error initializing RenderTarget {0}: Texture {1} has a dimension of 0 {2}",
 						this.FullName, target.FullName, Environment.NewLine);
 					return;
