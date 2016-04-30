833e90b3f79ecd92ac847ba9391b6c9cb42c0a05
Adam <adam@adamslair.net>
2016-04-30 14:32:11 +0200

    Open Resource Action Tweaks #CHANGE: The "Open Resource" / double click action now prefers to open already existing media source files and will only export the Resource if they no longer exist.

--- DualityEditorPlugins/EditorBase/EditorActions/OpenResource.cs
+++ DualityEditorPlugins/EditorBase/EditorActions/OpenResource.cs
@@ -21,30 +21,49 @@ namespace Duality.Editor.Plugins.Base.EditorActions
 
 		public override void Perform(Resource obj)
 		{
+			// If we still have the original source files around, prefer them.
+			string[] importedPaths = AssetManager.GetAssetSourceFiles(obj);
+			if (importedPaths.Length > 0 && importedPaths.All(p => File.Exists(p)))
+			{
+				this.OpenPaths(importedPaths);
+				return;
+			}
+
+			// Otherwise, export the Resource and open the result.
 			string[] exportedPaths = AssetManager.ExportAssets(obj);
-			
-			// If there is only a single source path, open the file right away
-			if (exportedPaths.Length == 1)
-			{
-				System.Diagnostics.Process.Start(exportedPaths[0]);
-			}
-			// If there are multiple source paths, just open the base directory
-			else
-			{
-				string mutualBaseDir = PathHelper.GetMutualBaseDirectory(exportedPaths);
-				System.Diagnostics.Process.Start(mutualBaseDir);
-			}
+			this.OpenPaths(exportedPaths);
 		}
 		public override bool CanPerformOn(Resource obj)
 		{
 			if (!base.CanPerformOn(obj)) return false;
+
+			// If we still have the original source files around, prefer them.
+			string[] importedPaths = AssetManager.GetAssetSourceFiles(obj);
+			if (importedPaths.Length > 0 && importedPaths.All(p => File.Exists(p)))
+				return true;
 			
+			// Otherwise, see if we can export the resource for opening it.
 			string[] exportedPaths = AssetManager.SimulateExportAssets(obj);
 			return exportedPaths.Length > 0;
 		}
 		public override bool MatchesContext(string context)
 		{
 			return context == DualityEditorApp.ActionContextOpenRes;
+		}
+
+		private void OpenPaths(string[] paths)
+		{
+			// If there is only a single source path, open the file right away
+			if (paths.Length == 1)
+			{
+				System.Diagnostics.Process.Start(paths[0]);
+			}
+			// If there are multiple source paths, just open the base directory
+			else
+			{
+				string mutualBaseDir = PathHelper.GetMutualBaseDirectory(paths);
+				System.Diagnostics.Process.Start(mutualBaseDir);
+			}
 		}
 	}
 }
