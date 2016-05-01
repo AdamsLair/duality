d3602d003f751bc61e2728e81b0a0e82e75955b0
Adam <adam@adamslair.net>
2016-05-01 15:10:49 +0200

    AssetManager Events #ADD: Added global editor events that are fired when a public import or export operation has been performed.

---
+++ DualityEditor/AssetManagement/AssetExportFinishedEventArgs.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+using Duality;
+
+namespace Duality.Editor.AssetManagement
+{
+	/// <summary>
+	/// Provides information about a finished export operation.
+	/// </summary>
+	public class AssetExportFinishedEventArgs : EventArgs
+	{
+		private bool     success = false;
+		private Resource input   = null;
+		private string[] output  = null;
+
+		/// <summary>
+		/// [GET] Whether the operation was successful.
+		/// </summary>
+		public bool IsSuccessful
+		{
+			get { return this.success; }
+		}
+		/// <summary>
+		/// [GET] The <see cref="Resource"/> that was exported.
+		/// </summary>
+		public Resource Input
+		{
+			get { return this.input; }
+		}
+		/// <summary>
+		/// [GET] The list of output source files that were exported.
+		/// </summary>
+		public IReadOnlyList<string> Output
+		{
+			get { return this.output; }
+		}
+
+		public AssetExportFinishedEventArgs(bool isSuccess, Resource input, IEnumerable<string> output)
+		{
+			this.success = isSuccess;
+			this.input = input;
+			this.output = output.ToArray();
+		}
+	}
+}
---
+++ DualityEditor/AssetManagement/AssetImportFinishedEventArgs.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+using Duality;
+
+namespace Duality.Editor.AssetManagement
+{
+	/// <summary>
+	/// Provides information about a finished import operation.
+	/// </summary>
+	public class AssetImportFinishedEventArgs : EventArgs
+	{
+		private bool                success  = false;
+		private bool                reImport = false;
+		private AssetImportInput[]  input    = null;
+		private AssetImportOutput[] output   = null;
+
+		/// <summary>
+		/// [GET] Whether the operation was successful.
+		/// </summary>
+		public bool IsSuccessful
+		{
+			get { return this.success; }
+		}
+		/// <summary>
+		/// [GET] Whether this was a re-import of an existing <see cref="Resource"/>.
+		/// </summary>
+		public bool IsReImport
+		{
+			get { return this.reImport; }
+		}
+		/// <summary>
+		/// [GET] The list of input source files that were used as a basis for the operation.
+		/// </summary>
+		public IReadOnlyList<AssetImportInput> Input
+		{
+			get { return this.input; }
+		}
+		/// <summary>
+		/// [GET] The list of output Resources that were imported, along with the source files that each of them used.
+		/// </summary>
+		public IReadOnlyList<AssetImportOutput> Output
+		{
+			get { return this.output; }
+		}
+
+		public AssetImportFinishedEventArgs(bool isSuccess, bool isReImport, IEnumerable<AssetImportInput> input, IEnumerable<AssetImportOutput> output)
+		{
+			this.success = isSuccess;
+			this.reImport = isReImport;
+			this.input = input.ToArray();
+			this.output = output.ToArray();
+		}
+	}
+}
