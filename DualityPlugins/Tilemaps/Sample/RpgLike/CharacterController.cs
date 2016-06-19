5f5f90aeee0cc916e1af6ddce5d5f173e8f1e8b0
Adam <adam@adamslair.net>
2016-06-19 12:24:26 +0200

    Tilemap Sample #ADD: Added collision-aware character controls.

--- DualityPlugins/Tilemaps/Sample/RpgLike/CharacterController.cs
+++ DualityPlugins/Tilemaps/Sample/RpgLike/CharacterController.cs
@@ -21,9 +21,10 @@ namespace Duality.Plugins.Tilemaps.Sample.RpgLike
 	[EditorHintImage(TilemapsResNames.ImageActorRenderer)]
 	public class CharacterController : Component, ICmpUpdatable
 	{
-		private float   speed          = 1.0f;
-		private float   acceleration   = 0.2f;
-		private Vector2 targetMovement = Vector2.Zero;
+		private float   speed           = 1.0f;
+		private float   acceleration    = 0.2f;
+		private float   moveSenseRadius = 96.0f;
+		private Vector2 targetMovement  = Vector2.Zero;
 
 		public float Speed
 		{
@@ -35,6 +36,11 @@ namespace Duality.Plugins.Tilemaps.Sample.RpgLike
 			get { return this.acceleration; }
 			set { this.acceleration = value; }
 		}
+		public float MovementSensingRadius
+		{
+			get { return this.moveSenseRadius; }
+			set { this.moveSenseRadius = value; }
+		}
 		public Vector2 TargetMovement
 		{
 			get { return this.targetMovement; }
@@ -43,10 +49,32 @@ namespace Duality.Plugins.Tilemaps.Sample.RpgLike
 
 		void ICmpUpdatable.OnUpdate()
 		{
+			Transform transform = this.GameObj.Transform;
 			RigidBody body = this.GameObj.GetComponent<RigidBody>();
 
-			Vector2 normalizedTargetMovement = this.targetMovement / MathF.Max(0.001f, this.targetMovement.Length);
-			Vector2 targetVelocity = normalizedTargetMovement * this.speed;
+			Vector2 adjustedTargetMovement = this.targetMovement / MathF.Max(1.0f, this.targetMovement.Length);;
+			if (adjustedTargetMovement.Length > 0.01f && this.moveSenseRadius > 0.0f)
+			{
+				Vector2 targetDir = this.targetMovement.Normalized;
+				float senseRadius = this.moveSenseRadius * adjustedTargetMovement.Length;
+
+				RayCastData firstHit;
+				Vector2 rayStart = transform.Pos.Xy;
+				Vector2 rayEnd = rayStart + targetDir * senseRadius;
+				bool hitAnything = RigidBody.RayCast(rayStart, rayEnd, data => data.Fraction, out firstHit);
+
+				if (hitAnything)
+				{
+					float movementRatio = 0.1f + 0.9f * ((firstHit.Pos - rayStart).Length / (rayEnd - rayStart).Length);
+					adjustedTargetMovement *= movementRatio;
+				}
+
+				VisualLog.Default
+					.DrawConnection(rayStart.X, rayStart.Y, 0.0f, hitAnything ? firstHit.Pos.X : rayEnd.X, hitAnything ? firstHit.Pos.Y : rayEnd.Y)
+					.WithOffset(-100);
+			}
+
+			Vector2 targetVelocity = adjustedTargetMovement * this.speed;
 			Vector2 appliedForce = (targetVelocity - body.LinearVelocity) * body.Mass * this.acceleration;
 			body.ApplyLocalForce(appliedForce);
 		}
