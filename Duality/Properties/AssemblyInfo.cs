2ca8feeb645c4911848dc0132a0255f882b3090b
Adam <adam@adamslair.net>
2016-06-20 22:40:12 +0200

    Package Update #CHANGE: Updated Package Specs to AdamsLair.Duality.Editor 2.5.2 #CHANGE: Updated Package Specs to AdamsLair.Duality 2.6.2

--- Duality/Properties/AssemblyInfo.cs
+++ Duality/Properties/AssemblyInfo.cs
@@ -32,7 +32,7 @@ using System.Runtime.InteropServices;
 //
 // Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
 // übernehmen, indem Sie "*" eingeben:
-// [assembly: AssemblyVersion("2.6.1")]
-[assembly: AssemblyVersion("2.6.1")]
+// [assembly: AssemblyVersion("2.6.2")]
+[assembly: AssemblyVersion("2.6.2")]
 [assembly: InternalsVisibleTo("DualityEditor")]
 [assembly: InternalsVisibleTo("DualityTests")]
