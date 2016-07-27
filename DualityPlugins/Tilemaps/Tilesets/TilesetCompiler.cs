589c4ed1fd706f06983cb2f8a726fa6e83ced232
Adam <adam@adamslair.net>
2016-07-27 20:46:26 +0200

    TilesetCompiler Cleanup #CHANGE: Split up the giant Compile method into multiple smaller methods.

--- DualityPlugins/Tilemaps/Tilesets/TilesetCompiler.cs
+++ DualityPlugins/Tilemaps/Tilesets/TilesetCompiler.cs
@@ -17,6 +17,23 @@ namespace Duality.Plugins.Tilemaps
 	/// </summary>
 	public class TilesetCompiler
 	{
+		private struct LayerGeometry
+		{
+			public int SourceTileCount;
+			public Point2 SourceTileBounds;
+			public int SourceTilesPerRow;
+			public int SourceTilesPerColumn;
+
+			public int TargetTileCount;
+			public Point2 TargetTileBounds;
+			public Point2 TargetTextureSize;
+		}
+		private struct LayerPixelData
+		{
+			public PixelData PixelData;
+			public List<Rect> Atlas;
+		}
+
 		private static readonly TilesetRenderInput DefaultRenderInput = new TilesetRenderInput();
 
 		/// <summary>
@@ -34,109 +51,23 @@ namespace Duality.Plugins.Tilemaps
 			for (int renderInputIndex = 0; renderInputIndex < input.RenderConfig.Count; renderInputIndex++)
 			{
 				TilesetRenderInput renderInput = input.RenderConfig[renderInputIndex] ?? DefaultRenderInput;
-				PixelData renderSourceData = (renderInput.SourceData.Res ?? Pixmap.Checkerboard.Res).MainLayer;
+				PixelData sourceLayerData = (renderInput.SourceData.Res ?? Pixmap.Checkerboard.Res).MainLayer;
 
-				// What's the space requirement for each tile?
-				Point2 sourceTileBounds = new Point2(
-					renderInput.SourceTileSize.X + renderInput.SourceTileSpacing * 2, 
-					renderInput.SourceTileSize.Y + renderInput.SourceTileSpacing * 2);
-				Point2 targetTileBounds = new Point2(
-					renderInput.SourceTileSize.X + renderInput.TargetTileSpacing * 2, 
-					renderInput.SourceTileSize.Y + renderInput.TargetTileSpacing * 2);
+				// Determine overal geometry values for this layer, such as tile bounds and texture sizes
+				LayerGeometry layerGeometry = this.CalculateLayerGeometry(renderInput, sourceLayerData);
+				minSourceTileCount = Math.Min(minSourceTileCount, layerGeometry.SourceTileCount);
 
-				// How many tiles will we have?
-				int sourceHorizontalCount = renderSourceData.Width / sourceTileBounds.X;
-				int sourceVerticalCount = renderSourceData.Height / sourceTileBounds.Y;
-				int sourceTileCount = sourceHorizontalCount * sourceVerticalCount;
-				int targetTileCount = sourceTileCount; // ToDo: Account for expanded AutoTiles
-
-				minSourceTileCount = Math.Min(minSourceTileCount, sourceTileCount);
-
-				// What's the optimal texture size to include them all?
-				int targetTextureWidth;
-				int targetTextureHeight;
-				{
-					int minTilesPerLine = MathF.Max(1, (int)MathF.Sqrt(targetTileCount));
-					targetTextureWidth = MathF.NextPowerOfTwo(targetTileBounds.X * minTilesPerLine);
-
-					int actualTilesPerLine = targetTextureWidth / targetTileBounds.X;
-					int requiredLineCount = 1 + (targetTileCount / actualTilesPerLine);
-					targetTextureHeight = MathF.NextPowerOfTwo(targetTileBounds.Y * requiredLineCount);
-				}
-
-				// Create a buffer for writing target pixel data
-				PixelData renderTargetData = new PixelData(targetTextureWidth, targetTextureHeight);
-
-				// Iterate over tiles and move each tile from source to target
-				List<Rect> tileAtlas = new List<Rect>();
-				Point2 targetTilePos = new Point2(0, 0);
-				for (int tileIndex = 0; tileIndex < sourceTileCount; tileIndex++)
-				{
-					// Initialize a new tile info when necessary
-					if (tileIndex >= output.TileData.Count)
-					{
-						output.TileData.Count++;
-						output.TileData.Data[tileIndex].IsVisuallyEmpty = true;
-					}
-
-					// Determine where on the source buffer the tile is located
-					Point2 sourceTilePos = new Point2(
-						sourceTileBounds.X * (tileIndex % sourceHorizontalCount),
-						sourceTileBounds.Y * (tileIndex / sourceHorizontalCount));
-
-					// Draw the source tile onto the target buffer, including its spacing / border
-					Point2 targetContentPos = new Point2(
-						targetTilePos.X + renderInput.TargetTileSpacing, 
-						targetTilePos.Y + renderInput.TargetTileSpacing);
-					renderSourceData.DrawOnto(renderTargetData, 
-						BlendMode.Solid, 
-						targetContentPos.X, 
-						targetContentPos.Y, 
-						renderInput.SourceTileSize.X, 
-						renderInput.SourceTileSize.Y, 
-						sourceTilePos.X + renderInput.SourceTileSpacing, 
-						sourceTilePos.Y + renderInput.SourceTileSpacing);
-
-					// Fill up the target spacing area with similar pixels
-					if (renderInput.TargetTileSpacing > 0)
-					{
-						FillTileSpacing(renderTargetData, renderInput.TargetTileSpacing, targetContentPos, renderInput.SourceTileSize);
-					}
-
-					// Update whether the tile is considered visually empty
-					if (output.TileData.Data[tileIndex].IsVisuallyEmpty)
-					{
-						bool isLayerVisuallyEmpty = IsCompletelyTransparent(
-							renderSourceData, 
-							new Point2(
-								sourceTilePos.X + renderInput.SourceTileSpacing,
-								sourceTilePos.Y + renderInput.SourceTileSpacing),
-							renderInput.SourceTileSize);
-						if (!isLayerVisuallyEmpty)
-							output.TileData.Data[tileIndex].IsVisuallyEmpty = false;
-					}
-
-					// Add an entry to the generated atlas
-					Rect atlasRect = new Rect(
-						targetTilePos.X + renderInput.TargetTileSpacing, 
-						targetTilePos.Y + renderInput.TargetTileSpacing, 
-						targetTileBounds.X - renderInput.TargetTileSpacing * 2, 
-						targetTileBounds.Y - renderInput.TargetTileSpacing * 2);
-					tileAtlas.Add(atlasRect);
-
-					// Advance the target tile position
-					targetTilePos.X += targetTileBounds.X;
-					if (targetTilePos.X + targetTileBounds.X > renderTargetData.Width)
-					{
-						targetTilePos.X = 0;
-						targetTilePos.Y += targetTileBounds.Y;
-					}
-				}
+				// Generate pixel data and atlas values for this layer's texture
+				LayerPixelData targetLayerData = this.GenerateLayerPixelData(
+					renderInput, 
+					sourceLayerData, 
+					layerGeometry, 
+					output.TileData);
 
 				// Create the texture to be used for this rendering input
-				using (Pixmap targetPixmap = new Pixmap(renderTargetData))
+				using (Pixmap targetPixmap = new Pixmap(targetLayerData.PixelData))
 				{
-					targetPixmap.Atlas = tileAtlas;
+					targetPixmap.Atlas = targetLayerData.Atlas;
 					Texture targetTexture = new Texture(
 						targetPixmap, TextureSizeMode.Enlarge, 
 						renderInput.TargetMagFilter, renderInput.TargetMinFilter, 
@@ -148,30 +79,157 @@ namespace Duality.Plugins.Tilemaps
 			}
 
 			// Generate additional per-tile data
-			{
-				// Copy input data
-				for (int i = 0; i < output.TileData.Count; i++)
-				{
-					if (i >= input.TileInput.Count) break;
-					output.TileData.Data[i].DepthOffset = input.TileInput.Data[i].DepthOffset;
-					output.TileData.Data[i].IsVertical  = input.TileInput.Data[i].IsVertical;
-					output.TileData.Data[i].Collision   = input.TileInput.Data[i].Collision;
-				}
-
-				// Retrieve texture atlas data for quick lookup during rendering
-				if (output.RenderData.Count > 0)
-				{
-					for (int i = 0; i < output.TileData.Count; i++)
-					{
-						output.RenderData[0].LookupAtlas(i, out output.TileData.Data[i].TexCoord0);
-					}
-				}
-			}
+			this.TransformTileData(input.TileInput, output.TileData, output.RenderData);
 
 			// Apply global tileset stats
 			output.TileCount = (input.RenderConfig.Count > 0) ? minSourceTileCount : 0;
 
 			return output;
+		}
+
+		/// <summary>
+		/// Determines the overall geometry of a single <see cref="Tileset"/> visual layer. This involves
+		/// tile boundaries in source and target data, as well as texture sizes and similar.
+		/// </summary>
+		/// <param name="renderInput"></param>
+		/// <param name="layerData"></param>
+		/// <returns></returns>
+		private LayerGeometry CalculateLayerGeometry(TilesetRenderInput renderInput, PixelData layerData)
+		{
+			LayerGeometry geometry;
+
+			// What's the space requirement for each tile?
+			geometry.SourceTileBounds = new Point2(
+				renderInput.SourceTileSize.X + renderInput.SourceTileSpacing * 2, 
+				renderInput.SourceTileSize.Y + renderInput.SourceTileSpacing * 2);
+			geometry.TargetTileBounds = new Point2(
+				renderInput.SourceTileSize.X + renderInput.TargetTileSpacing * 2, 
+				renderInput.SourceTileSize.Y + renderInput.TargetTileSpacing * 2);
+
+			// How many tiles will we have?
+			geometry.SourceTilesPerRow = layerData.Width / geometry.SourceTileBounds.X;
+			geometry.SourceTilesPerColumn = layerData.Height / geometry.SourceTileBounds.Y;
+			geometry.SourceTileCount = geometry.SourceTilesPerRow * geometry.SourceTilesPerColumn;
+			geometry.TargetTileCount = geometry.SourceTileCount; // ToDo: Account for expanded AutoTiles
+
+			// What's the optimal texture size to include them all?
+			int minTilesPerLine = MathF.Max(1, (int)MathF.Sqrt(geometry.TargetTileCount));
+			geometry.TargetTextureSize.X = MathF.NextPowerOfTwo(geometry.TargetTileBounds.X * minTilesPerLine);
+
+			int actualTilesPerLine = geometry.TargetTextureSize.X / geometry.TargetTileBounds.X;
+			int requiredLineCount = 1 + (geometry.TargetTileCount / actualTilesPerLine);
+			geometry.TargetTextureSize.Y = MathF.NextPowerOfTwo(geometry.TargetTileBounds.Y * requiredLineCount);
+
+			return geometry;
+		}
+		/// <summary>
+		/// Generates pixel and atlas data for a single <see cref="Tileset"/> visual layer.
+		/// </summary>
+		/// <param name="renderInput"></param>
+		/// <param name="sourceData"></param>
+		/// <param name="geometry"></param>
+		/// <param name="tileData"></param>
+		/// <returns></returns>
+		private LayerPixelData GenerateLayerPixelData(TilesetRenderInput renderInput, PixelData sourceData, LayerGeometry geometry, RawList<TileInfo> tileData)
+		{
+			// Create a buffer for writing target pixel data
+			LayerPixelData target;
+			target.PixelData = new PixelData(geometry.TargetTextureSize.X, geometry.TargetTextureSize.Y);
+			target.Atlas = new List<Rect>();
+
+			// Iterate over tiles and move each tile from source to target
+			Point2 targetTilePos = new Point2(0, 0);
+			for (int tileIndex = 0; tileIndex < geometry.SourceTileCount; tileIndex++)
+			{
+				// Initialize a new tile info when necessary
+				if (tileIndex >= tileData.Count)
+				{
+					tileData.Count++;
+					tileData.Data[tileIndex].IsVisuallyEmpty = true;
+				}
+
+				// Determine where on the source buffer the tile is located
+				Point2 sourceTilePos = new Point2(
+					geometry.SourceTileBounds.X * (tileIndex % geometry.SourceTilesPerRow),
+					geometry.SourceTileBounds.Y * (tileIndex / geometry.SourceTilesPerRow));
+
+				// Draw the source tile onto the target buffer, including its spacing / border
+				Point2 targetContentPos = new Point2(
+					targetTilePos.X + renderInput.TargetTileSpacing, 
+					targetTilePos.Y + renderInput.TargetTileSpacing);
+				sourceData.DrawOnto(target.PixelData, 
+					BlendMode.Solid, 
+					targetContentPos.X, 
+					targetContentPos.Y, 
+					renderInput.SourceTileSize.X, 
+					renderInput.SourceTileSize.Y, 
+					sourceTilePos.X + renderInput.SourceTileSpacing, 
+					sourceTilePos.Y + renderInput.SourceTileSpacing);
+
+				// Fill up the target spacing area with similar pixels
+				if (renderInput.TargetTileSpacing > 0)
+				{
+					FillTileSpacing(target.PixelData, renderInput.TargetTileSpacing, targetContentPos, renderInput.SourceTileSize);
+				}
+
+				// Update whether the tile is considered visually empty
+				if (tileData.Data[tileIndex].IsVisuallyEmpty)
+				{
+					bool isLayerVisuallyEmpty = IsCompletelyTransparent(
+						sourceData, 
+						new Point2(
+							sourceTilePos.X + renderInput.SourceTileSpacing,
+							sourceTilePos.Y + renderInput.SourceTileSpacing),
+						renderInput.SourceTileSize);
+					if (!isLayerVisuallyEmpty)
+						tileData.Data[tileIndex].IsVisuallyEmpty = false;
+				}
+
+				// Add an entry to the generated atlas
+				Rect atlasRect = new Rect(
+					targetTilePos.X + renderInput.TargetTileSpacing, 
+					targetTilePos.Y + renderInput.TargetTileSpacing, 
+					geometry.TargetTileBounds.X - renderInput.TargetTileSpacing * 2, 
+					geometry.TargetTileBounds.Y - renderInput.TargetTileSpacing * 2);
+				target.Atlas.Add(atlasRect);
+
+				// Advance the target tile position
+				targetTilePos.X += geometry.TargetTileBounds.X;
+				if (targetTilePos.X + geometry.TargetTileBounds.X > target.PixelData.Width)
+				{
+					targetTilePos.X = 0;
+					targetTilePos.Y += geometry.TargetTileBounds.Y;
+				}
+			}
+
+			return target;
+		}
+		/// <summary>
+		/// Transforms regular <see cref="TileInput"/> data into compiled <see cref="TileInfo"/> output data
+		/// and enhances it with performance-relevant additional information in the process.
+		/// </summary>
+		/// <param name="tileInput"></param>
+		/// <param name="tileData"></param>
+		/// <param name="renderData"></param>
+		private void TransformTileData(RawList<TileInput> tileInput, RawList<TileInfo> tileData, List<Texture> renderData)
+		{
+			// Copy input data
+			for (int i = 0; i < tileData.Count; i++)
+			{
+				if (i >= tileInput.Count) break;
+				tileData.Data[i].DepthOffset = tileInput.Data[i].DepthOffset;
+				tileData.Data[i].IsVertical  = tileInput.Data[i].IsVertical;
+				tileData.Data[i].Collision   = tileInput.Data[i].Collision;
+			}
+
+			// Retrieve texture atlas data for quick lookup during rendering
+			if (renderData.Count > 0)
+			{
+				for (int i = 0; i < tileData.Count; i++)
+				{
+					renderData[0].LookupAtlas(i, out tileData.Data[i].TexCoord0);
+				}
+			}
 		}
 
 		/// <summary>
