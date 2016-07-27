c7f08a9777226caf524386abc78355fd0688070d
Adam <adam@adamslair.net>
2016-07-27 20:54:20 +0200

    TilesetCompiler Docs #ADD: Added documentation to TilesetCompiler input and output data structures.

--- DualityPlugins/Tilemaps/Tilesets/TilesetCompilerInput.cs
+++ DualityPlugins/Tilemaps/Tilesets/TilesetCompilerInput.cs
@@ -10,11 +10,26 @@ using Duality.Plugins.Tilemaps.Properties;
 
 namespace Duality.Plugins.Tilemaps
 {
+	/// <summary>
+	/// Specifies input data for the <see cref="TilesetCompiler"/> to work with.
+	/// </summary>
 	public struct TilesetCompilerInput
 	{
+		/// <summary>
+		/// Input tile data. This data is not modified, but provided in
+		/// mutable form for performance reasons / to allow direct array access.
+		/// </summary>
 		public RawList<TileInput> TileInput;
+		/// <summary>
+		/// Visual layer configuration. This data is not modified, only read.
+		/// </summary>
 		public IReadOnlyList<TilesetRenderInput> RenderConfig;
 
+		/// <summary>
+		/// (Optional) potentially existing output that was produced in previous compilations.
+		/// A <see cref="TilesetCompiler"/> may choose to re-use some of the data
+		/// structures provided here, so they might be returned as output in modified form.
+		/// </summary>
 		public TilesetCompilerOutput ExistingOutput;
 	}
 }
--- DualityPlugins/Tilemaps/Tilesets/TilesetCompilerOutput.cs
+++ DualityPlugins/Tilemaps/Tilesets/TilesetCompilerOutput.cs
@@ -10,10 +10,24 @@ using Duality.Plugins.Tilemaps.Properties;
 
 namespace Duality.Plugins.Tilemaps
 {
+	/// <summary>
+	/// Contains output data that was produced or modified by a <see cref="TilesetCompiler"/>.
+	/// </summary>
 	public struct TilesetCompilerOutput
 	{
+		/// <summary>
+		/// The overall number of tiles in the compiled tileset.
+		/// </summary>
 		public int TileCount;
+		/// <summary>
+		/// Per-tile information providing collision and rendering cues.
+		/// Corresponds to the tile input that was specified in the <see cref="TilesetCompilerInput"/>.
+		/// </summary>
 		public RawList<TileInfo> TileData;
+		/// <summary>
+		/// One texture for each visual layer. Corresponds to the visual
+		/// layers that were specified in the <see cref="TilesetCompilerInput"/>.
+		/// </summary>
 		public List<Texture> RenderData;
 	}
 }
