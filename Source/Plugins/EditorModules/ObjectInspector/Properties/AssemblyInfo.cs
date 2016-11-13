b4e91897560a3796aff381f2d37dbff32276057e
Adam <adam@adamslair.net>
2016-11-13 12:53:26 +0100

    Package Update #CHANGE: Updated Package Specs to AdamsLair.Duality 2.8.3 #CHANGE: Updated Package Specs to AdamsLair.Duality.Backend.DefaultOpenTK 2.0.11 #CHANGE: Updated Package Specs to AdamsLair.Duality.Backend.DotNetFramework 2.1.1 #CHANGE: Updated Package Specs to AdamsLair.Duality.Editor 2.7.7 #CHANGE: Updated Package Specs to AdamsLair.Duality.Editor.Backend.DefaultOpenTK 2.0.4 #CHANGE: Updated Package Specs to AdamsLair.Duality.Editor.Plugins.Base 2.2.4 #CHANGE: Updated Package Specs to AdamsLair.Duality.Editor.Plugins.CamView 2.3.8 #CHANGE: Updated Package Specs to AdamsLair.Duality.Editor.Plugins.DynamicLighting 2.0.2 #CHANGE: Updated Package Specs to AdamsLair.Duality.Editor.Plugins.HelpAdvisor 2.0.4 #CHANGE: Updated Package Specs to AdamsLair.Duality.Editor.Plugins.LogView 2.1.5 #CHANGE: Updated Package Specs to AdamsLair.Duality.Editor.Plugins.ObjectInspector 2.0.5 #CHANGE: Updated Package Specs to AdamsLair.Duality.Editor.Plugins.PackageManagerFrontend 2.0.6 #CHANGE: Updated Package Specs to AdamsLair.Duality.Editor.Plugins.ProjectView 2.0.10 #CHANGE: Updated Package Specs to AdamsLair.Duality.Editor.Plugins.SceneView 2.0.9 #CHANGE: Updated Package Specs to AdamsLair.Duality.Editor.Plugins.Tilemaps 1.1.9 #CHANGE: Updated Package Specs to AdamsLair.Duality.Launcher 2.3.1 #CHANGE: Updated Package Specs to AdamsLair.Duality.Plugins.Compatibility 2.0.2 #CHANGE: Updated Package Specs to AdamsLair.Duality.Plugins.DynamicLighting 2.0.2 #CHANGE: Updated Package Specs to AdamsLair.Duality.Plugins.DynamicLighting.Sample 2.0.1 #CHANGE: Updated Package Specs to AdamsLair.Duality.Plugins.Steering 2.0.1 #CHANGE: Updated Package Specs to AdamsLair.Duality.Plugins.Steering.Sample 2.0.1 #CHANGE: Updated Package Specs to AdamsLair.Duality.Plugins.Tilemaps 1.1.8 #CHANGE: Updated Package Specs to AdamsLair.Duality.Plugins.Tilemaps.Sample 1.1.11 #CHANGE: Updated Package Specs to AdamsLair.Duality.Primitives 2.0.1 #CHANGE: Updated Package Specs to AdamsLair.Duality.Samples.AudioHandling 2.0.1 #CHANGE: Updated Package Specs to AdamsLair.Duality.Samples.BasicMenu 2.0.3 #CHANGE: Updated Package Specs to AdamsLair.Duality.Samples.BasicShaders 2.0.1 #CHANGE: Updated Package Specs to AdamsLair.Duality.Samples.CameraController 2.0.1 #CHANGE: Updated Package Specs to AdamsLair.Duality.Samples.DualStickSpaceShooter 2.0.2 #CHANGE: Updated Package Specs to AdamsLair.Duality.Samples.FlapOrDie 2.0.2 #CHANGE: Updated Package Specs to AdamsLair.Duality.Samples.InputHandling 2.0.2 #CHANGE: Updated Package Specs to AdamsLair.Duality.Samples.ParticleSystem 2.0.1

--- Source/Plugins/EditorModules/ObjectInspector/Properties/AssemblyInfo.cs
+++ Source/Plugins/EditorModules/ObjectInspector/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 //
 // Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
 // übernehmen, indem Sie "*" eingeben:
-// [assembly: AssemblyVersion("2.0.4")]
-[assembly: AssemblyVersion("2.0.4")]
+// [assembly: AssemblyVersion("2.0.5")]
+[assembly: AssemblyVersion("2.0.5")]
 
