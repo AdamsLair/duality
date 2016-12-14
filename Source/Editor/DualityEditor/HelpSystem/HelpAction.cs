8e53ba1374a32ece3fe520a7a007c0a575f1d556
Adam <adam@adamslair.net>
2016-12-14 21:32:49 +0100

    Split HelpSystem.cs #CHANGE: Split up file HelpInfo.cs into multiple files to follow one-class-per-file rule.

---
+++ Source/Editor/DualityEditor/HelpSystem/HelpAction.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Duality;
+
+namespace Duality.Editor
+{
+	public delegate bool HelpAction(HelpInfo info);
+}
---
+++ Source/Editor/DualityEditor/HelpSystem/IHelpProvider.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Drawing;
+
+using Duality;
+
+namespace Duality.Editor
+{
+	public interface IHelpProvider
+	{
+		HelpInfo ProvideHoverHelp(Point localPos, ref bool captured);
+	}
+}
