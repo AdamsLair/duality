9da515b80d8c98a07044e2a721d4252504cafba7
Adam <adam@adamslair.net>
2016-12-18 17:27:26 +0100

    SpriteIndexBlend PropertyEditor #ADD: Added a SpriteIndexBlend PropertyEditor that displays current, next and blend in a horizontal layout.

---
+++ Source/Editor/DualityEditor/Controls/PropertyEditors/SpriteIndexBlendPropertyEditor.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Linq;
+
+using AdamsLair.WinForms.PropertyEditing.Templates;
+
+using Duality;
+using Duality.Drawing;
+
+namespace Duality.Editor.Controls.PropertyEditors
+{
+	[PropertyEditorAssignment(typeof(SpriteIndexBlend))]
+	public class SpriteIndexBlendPropertyEditor : VectorPropertyEditor
+	{
+		public override object DisplayedValue
+		{
+			get 
+			{ 
+				return new SpriteIndexBlend((int)this.editor[0].Value, (int)this.editor[2].Value, (float)this.editor[1].Value);
+			}
+		}
+
+
+		public SpriteIndexBlendPropertyEditor() : base(3, 1)
+		{
+			this.editor[0].Edited += this.editorCurrent_Edited;
+			this.editor[1].Edited += this.editorBlend_Edited;
+			this.editor[2].Edited += this.editorNext_Edited;
+		}
+
+
+		protected override void OnGetValue()
+		{
+			base.OnGetValue();
+			object[] values = this.GetValue().ToArray();
+
+			this.BeginUpdate();
+			if (!values.Any())
+			{
+				this.editor[0].Value = 0;
+				this.editor[1].Value = 0;
+				this.editor[2].Value = 0;
+			}
+			else
+			{
+				var valNotNull = values.NotNull();
+				int avgCurrent = (int)Math.Round(valNotNull.Average(o => ((SpriteIndexBlend)o).Current));
+				int avgNext = (int)Math.Round(valNotNull.Average(o => ((SpriteIndexBlend)o).Next));
+				float avgBlend = valNotNull.Average(o => ((SpriteIndexBlend)o).Blend);
+
+				this.editor[0].Value = MathF.SafeToDecimal(avgCurrent);
+				this.editor[1].Value = MathF.SafeToDecimal(avgBlend);
+				this.editor[2].Value = MathF.SafeToDecimal(avgNext);
+
+				this.multiple[0] = (values.Any(o => o == null) || values.Any(o => ((SpriteIndexBlend)o).Current != avgCurrent));
+				this.multiple[1] = (values.Any(o => o == null) || values.Any(o => ((SpriteIndexBlend)o).Blend != avgBlend));
+				this.multiple[2] = (values.Any(o => o == null) || values.Any(o => ((SpriteIndexBlend)o).Next != avgNext));
+			}
+			this.EndUpdate();
+		}
+		protected override void ApplyDefaultSubEditorConfig(NumericEditorTemplate subEditor)
+		{
+			base.ApplyDefaultSubEditorConfig(subEditor);
+			if (subEditor == this.editor[1])
+			{
+				subEditor.DecimalPlaces = 2;
+				subEditor.Increment = 0.1m;
+			}
+			else
+			{
+				subEditor.DecimalPlaces = 0;
+				subEditor.Increment = 1;
+				subEditor.Minimum = -1;
+			}
+		}
+
+		private void editorCurrent_Edited(object sender, EventArgs e)
+		{
+			if (this.IsUpdating) return;
+			if (this.Disposed) return;
+			if (!this.ReadOnly)
+			{
+				object[] values = this.GetValue().ToArray();
+				SpriteIndexBlend newVal = (SpriteIndexBlend)this.DisplayedValue;
+				for (int i = 0; i < values.Length; i++)
+				{
+					if (values[i] == null)
+						values[i] = this.DisplayedValue;
+					else
+					{
+						SpriteIndexBlend oldVal = (SpriteIndexBlend)values[i];
+						values[i] = new SpriteIndexBlend(newVal.Current, oldVal.Next, oldVal.Blend);
+					}
+				}
+				this.SetValues(values);
+			}
+			this.PerformGetValue();
+		}
+		private void editorBlend_Edited(object sender, EventArgs e)
+		{
+			if (this.IsUpdating) return;
+			if (this.Disposed) return;
+			if (!this.ReadOnly)
+			{
+				object[] values = this.GetValue().ToArray();
+				SpriteIndexBlend newVal = (SpriteIndexBlend)this.DisplayedValue;
+				for (int i = 0; i < values.Length; i++)
+				{
+					if (values[i] == null)
+						values[i] = this.DisplayedValue;
+					else
+					{
+						SpriteIndexBlend oldVal = (SpriteIndexBlend)values[i];
+						values[i] = new SpriteIndexBlend(oldVal.Current, oldVal.Next, newVal.Blend);
+					}
+				}
+				this.SetValues(values);
+			}
+			this.PerformGetValue();
+		}
+		private void editorNext_Edited(object sender, EventArgs e)
+		{
+			if (this.IsUpdating) return;
+			if (this.Disposed) return;
+			if (!this.ReadOnly)
+			{
+				object[] values = this.GetValue().ToArray();
+				SpriteIndexBlend newVal = (SpriteIndexBlend)this.DisplayedValue;
+				for (int i = 0; i < values.Length; i++)
+				{
+					if (values[i] == null)
+						values[i] = this.DisplayedValue;
+					else
+					{
+						SpriteIndexBlend oldVal = (SpriteIndexBlend)values[i];
+						values[i] = new SpriteIndexBlend(oldVal.Current, newVal.Next, oldVal.Blend);
+					}
+				}
+				this.SetValues(values);
+			}
+			this.PerformGetValue();
+		}
+	}
+}
+
