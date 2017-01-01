b1b1e6a2acfc7c3d49abe6024995d37f6385c108
Adam <adam@adamslair.net>
2017-01-01 21:51:48 +0100

    Cloning Tests #ADD: Added two more cloning tests that check specifically for the occurrence of the previously fixed self-reference bug.

---
+++ Test/Core/Cloning/HelperObjects/SelfReferenceObject.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Reflection;
+
+using Duality;
+using Duality.Cloning;
+using Duality.Drawing;
+using Duality.Resources;
+using Duality.Components;
+using Duality.Components.Renderers;
+
+using NUnit.Framework;
+
+namespace Duality.Tests.Cloning.HelperObjects
+{
+	internal class SelfReferenceObject
+	{
+		private SelfReferenceObject selfRef;
+		public SelfReferenceObject SelfRef
+		{
+			get { return this.selfRef; }
+		}
+
+		public SelfReferenceObject()
+		{
+			this.selfRef = this;
+		}
+	}
+}
