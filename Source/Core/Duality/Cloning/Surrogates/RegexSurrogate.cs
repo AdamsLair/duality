de11777cc1c9cf073e36b4b4fd87097519a87596
Adam <adam@adamslair.net>
2017-01-06 21:07:10 +0100

    Regex Cloning / Serialization Surrogate #ADD: Added both a cloning and a serialization surrogate for Regex.

---
+++ Source/Core/Duality/Cloning/Surrogates/RegexSurrogate.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using System.Text.RegularExpressions;
+
+namespace Duality.Cloning.Surrogates
+{
+	public class RegexSurrogate : CloneSurrogate<Regex>
+	{
+		protected override bool IsImmutableTarget
+		{
+			get { return true; }
+		}
+		public override void CreateTargetObject(Regex source, ref Regex target, ICloneTargetSetup setup)
+		{
+			// Since Regex patterns can't be modified after creation, we'll always
+			// need to set up a new one as part of the copy operation.
+			if (source == null)
+				target = null;
+			else
+				target = new Regex(source.ToString(), source.Options, source.MatchTimeout);
+		}
+		public override void SetupCloneTargets(Regex source, Regex target, ICloneTargetSetup setup)
+		{
+			// Nothing to investigate or set up
+		}
+		public override void CopyDataTo(Regex source, Regex target, ICloneOperation operation)
+		{
+			// Nothing to copy.
+		}
+	}
+}
