c9b90d08d80d6a63c122daea58fe51a9e3ca38fb
Adam <adam@adamslair.net>
2017-01-06 21:35:10 +0100

    CultureInfo Cloning / Serialization Surrogate #ADD: Added both a cloning and a serialization surrogate for CultureInfo.

---
+++ Source/Core/Duality/Cloning/Surrogates/CultureInfoSurrogate.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using System.Globalization;
+
+namespace Duality.Cloning.Surrogates
+{
+	public class CultureInfoSurrogate : CloneSurrogate<CultureInfo>
+	{
+		protected override bool IsImmutableTarget
+		{
+			get { return true; }
+		}
+		public override void CreateTargetObject(CultureInfo source, ref CultureInfo target, ICloneTargetSetup setup)
+		{
+			// We'll just use the predefined Clone method that CultureInfo already provides
+			if (source == null)
+				target = null;
+			else
+				target = source.Clone() as CultureInfo;
+		}
+		public override void SetupCloneTargets(CultureInfo source, CultureInfo target, ICloneTargetSetup setup)
+		{
+			// Nothing to investigate or set up
+		}
+		public override void CopyDataTo(CultureInfo source, CultureInfo target, ICloneOperation operation)
+		{
+			// Nothing to copy.
+		}
+	}
+}
---
+++ Source/Core/Duality/Serialization/Surrogates/CultureInfoSurrogate.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace Duality.Serialization.Surrogates
+{
+	/// <summary>
+	/// De/Serializes a <see cref="Regex"/>.
+	/// </summary>
+	public class CultureInfoSurrogate : SerializeSurrogate<CultureInfo>
+	{
+		public override void WriteConstructorData(IDataWriter writer)
+		{
+			writer.WriteValue("name", this.RealObject.Name);
+		}
+		public override void WriteData(IDataWriter writer) {}
+		public override object ConstructObject(IDataReader reader, TypeInfo objType)
+		{
+			string name;
+			reader.ReadValue("name", out name);
+			try
+			{
+				return new CultureInfo(name);
+			}
+			catch (Exception)
+			{
+				Log.Core.WriteError("Unable to resolve CultureInfo '{0}'. Falling back to invariant culture instead.", name);
+				return CultureInfo.InvariantCulture;
+			}
+		}
+		public override void ReadData(IDataReader reader) {}
+	}
+}
