2d334990f06f59816a06b1f121509a304988095d
Adam <adam@adamslair.net>
2017-01-13 17:08:16 +0100

    TargetResize Utility #ADD: Added TargetResize enum and extension methods to provide a convenient solution to the "fit image to target size" problem.

---
+++ Source/Core/Duality/Utility/Math/TargetResize.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Duality
+{
+	/// <summary>
+	/// Describes how a rectangular object is resized to fit a target size.
+	/// </summary>
+	public enum TargetResize
+	{
+		/// <summary>
+		/// No resize takes place.
+		/// </summary>
+		None,
+		/// <summary>
+		/// The resize will match the object's width and height exactly with the target size.
+		/// </summary>
+		Stretch,
+		/// <summary>
+		/// The resize will scale the object so it fits inside the target rect, while keeping its aspect ratio.
+		/// </summary>
+		Fit,
+		/// <summary>
+		/// The resize will scale the object so the entire target rect fits inside, while keeping its aspect ratio.
+		/// </summary>
+		Fill
+	}
+}
