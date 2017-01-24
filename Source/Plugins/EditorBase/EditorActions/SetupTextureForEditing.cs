1f10b83e478c5e401a3400e2d4f58a5fab392fee
Barsonax <rick-van-dam@outlook.com>
2017-01-24 21:45:55 +0100

    master_488: When creating a new empty texture it is now initialized to 128x128 pixels instead of 0x0 pixels. Also changed the editor hint for the size to be between 1 and int.max.

---
+++ Source/Plugins/EditorBase/EditorActions/SetupTextureForEditing.cs
@@ -0,0 +1,22 @@
+﻿using Duality.Resources;
+
+namespace Duality.Editor.Plugins.Base.EditorActions
+{
+	public class SetupTextureForEditing : EditorSingleAction<Texture>
+	{
+		public override bool CanPerformOn(Texture texture)
+		{
+			return base.CanPerformOn(texture) && texture.PixelWidth == 0 && texture.PixelHeight == 0;
+		}
+
+		public override void Perform(Texture texture)
+		{
+			texture.Size = new Vector2(128, 128);
+		}
+		
+		public override bool MatchesContext(string context)
+		{
+			return context == DualityEditorApp.ActionContextSetupObjectForEditing;
+		}
+	}
+}
