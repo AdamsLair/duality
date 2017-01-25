acb0985e4dfa02f589a31938fad5065869096e22
Adam <adam@adamslair.net>
2017-01-25 17:39:29 +0100

    Package Update #CHANGE: Updated Package Specs to AdamsLair.Duality.Editor.Plugins.Base 2.2.7 #CHANGE: Updated Package Specs to AdamsLair.Duality 2.9.4 #CHANGE: Updated Package Specs to AdamsLair.Duality.Editor 2.7.16 #CHANGE: Updated Package Specs to AdamsLair.Duality.Backend.DefaultOpenTK 2.0.16

--- Source/Editor/DualityEditor/Properties/AssemblyInfo.cs
+++ Source/Editor/DualityEditor/Properties/AssemblyInfo.cs
@@ -32,6 +32,6 @@ using System.Runtime.InteropServices;
 //
 // Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
 // übernehmen, indem Sie "*" eingeben:
-// [assembly: AssemblyVersion("2.7.15")]
-[assembly: AssemblyVersion("2.7.15")]
+// [assembly: AssemblyVersion("2.7.16")]
+[assembly: AssemblyVersion("2.7.16")]
 [assembly: InternalsVisibleTo("DualityEditorTests")]
