95c90e0a4704499b6de95b6855b8d3e19de5a3c8
Adam <adam@adamslair.net>
2017-02-05 13:00:13 +0100

    Package Update #CHANGE: Updated Package Specs to AdamsLair.Duality.Plugins.Tilemaps 1.1.12 #CHANGE: Updated Package Specs to AdamsLair.Duality.Editor.Plugins.Tilemaps 1.1.14 #CHANGE: Updated Package Specs to AdamsLair.Duality.Samples.Tilemaps 1.1.17

--- Source/Plugins/Tilemaps/Editor/Properties/AssemblyInfo.cs
+++ Source/Plugins/Tilemaps/Editor/Properties/AssemblyInfo.cs
@@ -30,6 +30,6 @@ using System.Runtime.InteropServices;
 //
 // Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
 // übernehmen, indem Sie "*" eingeben:
-// [assembly: AssemblyVersion("1.1.13")]
-[assembly: AssemblyVersion("1.1.13")]
+// [assembly: AssemblyVersion("1.1.14")]
+[assembly: AssemblyVersion("1.1.14")]
 
