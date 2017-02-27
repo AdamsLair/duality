75e20aae16dc06246a8616012dbfd210bbc4c3d2
Adam <adam@adamslair.net>
2017-02-27 21:11:37 +0100

    Added ComponentExecutionOrder Tests #ADD: Added a basic framework for future testing of component execution order.

--- Test/Core/Components/ComponentExecutionOrderTest.cs
+++ Test/Core/Components/ComponentExecutionOrderTest.cs
@@ -10,7 +10,7 @@ using Duality.Components;
 using Duality.Serialization;
 
 using Duality.Tests.Components;
-using Duality.Tests.Components.RequirementTest;
+using Duality.Tests.Components.ExecutionOrderTest;
 
 using NUnit.Framework;
 
@@ -26,25 +26,133 @@ namespace Duality.Tests.Components
 
 			int sortIndexA1 = order.GetSortIndex(typeof(TestComponentA1));
 			int sortIndexA2 = order.GetSortIndex(typeof(TestComponentA2));
-			int sortIndexA3 = order.GetSortIndex(typeof(TestComponentA3));
 
 			// Expect no particular order, but consistent results
 			Assert.AreEqual(sortIndexA1, order.GetSortIndex(typeof(TestComponentA1)));
 			Assert.AreEqual(sortIndexA2, order.GetSortIndex(typeof(TestComponentA2)));
-			Assert.AreEqual(sortIndexA3, order.GetSortIndex(typeof(TestComponentA3)));
 
 			// Expect that clearing the type cache does not change any results
 			order.ClearTypeCache();
 			Assert.AreEqual(sortIndexA1, order.GetSortIndex(typeof(TestComponentA1)));
 			Assert.AreEqual(sortIndexA2, order.GetSortIndex(typeof(TestComponentA2)));
-			Assert.AreEqual(sortIndexA3, order.GetSortIndex(typeof(TestComponentA3)));
+		}
+
+		[Test] public void EnforceOrderSceneUpdate()
+		{
+			Assert.Inconclusive("Not yet implemented");
+
+			EventOrderLog eventLog = new EventOrderLog();
+			eventLog.EventFilter = EventType.Update;
+
+			Scene scene = this.GenerateSampleScene(
+				typeof(TestComponentA1), 
+				typeof(TestComponentA2), 
+				typeof(TestComponentA3), 
+				typeof(TestComponentA4), 
+				typeof(TestComponentA5));
+			this.AssignEventLog(scene, eventLog);
+
+			Scene.SwitchTo(scene, true);
+			DualityApp.Update();
+			Scene.SwitchTo(null, true);
+
+			this.AssertEventOrder(eventLog, new ComponentExecutionOrder());
+		}
+
+		private void AssertEventOrder(EventOrderLog eventLog, ComponentExecutionOrder order)
+		{
+			int lastIndex = int.MinValue;
+			foreach (TestComponent component in eventLog.EventOrder)
+			{
+				Type type = component.GetType();
+				int index = order.GetSortIndex(type);
+				Assert.GreaterOrEqual(index, lastIndex);
+				lastIndex = index;
+			}
+		}
+		private void AssignEventLog(Scene scene, EventOrderLog eventLog)
+		{
+			foreach (TestComponent component in scene.FindComponents<TestComponent>())
+			{
+				component.EventLog = eventLog;
+			}
+		}
+		private Scene GenerateSampleScene(params Type[] componentTypes)
+		{
+			Random rnd = new Random(1);
+			Scene scene = new Scene();
+			GameObject[] obj = new GameObject[10];
+			for (int i = 0; i < obj.Length; i++)
+			{
+				obj[i] = new GameObject(string.Format("Object #{0}", i));
+				foreach (Type type in componentTypes.Shuffle(rnd))
+				{
+					obj[i].AddComponent(type);
+				}
+
+				if ((i % 2) == 0)
+					scene.AddObject(obj[i]);
+				else
+					obj[i].Parent = obj[i - 1];
+			}
+			return scene;
 		}
 	}
 
 	namespace ExecutionOrderTest
 	{
-		public class TestComponentA1 : Component { }
-		public class TestComponentA2 : Component { }
-		public class TestComponentA3 : Component { }
+		[Flags]
+		public enum EventType
+		{
+			None = 0x0,
+			Update = 0x1,
+			All = Update
+		}
+		public class EventOrderLog
+		{
+			private EventType eventFilter = EventType.All;
+			private List<TestComponent> eventOrder = new List<TestComponent>();
+
+			public EventType EventFilter
+			{
+				get { return this.eventFilter; }
+				set { this.eventFilter = value; }
+			}
+			public IEnumerable<TestComponent> EventOrder
+			{
+				get { return this.eventOrder; }
+			}
+
+			public void Clear()
+			{
+				this.eventOrder.Clear();
+			}
+			public void Notify(EventType eventType, TestComponent receiver)
+			{
+				if ((this.eventFilter & eventFilter) == EventType.None) return;
+				this.eventOrder.Add(receiver);
+			}
+		}
+		public class TestComponent : Component, ICmpUpdatable
+		{
+			private EventOrderLog eventLog;
+
+			public EventOrderLog EventLog
+			{
+				get { return this.eventLog; }
+				set { this.eventLog = value; }
+			}
+
+			void ICmpUpdatable.OnUpdate()
+			{
+				this.eventLog.Notify(EventType.Update, this);
+			}
+		}
+
+		public class TestComponentA1 : TestComponent { }
+		public class TestComponentA2 : TestComponent { }
+		public class TestComponentA3 : TestComponent { }
+		public class TestComponentA4 : TestComponent { }
+		public class TestComponentA5 : TestComponent { }
 	}
 }
