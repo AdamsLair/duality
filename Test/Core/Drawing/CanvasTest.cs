0a2f66a42c2e2a3b2f8395238780216de27ea991
Adam <adam@adamslair.net>
2017-03-04 09:56:38 +0100

    Tweaked CanvasTest #CHANGE: Disabled mipmap generation in CanvasTest.

--- Test/Core/Drawing/CanvasTest.cs
+++ Test/Core/Drawing/CanvasTest.cs
@@ -260,7 +260,7 @@ namespace Duality.Tests.Drawing
 		{
 			PixelData pixelData;
 
-			using (Texture texture = new Texture(width, height, TextureSizeMode.NonPowerOfTwo))
+			using (Texture texture = new Texture(width, height, TextureSizeMode.NonPowerOfTwo, TextureMagFilter.Nearest, TextureMinFilter.Nearest))
 			using (RenderTarget renderTarget = new RenderTarget(AAQuality.Off, texture))
 			using (DrawDevice device = new DrawDevice())
 			{
