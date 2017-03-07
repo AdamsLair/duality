cdf6570a4b6ebac5bbf8eb8bd9312de9a21a7cc4
Nathan Hold <nathanhold@hotmail.com>
2017-03-07 17:06:30 +1000

    Extended TreeViewAdv
    #ADD: The  AutoExpandTreeView class that extends from TreeViewAdv for use with auto expanding on drag over.

---
+++ Source/Editor/DualityEditor/Controls/AutoExpandTreeView.cs
@@ -0,0 +1,68 @@
+using Aga.Controls.Tree;
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Duality.Editor.Controls
+{
+	public class AutoExpandTreeView : TreeViewAdv
+	{
+		public int DragOverExpandDelay
+		{
+			get;
+			set;
+		}
+
+		private TreeNodeAdv autoExpandNode = null;
+
+		private Timer timer = new Timer();
+
+		private void StartTimer()
+		{
+			if (DragOverExpandDelay > 0)
+			{
+				timer.Stop();
+				timer.Interval = DragOverExpandDelay;
+				timer.Tick += OnTimerTick;
+				timer.Start();
+			}
+		}
+
+		private void StopTimer()
+		{
+			if (DragOverExpandDelay > 0)
+			{
+				if (autoExpandNode != null)
+				{
+					autoExpandNode.Expand();
+				}
+			}
+
+			timer.Stop();
+		}
+
+		private void OnTimerTick(object sender, EventArgs e)
+		{
+			StopTimer();
+		}
+
+		protected override void OnDragOver(DragEventArgs drgevent)
+		{
+			base.OnDragOver(drgevent);
+
+			if (DragOverExpandDelay > 0)
+			{
+
+				Point p = PointToClient(new Point(drgevent.X, drgevent.Y));
+				var node = GetNodeAt(p);
+
+				if (autoExpandNode == null || autoExpandNode != node)
+				{
+					autoExpandNode = node;
+					StartTimer();
+				}
+			}
+
+		}
+	}
+}
