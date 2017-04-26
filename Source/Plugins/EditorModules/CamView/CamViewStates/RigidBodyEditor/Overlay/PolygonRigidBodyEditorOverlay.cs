2fdcb46a6d8eb62f39d705b5d6be1da071dc8728
Raúl Yebra <raul_yebra@hotmail.com>
2017-04-26 19:19:19 +0200

    Multiple vertex selection

--- Source/Plugins/EditorModules/CamView/CamViewStates/RigidBodyEditor/Overlay/PolygonRigidBodyEditorOverlay.cs
+++ Source/Plugins/EditorModules/CamView/CamViewStates/RigidBodyEditor/Overlay/PolygonRigidBodyEditorOverlay.cs
@@ -24,20 +24,22 @@ namespace Duality.Editor.Plugins.CamView.CamViewStates
         {
             public PolyShapeInfo shape;
             public VertexType type;
-            public int id;
+            public int id = -1;
             public Vector2 pos;
         }
 
         private const float SELECTOR_RADIUS = 5f;
 
         private bool rendering = false;
-        private List<Vector2> selectedVertices = new List<Vector2>();
+        private List<VertexInfo> selectedVertices = new List<VertexInfo>();
         private VertexInfo currentVertex = new VertexInfo();
+        private Rect selectionRect = Rect.Empty;
 		
         // This property would be used by PolygonRigidBodyEditorTool to know if it should add or edit a vertex on mouse left click
         public VertexInfo CurrentVertex { get { return currentVertex; } set { currentVertex = value; } }
+        public List<VertexInfo> SelectedVertices { get { return selectedVertices; } set { selectedVertices = value; } }
 
-        public void Draw(RigidBody body, Canvas canvas, Vector3 mousePos)
+        public void Draw(RigidBody body, Canvas canvas, Vector3 mousePos, bool selecting)
         {
             if (body == null) return;
 
@@ -49,51 +51,112 @@ namespace Duality.Editor.Plugins.CamView.CamViewStates
 				{
 					float radius = SELECTOR_RADIUS / MathF.Max(0.0001f, canvas.DrawDevice.GetScaleAtZ(0f));
 
-					if (currentVertex.type == VertexType.Selected)
-					{
-                        canvas.DrawCircle(currentVertex.pos.X, currentVertex.pos.Y, radius * 2f);
-                    }
-					else
-					{
-						currentVertex = new VertexInfo();
-					}
-
 					canvas.PushState();
 					canvas.State.ColorTint = ColorRgba.White;
 
-					IEnumerable<PolyShapeInfo> shapes = body.Shapes.Where(x => x.GetType() == typeof(PolyShapeInfo)).Cast<PolyShapeInfo>();
-					foreach (PolyShapeInfo shape in shapes)
-					{
-						Vector2[] vertices = shape.Vertices;
-						for (int i = 0; i < vertices.Length; i++)
-						{
-							int iNext = i < vertices.Length - 1 ? i + 1 : 0; // This works only if the shape is a closed polygon
-							Vector2 pA = vertices[i];
-							Vector2 pB = vertices[iNext];
+                    // Multiple selection doesn't work like single selection, but we need both to avoid extra cost
+                    if (selecting)
+                    {
+                        if (selectionRect == Rect.Empty)
+                        {
+                            selectionRect.X = mousePos.X;
+                            selectionRect.Y = mousePos.Y;
+                            selectionRect.W = 0f;
+                            selectionRect.H = 0f;
+                        }
+                        else
+                        {
+                            selectionRect.W = mousePos.X - selectionRect.X;
+                            selectionRect.H = mousePos.Y - selectionRect.Y;
+                        }
 
-							canvas.FillCircle(pA.X, pA.Y, radius); // Draw vertex
+                        canvas.DrawRect(selectionRect.X, selectionRect.Y, selectionRect.W, selectionRect.H);
 
-							if (currentVertex.type == VertexType.None) // Try to find a posible action (select or new)
-							{
-								if (MathF.Distance(pA.X, pA.Y, mousePos.X, mousePos.Y) <= radius) // Posible selection point found
-								{
-									currentVertex = new VertexInfo() { shape = shape, type = VertexType.PosibleSelect, id = i, pos = pA };
-									canvas.DrawCircle(pA.X, pA.Y, radius * 2f);
-								}
-								else if (MathF.Distance(pB.X, pB.Y, mousePos.X, mousePos.Y) > radius) // Posible new point found
-								{
-									Vector2 p = MathF.PointLineNearestPoint(mousePos.X, mousePos.Y, pA.X, pA.Y, pB.X, pB.Y);
-									if (MathF.Distance(p.X, p.Y, mousePos.X, mousePos.Y) < radius)
-									{
-										currentVertex = new VertexInfo() { shape = shape, type = VertexType.PosibleNew, id = i, pos = p };
+                        selectedVertices.Clear();
+                    }
+                    else
+                    {
+                        selectionRect = Rect.Empty;
+                    }
 
-										canvas.DrawCircle(p.X, p.Y, radius);
-										canvas.DrawCircle(p.X, p.Y, radius * 2f);
-									}
-								}
-							}
-						}
-					}
+                    if (selecting) // Must find selected vertices
+                    {
+                        currentVertex = new VertexInfo();
+
+                        IEnumerable<PolyShapeInfo> shapes = body.Shapes.Where(x => x.GetType() == typeof(PolyShapeInfo)).Cast<PolyShapeInfo>();
+                        foreach (PolyShapeInfo shape in shapes)
+                        {
+                            Vector2[] vertices = shape.Vertices;
+                            for (int i = 0; i < vertices.Length; i++)
+                            {
+                                int iNext = i < vertices.Length - 1 ? i + 1 : 0; // This works only if the shape is a closed polygon
+                                Vector2 pA = vertices[i];
+
+                                canvas.FillCircle(pA.X, pA.Y, radius); // Draw vertex
+
+                                if (selectionRect.Contains(pA))
+                                {
+                                    selectedVertices.Add(new VertexInfo() { shape = shape, type = VertexType.Selected, id = i, pos = pA });
+
+                                    canvas.DrawCircle(pA.X, pA.Y, radius * 2f); // Draw selected vertex circle
+                                    //canvas.FillCircleSegment(pA.X, pA.Y, radius * 2f, 0f, 0f, MathF.RadAngle360, radius * .5f); // Draw selected vertex circle
+                                }
+                            }
+                        }
+                    }
+                    else if (selectedVertices.Count > 0) // Only must render selected vertices
+                    {
+                        foreach (VertexInfo vertex in selectedVertices)
+                        {
+                            canvas.FillCircle(vertex.pos.X, vertex.pos.Y, radius); // Draw vertex
+                            canvas.DrawCircle(vertex.pos.X, vertex.pos.Y, radius * 2f); // Draw selected vertex circle
+                        }
+                    }
+                    if (!selecting) // Single vertex selection
+                    {
+                        if (currentVertex.type == VertexType.Selected)
+                        {
+                            canvas.DrawCircle(currentVertex.pos.X, currentVertex.pos.Y, radius * 2f); // Draw selected vertex circle (single mode is different that multiple/pinned mode)
+                        }
+                        else
+                        {
+                            currentVertex = new VertexInfo();
+                        }
+
+                        IEnumerable<PolyShapeInfo> shapes = body.Shapes.Where(x => x.GetType() == typeof(PolyShapeInfo)).Cast<PolyShapeInfo>();
+                        foreach (PolyShapeInfo shape in shapes)
+                        {
+                            Vector2[] vertices = shape.Vertices;
+                            for (int i = 0; i < vertices.Length; i++)
+                            {
+                                int iNext = i < vertices.Length - 1 ? i + 1 : 0; // This works only if the shape is a closed polygon
+                                Vector2 pA = vertices[i];
+                                Vector2 pB = vertices[iNext];
+
+                                canvas.FillCircle(pA.X, pA.Y, radius); // Draw vertex
+
+                                if (currentVertex.type == VertexType.None) // Try to find a posible action (select or new)
+                                {
+                                    if (MathF.Distance(pA.X, pA.Y, mousePos.X, mousePos.Y) <= radius) // Posible selection point found
+                                    {
+                                        currentVertex = new VertexInfo() { shape = shape, type = VertexType.PosibleSelect, id = i, pos = pA };
+                                        canvas.DrawCircle(pA.X, pA.Y, radius * 2f);
+                                    }
+                                    else if (MathF.Distance(pB.X, pB.Y, mousePos.X, mousePos.Y) > radius) // Posible new point found
+                                    {
+                                        Vector2 p = MathF.PointLineNearestPoint(mousePos.X, mousePos.Y, pA.X, pA.Y, pB.X, pB.Y);
+                                        if (MathF.Distance(p.X, p.Y, mousePos.X, mousePos.Y) < radius)
+                                        {
+                                            currentVertex = new VertexInfo() { shape = shape, type = VertexType.PosibleNew, id = i, pos = p };
+
+                                            canvas.DrawCircle(p.X, p.Y, radius);
+                                            canvas.DrawCircle(p.X, p.Y, radius * 2f);
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
 
 					canvas.PopState();
 				}
--- Source/Plugins/EditorModules/CamView/CamViewStates/RigidBodyEditor/Tools/VertexRigidBodyEditorTool.cs
+++ Source/Plugins/EditorModules/CamView/CamViewStates/RigidBodyEditor/Tools/VertexRigidBodyEditorTool.cs
@@ -18,6 +18,7 @@ namespace Duality.Editor.Plugins.CamView.CamViewStates
 	{
 		private PolygonRigidBodyEditorOverlay overlay = new PolygonRigidBodyEditorOverlay();
         private Vector3 mousePos;
+        bool selecting = false;
         private Vector2[] originalVertices;
 
         public override string Name
@@ -39,64 +40,101 @@ namespace Duality.Editor.Plugins.CamView.CamViewStates
 		public override int SortOrder
 		{
 			get { return 4; }
-		}
+        }
 
-		public override void BeginAction()
+        public override void OnWorldOverlayDrawcalls(Canvas canvas)
+        {
+            RigidBodyEditorCamViewState env = Environment as RigidBodyEditorCamViewState;
+            Point mousePos = env.View.RenderableControl.PointToClient(Cursor.Position);
+            this.mousePos = canvas.DrawDevice.GetSpaceCoord(new Vector3(mousePos.X, mousePos.Y, 0f));
+            overlay.Draw(base.Environment.ActiveBody, canvas, this.mousePos, selecting);
+        }
+
+        public override void BeginAction()
 		{
-            Vector2[] vertices = overlay.CurrentVertex.shape.Vertices;
-            originalVertices = new Vector2[vertices.Length];
-            for (int i = 0; i < vertices.Length; i++)
+            if (overlay.CurrentVertex.shape != null)
             {
-                this.originalVertices[i] = new Vector2(vertices[i].X, vertices[i].Y);
+                Vector2[] vertices = overlay.CurrentVertex.shape.Vertices;
+                originalVertices = new Vector2[vertices.Length];
+                for (int i = 0; i < vertices.Length; i++)
+                {
+                    this.originalVertices[i] = new Vector2(vertices[i].X, vertices[i].Y);
+                }
             }
 
-            if (overlay.CurrentVertex.type == PolygonRigidBodyEditorOverlay.VertexType.PosibleNew)
+            if (!selecting)
             {
-                List<Vector2> temp = overlay.CurrentVertex.shape.Vertices.ToList();
-                int id = overlay.CurrentVertex.id + 1;
-                temp.Insert(id, overlay.CurrentVertex.pos);
-                overlay.CurrentVertex.shape.Vertices = temp.ToArray();
+                if (overlay.CurrentVertex.type == PolygonRigidBodyEditorOverlay.VertexType.PosibleNew)
+                {
+                    List<Vector2> temp = overlay.CurrentVertex.shape.Vertices.ToList();
+                    int id = overlay.CurrentVertex.id + 1;
+                    temp.Insert(id, overlay.CurrentVertex.pos);
+                    overlay.CurrentVertex.shape.Vertices = temp.ToArray();
 
-                UndoRedoManager.Do(new EditRigidBodyPolyShapeAction(overlay.CurrentVertex.shape, originalVertices));
+                    UndoRedoManager.Do(new EditRigidBodyPolyShapeAction(overlay.CurrentVertex.shape, originalVertices));
 
-                overlay.CurrentVertex.id = id;
-                overlay.CurrentVertex.type = PolygonRigidBodyEditorOverlay.VertexType.Selected;
-            }
-            else if (overlay.CurrentVertex.type == PolygonRigidBodyEditorOverlay.VertexType.PosibleSelect)
-            {
-                overlay.CurrentVertex.type = PolygonRigidBodyEditorOverlay.VertexType.Selected;
+                    overlay.CurrentVertex.id = id;
+                    overlay.CurrentVertex.type = PolygonRigidBodyEditorOverlay.VertexType.Selected;
+
+                    overlay.SelectedVertices.Clear(); // Clear multiple selection
+
+                    return; // If a single vertex selection is found, exit the method
+                }
+                else if (overlay.CurrentVertex.type == PolygonRigidBodyEditorOverlay.VertexType.PosibleSelect)
+                {
+                    overlay.CurrentVertex.type = PolygonRigidBodyEditorOverlay.VertexType.Selected;
+
+                    return; // If a single vertex selection is found, exit the method
+                }
+
+                selecting = true; // If no single vertex selection is found, start multiple selection mode
             }
         }
 		public override void UpdateAction()
 		{
             if (overlay.CurrentVertex.type == PolygonRigidBodyEditorOverlay.VertexType.Selected)
             {
-                overlay.CurrentVertex.shape.Vertices[overlay.CurrentVertex.id] = this.mousePos.Xy;
+                Vector2 origin = new Vector2(overlay.CurrentVertex.shape.Vertices[overlay.CurrentVertex.id].X, overlay.CurrentVertex.shape.Vertices[overlay.CurrentVertex.id].Y);
+                if (overlay.SelectedVertices.Count > 0) // Check if the vertex selected is one of the previously selected (if not, clear the selection)
+                {
+                    if (overlay.SelectedVertices.FirstOrDefault(x => x.pos == overlay.CurrentVertex.pos) == null) overlay.SelectedVertices.Clear();
+                }
+                if (overlay.SelectedVertices.Count > 0)
+                {
+                    Vector2 diff = new Vector2(this.mousePos.X - origin.X, this.mousePos.Y - origin.Y);
+                    overlay.CurrentVertex.pos += diff;
+                    foreach (PolygonRigidBodyEditorOverlay.VertexInfo vertex in overlay.SelectedVertices)
+                    {
+                        vertex.pos += diff;
+                        overlay.CurrentVertex.shape.Vertices[vertex.id] = vertex.pos;
+                    }
+                }
+                else
+                {
+                    overlay.CurrentVertex.shape.Vertices[overlay.CurrentVertex.id] = this.mousePos.Xy;
+                }
             }
         }
-		public override void EndAction()
-        {
-        }
-        
-		public override void OnWorldOverlayDrawcalls(Canvas canvas)
-        {
-            RigidBodyEditorCamViewState env = Environment as RigidBodyEditorCamViewState;
-			Point mousePos = env.View.RenderableControl.PointToClient(Cursor.Position);
-            this.mousePos = canvas.DrawDevice.GetSpaceCoord(new Vector3(mousePos.X, mousePos.Y, 0f));
-            overlay.Draw(base.Environment.ActiveBody, canvas, this.mousePos);
-        }
+        public override void EndAction() { }
 
         public override void OnActionKeyReleased()
         {
             base.OnActionKeyReleased();
 
-            if (overlay.CurrentVertex.type == PolygonRigidBodyEditorOverlay.VertexType.Selected)
+            if (!selecting)
             {
-                overlay.CurrentVertex.shape.Vertices = overlay.CurrentVertex.shape.Vertices;
+                if (overlay.CurrentVertex.type == PolygonRigidBodyEditorOverlay.VertexType.Selected)
+                {
+                    overlay.CurrentVertex.shape.Vertices = overlay.CurrentVertex.shape.Vertices;
 
-                UndoRedoManager.Do(new EditRigidBodyPolyShapeAction(overlay.CurrentVertex.shape, originalVertices));
+                    UndoRedoManager.Do(new EditRigidBodyPolyShapeAction(overlay.CurrentVertex.shape, originalVertices));
 
-                overlay.CurrentVertex = new PolygonRigidBodyEditorOverlay.VertexInfo();
+                    overlay.CurrentVertex = new PolygonRigidBodyEditorOverlay.VertexInfo();
+                }
+            }
+            else
+            {
+                selecting = false;
             }
             this.Environment.EndToolAction(true);
         }
