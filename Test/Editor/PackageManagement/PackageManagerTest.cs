505b4080095b07d4d889f299aff97865125d825f
Adam <adam@adamslair.net>
2017-05-13 19:19:15 +0200

    PackageManager Tests #ADD: Added some basic package manager tests to check for non-existent config files and empty package setups.

---
+++ Test/Editor/PackageManagement/PackageManagerTest.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+using NUnit.Framework;
+
+namespace Duality.Editor.PackageManagement.Tests
+{
+	[TestFixture]
+	public class PackageManagerTest
+	{
+		private const string TestBasePath = "PackageManagerTest";
+		private const string TestClientPath = TestBasePath + "\\Client";
+		private const string TestRepositoryPath = TestBasePath + "\\Repository";
+
+		private PackageManagerEnvironment workEnv;
+
+
+		[SetUp] public void Init()
+		{
+			Directory.CreateDirectory(TestClientPath);
+			Directory.CreateDirectory(TestRepositoryPath);
+
+			this.workEnv = new PackageManagerEnvironment(TestClientPath);
+		}
+		[TearDown] public void Cleanup()
+		{
+			this.workEnv = null;
+
+			Directory.Delete(TestBasePath, true);
+		}
+
+
+		[Test] public void EmptySetup()
+		{
+			PackageSetup setup = new PackageSetup();
+			PackageManager packageManager = new PackageManager(this.workEnv, setup);
+
+			Assert.IsFalse(packageManager.IsPackageSyncRequired);
+			Assert.IsFalse(packageManager.LocalSetup.IsFirstInstall);
+			Assert.IsEmpty(packageManager.LocalSetup.Packages);
+			Assert.AreEqual(1, packageManager.LocalSetup.RepositoryUrls.Count);
+		}
+		[Test] public void NonExistentConfigFile()
+		{
+			PackageManager packageManager = new PackageManager(this.workEnv);
+
+			Assert.IsFalse(packageManager.IsPackageSyncRequired);
+			Assert.IsFalse(packageManager.LocalSetup.IsFirstInstall);
+			Assert.IsEmpty(packageManager.LocalSetup.Packages);
+			Assert.AreEqual(1, packageManager.LocalSetup.RepositoryUrls.Count);
+		}
+	}
+}
