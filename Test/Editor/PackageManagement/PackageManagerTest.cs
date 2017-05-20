9bb66cd44e6a8fa9cfa7229a288350339eea861b
Adam <adam@adamslair.net>
2017-05-20 22:48:43 +0200

    PackageManager Tests WiP #ADD: Added a test for installing a simple package with no dependencies, covering precondition, info retrieval, install invocation, fired events, local setup changes and package update schedule.

--- Test/Editor/PackageManagement/PackageManagerTest.cs
+++ Test/Editor/PackageManagement/PackageManagerTest.cs
@@ -2,6 +2,10 @@
 using System.IO;
 using System.Linq;
 using System.Collections.Generic;
+using System.Xml;
+using System.Xml.Linq;
+
+using Duality.IO;
 
 using NUnit.Framework;
 
@@ -21,6 +25,9 @@ namespace Duality.Editor.PackageManagement.Tests
 
 		[SetUp] public void Init()
 		{
+			if (Directory.Exists(TestBasePath))
+				Directory.Delete(TestBasePath, true);
+
 			Directory.CreateDirectory(TestClientPath);
 			Directory.CreateDirectory(TestRepositoryPath);
 
@@ -95,6 +102,56 @@ namespace Duality.Editor.PackageManagement.Tests
 			Assert.IsNotNull(packageSampleInfo);
 			Assert.AreEqual(packagePluginLatest.Version, packagePluginInfo.Version);
 		}
+		[Test] public void InstallPackage()
+		{
+			PackageName packageName = new PackageName("AdamsLair.Duality.TestPlugin", new Version(1, 0, 0, 0));
+			this.CreateRemoteMockPackage(packageName, new[] { PackageManager.DualityTag, PackageManager.PluginTag });
+
+			PackageManager packageManager = new PackageManager(this.workEnv, this.setup);
+			
+			// Precondition: No packages installed, no apply or sync required
+			Assert.IsFalse(packageManager.IsPackageSyncRequired);
+			Assert.IsEmpty(packageManager.LocalSetup.Packages);
+			Assert.IsFalse(File.Exists(this.workEnv.UpdateFilePath));
+
+			// Retrieve PackageInfo from the mock repository
+			PackageInfo packageInfo = packageManager.QueryPackageInfo(packageName);
+			Assert.IsNotNull(packageInfo);
+			Assert.AreEqual(packageInfo.PackageName, packageName);
+
+			// Install the package while listening for events
+			List<PackageName> installEvents = new List<PackageName>();
+			EventHandler<PackageEventArgs> installedHandler = (sender, args) =>
+			{
+				installEvents.Add(args.PackageName);
+			};
+			packageManager.PackageInstalled += installedHandler;
+			packageManager.InstallPackage(packageInfo);
+			packageManager.PackageInstalled -= installedHandler;
+
+			// Assert that the package was installed properly
+			Assert.AreEqual(1, installEvents.Count);
+			Assert.AreEqual(packageName, installEvents[0]);
+			Assert.AreEqual(1, packageManager.LocalSetup.Packages.Count);
+			Assert.AreEqual(packageName, packageManager.LocalSetup.Packages[0].PackageName);
+			Assert.IsTrue(packageManager.LocalSetup.Packages[0].IsInstallationComplete);
+
+			// Assert that there is an apply script now
+			Assert.IsTrue(File.Exists(this.workEnv.UpdateFilePath));
+
+			// Load the apply script and assert its contents match the expected
+			PackageUpdateSchedule applyScript = PackageUpdateSchedule.Load(this.workEnv.UpdateFilePath);
+			List<XElement> updateItems = applyScript.Items.ToList();
+			Assert.AreEqual(2, updateItems.Count);
+
+			// Note that NuGet by default does not recognize lib subfolders during installation.
+			// The files will be packaged with subfolders, but their EffectivePath won't include
+			// them - which is why, unless Duality addresses this at some point in the future, 
+			// all plugins will end up in the Plugins root folder, regardless of their previous
+			// hierarchy.
+			this.AssertUpdateScheduleCopyItem(updateItems, packageName, "lib\\Foo.dll", "Plugins\\Foo.dll");
+			this.AssertUpdateScheduleCopyItem(updateItems, packageName, "lib\\Subfolder\\Bar.dll", "Plugins\\Bar.dll");
+		}
 
 		private void CreateRemoteMockPackage(PackageName name, IEnumerable<string> tags = null, IEnumerable<PackageName> dependencies = null)
 		{
@@ -123,9 +180,9 @@ namespace Duality.Editor.PackageManagement.Tests
 
 			List<NuGet.ManifestFile> files = new List<NuGet.ManifestFile>();
 			files.Add(new NuGet.ManifestFile { Source = "Foo.dll", Target = "lib" });
-			files.Add(new NuGet.ManifestFile { Source = "Subfolder/Bar.dll", Target = "lib/Subfolder" });
+			files.Add(new NuGet.ManifestFile { Source = "Subfolder\\Bar.dll", Target = "lib\\Subfolder" });
 			this.CreateMockPackageFile("Foo.dll");
-			this.CreateMockPackageFile("Subfolder/Bar.dll");
+			this.CreateMockPackageFile("Subfolder\\Bar.dll");
 
 			builder.PopulateFiles(TestPackageBuildPath, files);
 			builder.Populate(metadata);
@@ -144,6 +201,39 @@ namespace Duality.Editor.PackageManagement.Tests
 			string directory = Path.GetDirectoryName(filePath);
 			Directory.CreateDirectory(directory);
 			File.WriteAllText(filePath, "Mock Content");
+		}
+
+		private void AssertUpdateScheduleCopyItem(IEnumerable<XElement> items, PackageName package, string source, string target)
+		{
+			string sourceAbs = Path.Combine(this.workEnv.RepositoryPath, package.Id + "." + package.Version, source);
+			string targetAbs = Path.Combine(this.workEnv.RootPath, target);
+
+			foreach (XElement item in items)
+			{
+				if (item.Name != PackageUpdateSchedule.CopyItem) continue;
+				if (!PathOp.ArePathsEqual(item.GetAttributeValue("source"), sourceAbs)) continue;
+
+				string itemTarget = item.GetAttributeValue("target");
+				Assert.IsTrue(
+					PathOp.ArePathsEqual(itemTarget, targetAbs), 
+					"Found copy instruction with the expected source, but the target differs." + Environment.NewLine +
+					"  Matching source: '{0}'" + Environment.NewLine +
+					"  Expected target: '{1}'" + Environment.NewLine +
+					"  Actual target:   '{2}'",
+					sourceAbs,
+					targetAbs,
+					itemTarget);
+				return;
+			}
+
+			Assert.Fail(
+				"Expected copy instruction, but found no matching item." + Environment.NewLine +
+				"  {0} update schedule items" + Environment.NewLine +
+				"  Expected source: '{1}'" + Environment.NewLine +
+				"  Expected target: '{2}'",
+				items.Count(),
+				sourceAbs,
+				targetAbs);
 		}
 	}
 }
