0db9a0c03496a20a44ecb6ffd8a0d1792c0d920a
Adam <adam@adamslair.net>
2017-05-23 18:35:26 +0200

    LocalPackage Docs #ADD: Added XML comments to LocalPackage.

--- Source/Editor/DualityEditor/PackageManagement/LocalPackage.cs
+++ Source/Editor/DualityEditor/PackageManagement/LocalPackage.cs
@@ -5,24 +5,43 @@ using System.Text;
 
 namespace Duality.Editor.PackageManagement
 {
+	/// <summary>
+	/// Describes a Duality package that is part of the local <see cref="PackageSetup"/>.
+	/// </summary>
 	public sealed class LocalPackage
 	{
-		private	PackageName	packageName	= PackageName.None;
-		private	PackageInfo	info		= null;
+		private PackageName packageName = PackageName.None;
+		private PackageInfo info        = null;
 
 		
+		/// <summary>
+		/// [GET] The name of the package, including ID and version number.
+		/// Note that a local package may be version-invariant to flag itself
+		/// for an update to the newest available version during package verify.
+		/// </summary>
 		public PackageName PackageName
 		{
 			get { return this.packageName; }
 		}
+		/// <summary>
+		/// [GET] The ID of the package.
+		/// </summary>
 		public string Id
 		{
 			get { return this.packageName.Id; }
 		}
+		/// <summary>
+		/// [GET] The version of the package. Can be null, in which case the
+		/// package will be treated as version-invariant.
+		/// </summary>
 		public Version Version
 		{
 			get { return this.packageName.Version; }
 		}
+		/// <summary>
+		/// [GET] A <see cref="PackageInfo"/> representing the locally installed
+		/// version of the package.
+		/// </summary>
 		public PackageInfo Info
 		{
 			get { return this.info; }
