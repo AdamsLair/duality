06885754f87ecda9b051f7e9a244719ecfeb4edf
YMRYMR <raul_yebra@hotmail.com>
2017-06-17 20:25:13 +0200

    RigidBody Editor Vertex Editing
    #ADD: Added a new RigidBody Editor tool that allows to edit shapes via world space mouse interaction after they have been created.
    #CHANGE: Extended the IRigidBodyEditorToolEnvironment and RigidBodyEditorTool API to account for advanced tools that do overlay rendering.

--- Source/Plugins/EditorModules/CamView/CamViewStates/RigidBodyEditor/Tools/NoRigidBodyEditorTool.cs
+++ Source/Plugins/EditorModules/CamView/CamViewStates/RigidBodyEditor/Tools/NoRigidBodyEditorTool.cs
@@ -4,25 +4,33 @@ using System.Drawing;
 using System.Linq;
 using System.Windows.Forms;
 
+using Duality.Editor.Plugins.CamView.Properties;
+using Duality.Editor.Properties;
+
 
 namespace Duality.Editor.Plugins.CamView.CamViewStates
 {
 	/// <summary>
-	/// A dummy tool that represents not performing any operation on the edited <see cref="Duality.Components.Physics.RigidBody"/>.
+	/// A tool that allows you to select and edit <see cref="Duality.Components.Physics.ShapeInfo"/> objects using
+	/// the regular move / rotate / scale actions that you already know from the Scene Editor.
 	/// </summary>
 	public class NoRigidBodyEditorTool : RigidBodyEditorTool
 	{
 		public override string Name
 		{
-			get { return null; }
+			get { return CamViewRes.RigidBodyCamViewState_ItemName_EditShapes; }
 		}
 		public override Image Icon
 		{
-			get { return null; }
+			get { return CamViewResCache.IconShapeSelect; }
 		}
 		public override Cursor ActionCursor
 		{
 			get { return CursorHelper.Arrow; }
+		}
+		public override int SortOrder
+		{
+			get { return -1000; }
 		}
 	}
 }
---
+++ Source/Plugins/EditorModules/CamView/UndoRedoActions/EditRigidBodyPolyShapeAction.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Duality;
+using Duality.Cloning;
+using Duality.Components.Physics;
+using Duality.Editor;
+using Duality.Editor.Plugins.CamView.Properties;
+
+
+namespace Duality.Editor.Plugins.CamView.UndoRedoActions
+{
+	public class EditRigidBodyPolyShapeAction : UndoRedoAction
+	{
+		private Vector2[] originalVertices = null;
+		private Vector2[] newVertices      = null;
+		private ShapeInfo targetShape      = null;
+		private bool      sameVertices     = false;
+
+		public override string Name
+		{
+			get
+			{
+				if (originalVertices.Length == newVertices.Length)
+					return CamViewRes.UndoRedo_MoveRigidBodyShapeVertex;
+				else if (originalVertices.Length > newVertices.Length)
+					return CamViewRes.UndoRedo_DeleteRigidBodyShapeVertex;
+				else 
+					return CamViewRes.UndoRedo_CreateRigidBodyShapeVertex;
+			}
+		}
+		public override bool IsVoid
+		{
+			get { return this.sameVertices; }
+		}
+
+		public EditRigidBodyPolyShapeAction(ShapeInfo shape, Vector2[] originalVertices, Vector2[] newVertices)
+		{
+			this.targetShape = shape;
+			this.originalVertices = (Vector2[])originalVertices.Clone();
+			this.newVertices = (Vector2[])newVertices.Clone();
+			this.sameVertices = this.AreVerticesEqual(this.originalVertices, this.newVertices);
+		}
+
+		public override void Do()
+		{
+			Vector2[] temp = (Vector2[])this.newVertices.Clone();
+			this.SetVertices(this.targetShape, temp);
+            
+			DualityEditorApp.NotifyObjPropChanged(
+				this, 
+				new ObjectSelection(targetShape.Parent), 
+				ReflectionInfo.Property_RigidBody_Shapes);
+		}
+		public override void Undo()
+		{
+			Vector2[] temp = (Vector2[])this.originalVertices.Clone();
+			this.SetVertices(this.targetShape, temp);
+
+			DualityEditorApp.NotifyObjPropChanged(
+				this, 
+				new ObjectSelection(targetShape.Parent), 
+				ReflectionInfo.Property_RigidBody_Shapes);
+		}
+		
+		private void SetVertices(ShapeInfo shapeInfo, Vector2[] vertices)
+		{
+			if (shapeInfo is PolyShapeInfo)
+				(shapeInfo as PolyShapeInfo).Vertices = vertices;
+			else if (shapeInfo is LoopShapeInfo)
+				(shapeInfo as LoopShapeInfo).Vertices = vertices;
+			else if (shapeInfo is ChainShapeInfo)
+				(shapeInfo as ChainShapeInfo).Vertices = vertices;
+			else
+				throw new NotImplementedException();
+		}
+
+		private bool AreVerticesEqual(Vector2[] a, Vector2[] b)
+		{
+			if (a == b) return true;
+			if (a == null || b == null) return false;
+			if (a.Length != b.Length) return false;
+			
+			for (int i = 0; i < a.Length; i++)
+			{
+				if (a[i] != b[i]) return false;
+			}
+
+			return true;
+		}
+	}
+}
