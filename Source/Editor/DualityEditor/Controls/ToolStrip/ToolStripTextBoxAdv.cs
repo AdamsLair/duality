f7588ac0b5e5ea4c81623019e9ce67c2ae8db9e8
Adam <adam@adamslair.net>
2017-06-21 22:08:36 +0200

    GameView Output Size WiP #ADD: Added a custom toolstrip text box that allows to specify proper padding and height and adjusts focus handling. #ADD: Added a WiP toolstrip to the Game View CamViewState.

---
+++ Source/Editor/DualityEditor/Controls/ToolStrip/ToolStripTextBoxAdv.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Windows.Forms.Design;
+using System.Drawing;
+using System.ComponentModel;
+
+namespace Duality.Editor.Controls.ToolStrip
+{
+	[ToolStripItemDesignerAvailability(ToolStripItemDesignerAvailability.ToolStrip | ToolStripItemDesignerAvailability.StatusStrip)]
+	public class ToolStripTextBoxAdv : ToolStripControlHost
+	{
+		private Panel container = null;
+		private TextBox textBox = null;
+		private bool selectAllOnClick = false;
+
+		public event EventHandler ProceedRequested = null;
+		public event EventHandler ValueChanged
+		{
+			add { this.textBox.TextChanged += value; }
+			remove { this.textBox.TextChanged -= value; }
+		}
+
+		public override string Text
+		{
+			get { return this.textBox.Text; }
+			set { this.textBox.Text = value; }
+		}
+		public override Color BackColor
+		{
+			get { return this.container.BackColor; }
+			set
+			{
+				this.container.BackColor = value;
+				this.textBox.BackColor = value;
+			}
+		}
+		public override bool Focused
+		{
+			get { return this.textBox.Focused; }
+		}
+		public int MaxLength
+		{
+			get { return this.textBox.MaxLength; }
+			set { this.textBox.MaxLength = value; }
+		}
+
+		public ToolStripTextBoxAdv(string name) : base(new Panel(), name)
+		{
+			this.container = (Panel)this.Control;
+			this.container.Padding = new Padding(0, 2, 0, 1);
+			this.container.Height = 20;
+			this.container.BorderStyle = BorderStyle.FixedSingle;
+			this.container.BackColor = this.BackColor;
+
+			this.textBox = new TextBox();
+			this.textBox.Dock = DockStyle.Fill;
+			this.textBox.BorderStyle = BorderStyle.None;
+			this.textBox.TextAlign = HorizontalAlignment.Center;
+			this.textBox.BackColor = this.BackColor;
+			this.textBox.Multiline = false;
+			this.textBox.GotFocus += this.textBox_GotFocus;
+			this.textBox.MouseClick += this.textBox_MouseClick;
+			this.textBox.KeyUp += this.textBox_KeyUp;
+			this.textBox.KeyDown += this.textBox_KeyDown;
+
+			this.container.Controls.Add(this.textBox);
+		}
+
+		private bool IsProceedKey(Keys keyCode)
+		{
+			return
+				keyCode == Keys.Return || 
+				keyCode == Keys.Enter ||
+				keyCode == Keys.Tab;
+		}
+		private bool IsKeyInputExceedingLength()
+		{
+			return 
+				this.textBox.SelectionLength == 0 &&
+				this.textBox.TextLength >= this.textBox.MaxLength;
+		}
+
+		protected override void OnGotFocus(EventArgs e)
+		{
+			base.OnGotFocus(e);
+			this.textBox.Focus();
+		}
+
+		private void textBox_MouseClick(object sender, MouseEventArgs e)
+		{
+			if (this.selectAllOnClick)
+			{
+				this.textBox.SelectAll();
+				this.selectAllOnClick = false;
+			}
+		}
+		private void textBox_GotFocus(object sender, EventArgs e)
+		{
+			this.textBox.SelectAll();
+			this.selectAllOnClick = true;
+		}
+		private void textBox_KeyUp(object sender, KeyEventArgs e)
+		{
+			if (this.IsProceedKey(e.KeyCode) || this.IsKeyInputExceedingLength())
+			{
+				e.Handled = true;
+				e.SuppressKeyPress = true;
+
+				if (this.ProceedRequested != null)
+					this.ProceedRequested(this, EventArgs.Empty);
+			}
+		}
+		private void textBox_KeyDown(object sender, KeyEventArgs e)
+		{
+			if (this.IsProceedKey(e.KeyCode) || this.IsKeyInputExceedingLength())
+			{
+				e.Handled = true;
+				e.SuppressKeyPress = true;
+			}
+		}
+
+	}
+}
--- Source/Plugins/EditorModules/CamView/CamViewStates/GameViewCamViewState.cs
+++ Source/Plugins/EditorModules/CamView/CamViewStates/GameViewCamViewState.cs
@@ -1,9 +1,16 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Drawing;
 
 using Duality;
 using Duality.Components;
 using Duality.Resources;
 using Duality.Drawing;
+
+using Duality.Editor.Controls.ToolStrip;
+
 
 namespace Duality.Editor.Plugins.CamView.CamViewStates
 {
@@ -13,10 +20,16 @@ namespace Duality.Editor.Plugins.CamView.CamViewStates
 	/// </summary>
 	public class GameViewCamViewState : CamViewState
 	{
+		private ToolStrip toolstrip = null;
+		private ToolStripTextBoxAdv textBoxRenderWidth = null;
+		private ToolStripTextBoxAdv textBoxRenderHeight = null;
+
+
 		public override string StateName
 		{
 			get { return Properties.CamViewRes.CamViewState_GameView_Name; }
 		}
+
 
 		public GameViewCamViewState()
 		{
@@ -24,15 +37,61 @@ namespace Duality.Editor.Plugins.CamView.CamViewStates
 			this.EngineUserInput = true;
 		}
 
+		private void AddToolbar()
+		{
+			this.View.SuspendLayout();
+			this.toolstrip = new ToolStrip();
+			this.toolstrip.SuspendLayout();
+
+			this.toolstrip.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
+			this.toolstrip.Name = "toolstrip";
+			this.toolstrip.Text = "GameView Tools";
+			this.toolstrip.Renderer = new Duality.Editor.Controls.ToolStrip.DualitorToolStripProfessionalRenderer();
+			this.toolstrip.BackColor = Color.FromArgb(212, 212, 212);
+
+			this.textBoxRenderWidth = new ToolStripTextBoxAdv("textBoxRenderWidth");
+			this.textBoxRenderWidth.BackColor = Color.FromArgb(196, 196, 196);
+			this.textBoxRenderWidth.AutoSize = false;
+			this.textBoxRenderWidth.Width = 35;
+			this.textBoxRenderWidth.MaxLength = 4;
+			this.textBoxRenderWidth.ProceedRequested += (sender, e) => this.textBoxRenderHeight.Focus();
+
+			this.textBoxRenderHeight = new ToolStripTextBoxAdv("textBoxRenderHeight");
+			this.textBoxRenderHeight.BackColor = Color.FromArgb(196, 196, 196);
+			this.textBoxRenderHeight.AutoSize = false;
+			this.textBoxRenderHeight.Width = 35;
+			this.textBoxRenderHeight.MaxLength = 4;
+			this.textBoxRenderHeight.ProceedRequested += (sender, e) => this.textBoxRenderWidth.Focus();
+
+			this.toolstrip.Items.Add(new ToolStripLabel("Screen Size "));
+			this.toolstrip.Items.Add(this.textBoxRenderWidth);
+			this.toolstrip.Items.Add(new ToolStripLabel("x"));
+			this.toolstrip.Items.Add(this.textBoxRenderHeight);
+
+			this.View.Controls.Add(this.toolstrip);
+			this.View.Controls.SetChildIndex(this.toolstrip, this.View.Controls.IndexOf(this.View.ToolbarCamera));
+			this.toolstrip.ResumeLayout(true);
+			this.View.ResumeLayout(true);
+		}
+		private void RemoveToolbar()
+		{
+			this.toolstrip.Dispose();
+			this.toolstrip = null;
+			this.textBoxRenderWidth = null;
+			this.textBoxRenderHeight = null;
+		}
+
 		protected internal override void OnEnterState()
 		{
 			base.OnEnterState();
 			this.View.SetToolbarCamSettingsEnabled(false);
 			this.CameraObj.Active = false;
+			this.AddToolbar();
 		}
 		protected internal override void OnLeaveState()
 		{
 			base.OnLeaveState();
+			this.RemoveToolbar();
 			this.View.SetToolbarCamSettingsEnabled(true);
 			this.CameraObj.Active = true;
 		}
