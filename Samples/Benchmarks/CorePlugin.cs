4c69360e66795b19261539058e14795118f186a8
Adam <adam@adamslair.net>
2017-07-10 18:38:10 +0200

    Benchmarks Project Setup #ADD: Set up a benchmarks sample package project, empty and yet to be filled.

---
+++ Samples/Benchmarks/CorePlugin.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Duality;
+
+namespace Duality.Samples.Benchmarks
+{
+    public class BenchmarksSampleCorePlugin : CorePlugin {}
+}
