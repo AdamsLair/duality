9a6c351fbaf0f7af2ce9d368d0d09342c82a122e
Adam <adam@adamslair.net>
2017-08-05 19:02:00 +0200

    Package Update #CHANGE: Updated Package Specs to AdamsLair.Duality 2.12.13

--- Source/Core/Duality/Properties/AssemblyInfo.cs
+++ Source/Core/Duality/Properties/AssemblyInfo.cs
@@ -32,7 +32,7 @@ using System.Runtime.InteropServices;
 //
 // Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
 // übernehmen, indem Sie "*" eingeben:
-// [assembly: AssemblyVersion("2.12.12")]
-[assembly: AssemblyVersion("2.12.12")]
+// [assembly: AssemblyVersion("2.12.13")]
+[assembly: AssemblyVersion("2.12.13")]
 [assembly: InternalsVisibleTo("DualityEditor")]
 [assembly: InternalsVisibleTo("DualityTests")]
