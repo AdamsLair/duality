bc99c3f1b23dc43485c4136e4564550cea6cd746
Adam <adam@adamslair.net>
2017-08-31 17:12:08 +0200

    Added VertexSegment Type #ADD: Added a first implementation of VertexSegment<T>, a wrapper struct for addressing a subsection of a shared vertex array. Its main priority is performance over safety, so any internal validation code is inactive in releases. #ADD: Added a test for construction and indexing VertexSegment<T> data.

---
+++ Source/Core/Duality/Drawing/VertexData/VertexSegment.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace Duality.Drawing
+{
+	/// <summary>
+	/// Defines a writable segment in a shared vertex array. This type sacrifices
+	/// validation and parameter checking for some extra performance - when specifying
+	/// parameters, reading and writing data, do so with the same caution that you
+	/// would apply to unsafe code.
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	public struct VertexSegment<T> where T : struct, IVertexData
+	{
+		/// <summary>
+		/// The shared vertex array that is addressed in this segment.
+		/// </summary>
+		public T[] Data;
+		/// <summary>
+		/// The index offset to the beginning of <see cref="Data"/> at which this segments starts.
+		/// </summary>
+		public int Offset;
+		/// <summary>
+		/// The length of this segment.
+		/// </summary>
+		public int Length;
+
+		/// <summary>
+		/// Addresses the underlying <see cref="Data"/> starting at the <see cref="Offset"/> of this segment.
+		/// </summary>
+		/// <param name="index"></param>
+		/// <returns></returns>
+		public T this[int index]
+		{
+			// No range checking for performance reasons.
+			// Using a throw helper allows inlining, but
+			// not checking at all reduces instructions from
+			// ~16 to ~11.
+			get
+			{
+#if DEBUG
+				if (index < 0 || index >= this.Length)
+					throw new IndexOutOfRangeException();
+#endif
+				return this.Data[this.Offset + index];
+			}
+			set
+			{
+#if DEBUG
+				if (index < 0 || index >= this.Length)
+					throw new IndexOutOfRangeException();
+#endif
+				this.Data[this.Offset + index] = value;
+			}
+		}
+
+		public VertexSegment(T[] data, int offset, int length)
+		{
+			// No argument validation for performance reasons.
+			// Keeping it simple allows the JIT compiler to inline
+			// the entire constructor with a few mov instructions.
+#if DEBUG
+			if (data == null) throw new ArgumentNullException("data");
+			if (offset < 0 || offset >= data.Length) throw new ArgumentOutOfRangeException("offset");
+			if (length < 0 || length > data.Length - offset) throw new ArgumentOutOfRangeException("length");
+#endif
+			this.Data = data;
+			this.Offset = offset;
+			this.Length = length;
+		}
+	}
+}
---
+++ Test/Core/Drawing/VertexSegmentTest.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Drawing;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Diagnostics;
+
+using Duality;
+using Duality.Resources;
+using Duality.Drawing;
+using Duality.Tests.Properties;
+
+using NUnit.Framework;
+
+namespace Duality.Tests.Drawing
+{
+	public class VertexSegmentTest
+	{
+		[Test] public void Constructor()
+		{
+			// Create a vertex array and segment
+			VertexC1P3[] array = new VertexC1P3[10];
+			VertexSegment<VertexC1P3> segment = new VertexSegment<VertexC1P3>(
+				array, 
+				1, 
+				array.Length - 2);
+
+			// Assert that all the segment parameters are set properly
+			Assert.AreEqual(1, segment.Offset);
+			Assert.AreEqual(array.Length - 2, segment.Length);
+		}
+		[Test] public void IndexerAccess()
+		{
+			// Create a vertex array and segment
+			VertexC1P3[] array = new VertexC1P3[10];
+			VertexSegment<VertexC1P3> segment = new VertexSegment<VertexC1P3>(
+				array, 
+				1, 
+				array.Length - 2);
+			
+			// Fill the vertex array with data
+			for (int i = 0; i < array.Length; i++)
+			{
+				array[i].Color = new ColorRgba(i);
+			}
+
+			// Assert that we can access the vertex data via indexer
+			for (int i = 0; i < segment.Length; i++)
+			{
+				Assert.AreEqual(array[i + 1].Color, segment[i].Color);
+			}
+
+			// Assert that we can change data via indexer
+			for (int i = 0; i < segment.Length; i++)
+			{
+				ColorRgba newColor = new ColorRgba(1234);
+				segment[i] = new VertexC1P3
+				{
+					Color = newColor
+				};
+				Assert.AreEqual(newColor, segment[i].Color);
+				Assert.AreEqual(newColor, array[i + 1].Color);
+			}
+		}
+	}
+}
