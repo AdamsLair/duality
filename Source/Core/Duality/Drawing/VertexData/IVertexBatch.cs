f2523c643f76ce3ec78fa2f03ca143d8dfb2d7a2
Adam <adam@adamslair.net>
2017-09-01 18:51:24 +0200

    VertexBatch Docs #ADD: Added XML comments for VertexBatch and IVertexBatch.

--- Source/Core/Duality/Drawing/VertexData/IVertexBatch.cs
+++ Source/Core/Duality/Drawing/VertexData/IVertexBatch.cs
@@ -3,13 +3,37 @@ using System.Collections.Generic;
 
 namespace Duality.Drawing
 {
+	/// <summary>
+	/// Represents a single batch of dynamically gathered vertex data.
+	/// </summary>
 	public interface IVertexBatch
 	{
+		/// <summary>
+		/// [GET] The number of vertices in this batch.
+		/// </summary>
 		int Count { get; }
+		/// <summary>
+		/// [GET] The <see cref="VertexDeclaration"/> that is associated with 
+		/// the type of vertex data in this batch.
+		/// </summary>
 		VertexDeclaration Declaration { get; }
-
+		
+		/// <summary>
+		/// Clears the batch of all vertex data.
+		/// </summary>
 		void Clear();
-		RawList<T> GetTypedData<T>();
+		/// <summary>
+		/// Retrieves the batches internal vertex list for direct access.
+		/// Will return null if the batches vertex type does not match the generic
+		/// type parameter.
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <returns></returns>
+		RawList<T> GetTypedData<T>() where T : struct, IVertexData;
+		/// <summary>
+		/// Locks the batch in order to access its vertex data directly using an <see cref="IntPtr"/>.
+		/// </summary>
+		/// <returns></returns>
 		PinnedArrayHandle Lock();
 	}
 }
