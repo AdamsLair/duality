438785a6aed68e7fd8964b22ca604b84994c98fb
Adam <adam@adamslair.net>
2017-10-06 20:58:53 +0200

    UniqueIdentifyableHelper Docs, Formatting

--- Source/Core/Duality/Serialization/UniqueIdentifyableHelper.cs
+++ Source/Core/Duality/Serialization/UniqueIdentifyableHelper.cs
@@ -6,21 +6,49 @@ using System.Threading.Tasks;
 
 namespace Duality.Serialization
 {
+	/// <summary>
+	/// A static class that provides helper function related to generating serialization IDs
+	/// for interface implementations of <see cref="IUniqueIdentifyable"/>.
+	/// </summary>
 	public static class UniqueIdentifyableHelper
 	{
-		// Based on https://stackoverflow.com/a/2351171
-		private const int Multipler = 37;
+		/// <summary>
+		/// Creates a unique identifier based on the specified string.
+		/// It is guaranteed to remain the same across .NET runtimes, libraries and platforms.
+		/// </summary>
+		/// <param name="value"></param>
+		/// <returns></returns>
 		public static int GetIdentifier(string value)
 		{
+			// Based on https://stackoverflow.com/a/2351171
 			int result = 0;
-			unchecked { foreach (char c in value) result = (Multipler * result) + c; }
+			unchecked
+			{
+				for (int i = 0; i < value.Length; i++)
+				{
+					result = value[i] + 37 * result;
+				}
+			}
 			return result;
 		}
-
+		/// <summary>
+		/// Creates a unique identifier based on the specified <see cref="Guid"/>.
+		/// It is guaranteed to remain the same across .NET runtimes, libraries and platforms.
+		/// </summary>
+		/// <param name="guid"></param>
+		/// <returns></returns>
 		public static int GetIdentifier(Guid guid)
 		{
+			// Based on https://stackoverflow.com/a/2351171
 			int result = 0;
-			unchecked { foreach (byte b in guid.ToByteArray()) result = (Multipler * result) + b; }
+			unchecked
+			{
+				byte[] data = guid.ToByteArray();
+				for (int i = 0; i < data.Length; i++)
+				{
+					result = data[i] + 37 * result;
+				}
+			}
 			return result;
 		}
 	}
