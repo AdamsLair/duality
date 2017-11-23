152d6f4720d60513d125c8ad3a241e7d5fd7770d
Bob Chatman <bob.chatman@gmail.com>
2017-11-23 12:00:36 -0800

    EditorHelper Path Constants Cleanup
    #CHANGE: Now using static readonly fields instead of constants.
    #CHANGE: Now using Path.Combine instead of string concatenation.

--- Source/Editor/DualityEditor/Utility/EditorHelper.cs
+++ Source/Editor/DualityEditor/Utility/EditorHelper.cs
@@ -19,20 +19,21 @@ namespace Duality.Editor
 {
 	public static class EditorHelper
 	{
-		public const string DualityLauncherExecFile				= @"DualityLauncher.exe";
-		public const string BackupDirectory						= @"Backup";
-		public const string SourceDirectory						= @"Source";
-		public const string SourceMediaDirectory				= SourceDirectory + @"\Media";
-		public const string SourceCodeDirectory					= SourceDirectory + @"\Code";
-		public const string SourceCodeProjectCorePluginDir		= SourceCodeDirectory + @"\CorePlugin";
-		public const string SourceCodeProjectEditorPluginDir	= SourceCodeDirectory + @"\EditorPlugin";
-		public const string DefaultSourceCodeSolutionFile		= SourceCodeDirectory + @"\ProjectPlugins.sln";
-		public const string SourceCodeProjectCorePluginFile		= SourceCodeProjectCorePluginDir + @"\CorePlugin.csproj";
-		public const string SourceCodeProjectEditorPluginFile	= SourceCodeProjectEditorPluginDir + @"\EditorPlugin.csproj";
-		public const string SourceCodeErrorHandlerFile			= SourceCodeProjectCorePluginDir + @"\Properties\ErrorHandlers.cs";
-		public const string SourceCodeCorePluginFile			= SourceCodeProjectCorePluginDir + @"\CorePlugin.cs";
-		public const string SourceCodeComponentExampleFile		= SourceCodeProjectCorePluginDir + @"\YourCustomComponentType.cs";
-		public const string SourceCodeEditorPluginFile			= SourceCodeProjectEditorPluginDir + @"\EditorPlugin.cs";
+		public static readonly string DualityLauncherExecFile			= "DualityLauncher.exe";
+		public static readonly string BackupDirectory					= "Backup";
+		public static readonly string SourceDirectory					= "Source";
+
+		public static readonly string SourceMediaDirectory				= Path.Combine(SourceDirectory, "Media");
+		public static readonly string SourceCodeDirectory				= Path.Combine(SourceDirectory, "Code");
+		public static readonly string SourceCodeProjectCorePluginDir	= Path.Combine(SourceCodeDirectory, "CorePlugin");
+		public static readonly string SourceCodeProjectEditorPluginDir	= Path.Combine(SourceCodeDirectory, "EditorPlugin");
+		public static readonly string DefaultSourceCodeSolutionFile		= Path.Combine(SourceCodeDirectory, "ProjectPlugins.sln");
+		public static readonly string SourceCodeProjectCorePluginFile	= Path.Combine(SourceCodeProjectCorePluginDir, "CorePlugin.csproj");
+		public static readonly string SourceCodeProjectEditorPluginFile	= Path.Combine(SourceCodeProjectEditorPluginDir, "EditorPlugin.csproj");
+		public static readonly string SourceCodeErrorHandlerFile		= Path.Combine(SourceCodeProjectCorePluginDir, "Properties", "ErrorHandlers.cs");
+		public static readonly string SourceCodeCorePluginFile			= Path.Combine(SourceCodeProjectCorePluginDir, "CorePlugin.cs");
+		public static readonly string SourceCodeComponentExampleFile	= Path.Combine(SourceCodeProjectCorePluginDir, "YourCustomComponentType.cs");
+		public static readonly string SourceCodeEditorPluginFile		= Path.Combine(SourceCodeProjectEditorPluginDir, "EditorPlugin.cs");
 
 		public static readonly string GlobalUserDirectory = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "Duality");
 		public static readonly string GlobalProjectTemplateDirectory = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "Duality", "ProjectTemplates");
