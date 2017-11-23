ebae8db9e0526cf62d394a1f4b804a4b421c2431
Bob Chatman <bob.chatman@gmail.com>
2017-11-23 11:50:01 -0800

    Aligning Fields

--- Source/Editor/DualityEditor/Utility/EditorHelper.cs
+++ Source/Editor/DualityEditor/Utility/EditorHelper.cs
@@ -19,9 +19,9 @@ namespace Duality.Editor
 {
 	public static class EditorHelper
 	{
-		public static readonly string DualityLauncherExecFile				= "DualityLauncher.exe";
-		public static readonly string BackupDirectory						= "Backup";
-		public static readonly string SourceDirectory						= "Source";
+		public static readonly string DualityLauncherExecFile			= "DualityLauncher.exe";
+		public static readonly string BackupDirectory					= "Backup";
+		public static readonly string SourceDirectory					= "Source";
 
 		public static readonly string SourceMediaDirectory      		= Path.Combine(SourceDirectory, "Media");
 		public static readonly string SourceCodeDirectory 				= Path.Combine(SourceDirectory, "Code");
