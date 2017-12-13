90fef96a745d383091525506f2a6a9e624dc1259
Bob Chatman <bob.chatman@gmail.com>
2017-12-13 10:38:12 -0800

    Adding content selection dialog form

---
+++ Source/Editor/DualityEditor/Forms/ContentSelectionDialog.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Duality.Editor.Forms
+{
+	public partial class ContentSelectionDialog : Form
+	{
+		public ContentSelectionDialog()
+		{
+			InitializeComponent();
+		}
+	}
+}
