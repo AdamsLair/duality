c418ecdbf4c7d2a9971d32db006da348e2d05a7f
Adam <adam@adamslair.net>
2017-12-14 21:41:29 +0100

    ShaderSourceBuilder Skeleton, Test #ADD: Added the not-yet-implemented API skeleton for a ShaderSourceBuilder utility class. #ADD: Added a (failing) test case that roughly outlines the expected ShaderSourceBuilder behavior.

---
+++ Source/Core/Duality/Resources/Shaders/ShaderSourceBuilder.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Duality.Resources
+{
+	public class ShaderSourceBuilder
+	{
+		private string mainChunk = string.Empty;
+		private List<string> sharedChunk = new List<string>();
+
+		public void Clear()
+		{
+			this.mainChunk = string.Empty;
+			this.sharedChunk.Clear();
+		}
+		public void SetMainChunk(string sourceCode)
+		{
+			this.mainChunk = sourceCode ?? string.Empty;
+		}
+		public void AddSharedChunk(string sourceCode)
+		{
+			this.sharedChunk.Add(sourceCode);
+		}
+
+		public string Build()
+		{
+			// ToDo
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ Test/Core/Resources/Shaders/ShaderSourceBuilderTest.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+
+using Duality;
+using Duality.Resources;
+using Duality.Tests.Properties;
+
+using NUnit.Framework;
+
+
+namespace Duality.Tests.Resources
+{
+	[TestFixture]
+	public class ShaderSourceBuilderTest
+	{
+		[Test] public void Basics()
+		{
+			ShaderSourceBuilder builder = new ShaderSourceBuilder();
+			string mainShader = new StringBuilder()
+				.AppendLine("uniform vec4 mainUniformA;")
+				.AppendLine("uniform float mainUniformB;")
+				.AppendLine("uniform vec4 sharedUniformA;")
+				.AppendLine()
+				.AppendLine("attribute vec4 mainAttributeA;")
+				.AppendLine("attribute vec4 sharedAttributeA;")
+				.AppendLine("in float mainAttributeB;")
+				.AppendLine()
+				.AppendLine("varying vec4 sharedVaryingA;")
+				.AppendLine("varying vec4 mainVaryingA;")
+				.AppendLine("out float mainVaryingB;")
+				.AppendLine()
+				.AppendLine("void main()")
+				.AppendLine("{")
+				.AppendLine("  gl_Position = sharedFunc(gl_Vertex);")
+				.AppendLine("}")
+				.ToString();
+			string sharedShaderA = new StringBuilder()
+				.AppendLine("uniform vec4 sharedUniformA;")
+				.AppendLine("attribute vec4 sharedAttributeA;")
+				.AppendLine("varying vec4 sharedVaryingA;")
+				.AppendLine()
+				.AppendLine("vec4 sharedFuncA(vec4 pos)")
+				.AppendLine("{")
+				.AppendLine("  return pos + vec4(1.0, 0.0, 0.0, 0.0);")
+				.AppendLine("}")
+				.ToString();
+			string sharedShaderB = new StringBuilder()
+				.AppendLine("uniform float sharedUniformB;")
+				.AppendLine("in float sharedAttributeB;")
+				.AppendLine("out float sharedVaryingB;")
+				.AppendLine()
+				.AppendLine("vec4 sharedFuncB(vec4 pos)")
+				.AppendLine("{")
+				.AppendLine("  return pos + vec4(1.0, 0.0, 0.0, 0.0);")
+				.AppendLine("}")
+				.ToString();
+			string expectedResultShader = new StringBuilder()
+				.AppendLine("#line 10000")
+				.AppendLine("uniform vec4 sharedUniformA;")
+				.AppendLine("attribute vec4 sharedAttributeA;")
+				.AppendLine("varying vec4 sharedVaryingA;")
+				.AppendLine()
+				.AppendLine("vec4 sharedFuncA(vec4 pos)")
+				.AppendLine("{")
+				.AppendLine("  return pos + vec4(1.0, 0.0, 0.0, 0.0);")
+				.AppendLine("}")
+				.AppendLine()
+				.AppendLine("#line 20000")
+				.AppendLine("uniform float sharedUniformB;")
+				.AppendLine("in float sharedAttributeB;")
+				.AppendLine("out float sharedVaryingB;")
+				.AppendLine()
+				.AppendLine("vec4 sharedFuncB(vec4 pos)")
+				.AppendLine("{")
+				.AppendLine("  return pos + vec4(1.0, 0.0, 0.0, 0.0);")
+				.AppendLine("}")
+				.AppendLine()
+				.AppendLine("#line 1")
+				.AppendLine("uniform vec4 mainUniformA;")
+				.AppendLine("uniform float mainUniformB;")
+				.AppendLine("// Shared: uniform vec4 sharedUniformA;")
+				.AppendLine()
+				.AppendLine("attribute vec4 mainAttributeA;")
+				.AppendLine("// Shared: attribute vec4 sharedAttributeA;")
+				.AppendLine("in float mainAttributeB;")
+				.AppendLine()
+				.AppendLine("// Shared: varying vec4 sharedVaryingA;")
+				.AppendLine("varying vec4 mainVaryingA;")
+				.AppendLine("out float mainVaryingB;")
+				.AppendLine()
+				.AppendLine("void main()")
+				.AppendLine("{")
+				.AppendLine("  gl_Position = sharedFunc(gl_Vertex);")
+				.AppendLine("}")
+				.ToString();
+
+			builder.SetMainChunk(mainShader);
+			builder.AddSharedChunk(sharedShaderA);
+			builder.AddSharedChunk(sharedShaderB);
+
+			string resultShader = builder.Build();
+			Assert.AreEqual(expectedResultShader, resultShader);
+		}
+	}
+}
