bcbfb35c43c2802b0eaac789c52f00646296851a
Adam <adam@adamslair.net>
2017-12-14 21:49:25 +0100

    ShaderSourceBuilder Implementation Draft #ADD: Added a few lines of basic implementation with ToDo comments, yet to reach successful test state.

--- Source/Core/Duality/Resources/Shaders/ShaderSourceBuilder.cs
+++ Source/Core/Duality/Resources/Shaders/ShaderSourceBuilder.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Text.RegularExpressions;
 
 namespace Duality.Resources
@@ -9,6 +10,9 @@ namespace Duality.Resources
 	{
 		private string mainChunk = string.Empty;
 		private List<string> sharedChunk = new List<string>();
+
+		private StringBuilder textBuilder = new StringBuilder();
+
 
 		public void Clear()
 		{
@@ -26,8 +30,23 @@ namespace Duality.Resources
 
 		public string Build()
 		{
-			// ToDo
-			throw new NotImplementedException();
+			this.textBuilder.Clear();
+
+			// ToDo: Automatically comment out redefined uniforms
+			// ToDo: Make sure the #version directive is moved to the top of the result
+
+			for (int i = 0; i < this.sharedChunk.Count; i++)
+			{
+				this.textBuilder.AppendFormat("#line {0}", (i + 1) * 10000);
+				this.textBuilder.AppendLine();
+				this.textBuilder.Append(this.sharedChunk[i]);
+				this.textBuilder.AppendLine();
+			}
+
+			this.textBuilder.AppendLine("#line 1");
+			this.textBuilder.Append(this.mainChunk);
+
+			return this.textBuilder.ToString();
 		}
 	}
 }
