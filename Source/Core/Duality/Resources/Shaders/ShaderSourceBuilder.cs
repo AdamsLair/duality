f5a227a1f98f44afc02145531e12729246c382d7
Adam <adam@adamslair.net>
2017-12-15 22:09:47 +0100

    ShaderSourceBuilder Tests, Implementation #ADD: Implemented ShaderSourceBuilder in first iteration. #ADD: Added tests for comment and version directive handling.

--- Source/Core/Duality/Resources/Shaders/ShaderSourceBuilder.cs
+++ Source/Core/Duality/Resources/Shaders/ShaderSourceBuilder.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.IO;
 using System.Text;
 using System.Text.RegularExpressions;
 
@@ -8,6 +9,35 @@ namespace Duality.Resources
 {
 	public class ShaderSourceBuilder
 	{
+		private struct IndexRange
+		{
+			public int Index;
+			public int Length;
+
+			public IndexRange(int index, int length)
+			{
+				this.Index = index;
+				this.Length = length;
+			}
+
+			public bool Overlaps(IndexRange other)
+			{
+				if (other.Index >= this.Index && other.Index < this.Index + this.Length) return true;
+				if (this.Index >= other.Index && this.Index < other.Index + other.Length) return true;
+				return false;
+			}
+
+			public override string ToString()
+			{
+				return string.Format("[{0} - {1}]", this.Index, this.Index + this.Length);
+			}
+		}
+
+		private static readonly Regex RegexBlockComment = new Regex(@"/\*(.*?)\*/", RegexOptions.Singleline);
+		private static readonly Regex RegexLineComment = new Regex(@"//(.*?)\r?\n", RegexOptions.Singleline);
+		private static readonly Regex RegexVariableDeclaration = new Regex(@"(uniform|varying|attribute|in|out)\s+(\w+)\s+(\w+)\s*;", RegexOptions.Singleline);
+		private static readonly Regex RegexVersionDirective = new Regex(@"#version\s+(\d+)\s+");
+
 		private string mainChunk = string.Empty;
 		private List<string> sharedChunk = new List<string>();
 
@@ -32,9 +62,7 @@ namespace Duality.Resources
 		{
 			this.textBuilder.Clear();
 
-			// ToDo: Automatically comment out redefined uniforms
-			// ToDo: Make sure the #version directive is moved to the top of the result
-
+			// Append shared chunks at the top
 			for (int i = 0; i < this.sharedChunk.Count; i++)
 			{
 				this.textBuilder.AppendFormat("#line {0}", (i + 1) * 10000);
@@ -43,10 +71,102 @@ namespace Duality.Resources
 				this.textBuilder.AppendLine();
 			}
 
+			// Append main chunk below
 			this.textBuilder.AppendLine("#line 1");
 			this.textBuilder.Append(this.mainChunk);
 
-			return this.textBuilder.ToString();
+			// Generate a first, raw version of the merged source code
+			string rawMerge = this.textBuilder.ToString();
+
+			// Identify all commented regions, so we can exclude them from further processing
+			List<IndexRange> ignoreRegions = new List<IndexRange>();
+			foreach (Match match in RegexBlockComment.Matches(rawMerge))
+			{
+				ignoreRegions.Add(new IndexRange(
+					match.Groups[1].Index, 
+					match.Groups[1].Length));
+			}
+			foreach (Match match in RegexLineComment.Matches(rawMerge))
+			{
+				ignoreRegions.Add(new IndexRange(
+					match.Groups[1].Index,
+					match.Groups[1].Length));
+			}
+
+			// Identify (shared) variable declarations
+			HashSet<string> variableDeclarations = new HashSet<string>();
+			List<IndexRange> removeLines = new List<IndexRange>();
+			foreach (Match match in RegexVariableDeclaration.Matches(rawMerge))
+			{
+				IndexRange range = new IndexRange(match.Index, match.Length);
+				if (this.AnyRangeOverlap(range, ignoreRegions)) continue;
+
+				// Normalize declaration, so we can compare it to others and match regardless of spacing,
+				// declaring keyword or optional qualifiers.
+				string normalizedDecl = (match.Groups[2].Value + match.Groups[3].Value);
+
+				// If we previously saw that declaration, schedule it for removal
+				if (!variableDeclarations.Add(normalizedDecl))
+				{
+					IndexRange lineRange = this.ExpandToLine(rawMerge, range);
+					if (lineRange.Length > 0)
+						removeLines.Add(lineRange);
+				}
+			}
+
+			// Identify version directives
+			int lastVersion = 0;
+			foreach (Match match in RegexVersionDirective.Matches(rawMerge))
+			{
+				IndexRange range = new IndexRange(match.Index, match.Length);
+				if (this.AnyRangeOverlap(range, ignoreRegions)) continue;
+
+				int version;
+				if (int.TryParse(match.Groups[1].Value, out version))
+					lastVersion = version;
+
+				IndexRange lineRange = this.ExpandToLine(rawMerge, range);
+				if (lineRange.Length > 0)
+					removeLines.Add(lineRange);
+			}
+
+			// Comment out lines that we scheduled for removal
+			this.CommentOutLines(this.textBuilder, removeLines);
+
+			// If we encountered any version directive, add it back at the top
+			if (lastVersion > 0)
+			{
+				this.textBuilder.Insert(0, "\r\n");
+				this.textBuilder.Insert(0, lastVersion);
+				this.textBuilder.Insert(0, "#version ");
+			}
+
+			rawMerge = this.textBuilder.ToString();
+			return rawMerge;
+		}
+
+		private IndexRange ExpandToLine(string text, IndexRange range)
+		{
+			int lineBeginIndex = text.LastIndexOfAny(new char[] { '\n', '\r' }, range.Index) + 1;
+			int lineEndIndex = text.IndexOfAny(new char[] { '\n', '\r' }, range.Index + range.Length);
+			return new IndexRange(lineBeginIndex, lineEndIndex - lineBeginIndex);
+		}
+		private bool AnyRangeOverlap(IndexRange range, List<IndexRange> compareToRanges)
+		{
+			foreach (IndexRange ignoreRange in compareToRanges)
+			{
+				if (range.Overlaps(ignoreRange))
+					return true;
+			}
+			return false;
+		}
+		private void CommentOutLines(StringBuilder builder, List<IndexRange> lines)
+		{
+			for (int i = lines.Count - 1; i >= 0; i--)
+			{
+				IndexRange range = lines[i];
+				this.textBuilder.Insert(range.Index, "// ");
+			}
 		}
 	}
 }
--- Test/Core/Resources/Shaders/ShaderSourceBuilderTest.cs
+++ Test/Core/Resources/Shaders/ShaderSourceBuilderTest.cs
@@ -80,13 +80,13 @@ namespace Duality.Tests.Resources
 				.AppendLine("#line 1")
 				.AppendLine("uniform vec4 mainUniformA;")
 				.AppendLine("uniform float mainUniformB;")
-				.AppendLine("// Shared: uniform vec4 sharedUniformA;")
+				.AppendLine("// uniform vec4 sharedUniformA;")
 				.AppendLine()
 				.AppendLine("attribute vec4 mainAttributeA;")
-				.AppendLine("// Shared: attribute vec4 sharedAttributeA;")
+				.AppendLine("// attribute vec4 sharedAttributeA;")
 				.AppendLine("in float mainAttributeB;")
 				.AppendLine()
-				.AppendLine("// Shared: varying vec4 sharedVaryingA;")
+				.AppendLine("// varying vec4 sharedVaryingA;")
 				.AppendLine("varying vec4 mainVaryingA;")
 				.AppendLine("out float mainVaryingB;")
 				.AppendLine()
@@ -99,6 +99,105 @@ namespace Duality.Tests.Resources
 			builder.SetMainChunk(mainShader);
 			builder.AddSharedChunk(sharedShaderA);
 			builder.AddSharedChunk(sharedShaderB);
+
+			string resultShader = builder.Build();
+			Assert.AreEqual(expectedResultShader, resultShader);
+		}
+		[Test] public void CommentHandling()
+		{
+			ShaderSourceBuilder builder = new ShaderSourceBuilder();
+			string mainShader = new StringBuilder()
+				.AppendLine("uniform vec4 sharedUniformA;")
+				.AppendLine("uniform float sharedUniformB;")
+				.AppendLine("uniform float sharedUniformC;")
+				.AppendLine()
+				.AppendLine("void main()")
+				.AppendLine("{")
+				.AppendLine("  gl_Position = sharedFunc(gl_Vertex);")
+				.AppendLine("}")
+				.ToString();
+			string sharedShader = new StringBuilder()
+				.AppendLine("uniform vec4 sharedUniformA;")
+				.AppendLine("//uniform float sharedUniformB;")
+				.AppendLine("/*")
+				.AppendLine("uniform float sharedUniformC;")
+				.AppendLine("*/")
+				.AppendLine()
+				.AppendLine("vec4 sharedFuncA(vec4 pos)")
+				.AppendLine("{")
+				.AppendLine("  return pos + vec4(1.0, 0.0, 0.0, 0.0);")
+				.AppendLine("}")
+				.ToString();
+			string expectedResultShader = new StringBuilder()
+				.AppendLine("#line 10000")
+				.AppendLine("uniform vec4 sharedUniformA;")
+				.AppendLine("//uniform float sharedUniformB;")
+				.AppendLine("/*")
+				.AppendLine("uniform float sharedUniformC;")
+				.AppendLine("*/")
+				.AppendLine()
+				.AppendLine("vec4 sharedFuncA(vec4 pos)")
+				.AppendLine("{")
+				.AppendLine("  return pos + vec4(1.0, 0.0, 0.0, 0.0);")
+				.AppendLine("}")
+				.AppendLine()
+				.AppendLine("#line 1")
+				.AppendLine("// uniform vec4 sharedUniformA;")
+				.AppendLine("uniform float sharedUniformB;")
+				.AppendLine("uniform float sharedUniformC;")
+				.AppendLine()
+				.AppendLine("void main()")
+				.AppendLine("{")
+				.AppendLine("  gl_Position = sharedFunc(gl_Vertex);")
+				.AppendLine("}")
+				.ToString();
+
+			builder.SetMainChunk(mainShader);
+			builder.AddSharedChunk(sharedShader);
+
+			string resultShader = builder.Build();
+			Assert.AreEqual(expectedResultShader, resultShader);
+		}
+		[Test] public void VersionDirective()
+		{
+			ShaderSourceBuilder builder = new ShaderSourceBuilder();
+			string mainShader = new StringBuilder()
+				.AppendLine("#version 150")
+				.AppendLine()
+				.AppendLine("void main()")
+				.AppendLine("{")
+				.AppendLine("  gl_Position = sharedFunc(gl_Vertex);")
+				.AppendLine("}")
+				.ToString();
+			string sharedShader = new StringBuilder()
+				.AppendLine("#version 140")
+				.AppendLine()
+				.AppendLine("vec4 sharedFuncA(vec4 pos)")
+				.AppendLine("{")
+				.AppendLine("  return pos + vec4(1.0, 0.0, 0.0, 0.0);")
+				.AppendLine("}")
+				.ToString();
+			string expectedResultShader = new StringBuilder()
+				.AppendLine("#version 150")
+				.AppendLine("#line 10000")
+				.AppendLine("// #version 140")
+				.AppendLine()
+				.AppendLine("vec4 sharedFuncA(vec4 pos)")
+				.AppendLine("{")
+				.AppendLine("  return pos + vec4(1.0, 0.0, 0.0, 0.0);")
+				.AppendLine("}")
+				.AppendLine()
+				.AppendLine("#line 1")
+				.AppendLine("// #version 150")
+				.AppendLine()
+				.AppendLine("void main()")
+				.AppendLine("{")
+				.AppendLine("  gl_Position = sharedFunc(gl_Vertex);")
+				.AppendLine("}")
+				.ToString();
+
+			builder.SetMainChunk(mainShader);
+			builder.AddSharedChunk(sharedShader);
 
 			string resultShader = builder.Build();
 			Assert.AreEqual(expectedResultShader, resultShader);
