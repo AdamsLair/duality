7d9c8081480eb5937b62f077bf2ce2c21c3b43e9
Bob Chatman <bob.chatman@gmail.com>
2017-12-15 22:12:44 -0800

    replacing ExplorerListView with AutoExpandTreeView

--- Source/Editor/DualityEditor/Forms/ObjectRefSelectionDialog.Designer.cs
+++ Source/Editor/DualityEditor/Forms/ObjectRefSelectionDialog.Designer.cs
@@ -28,31 +28,17 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
 			this.buttonCancel = new System.Windows.Forms.Button();
 			this.buttonOk = new System.Windows.Forms.Button();
 			this.labelInfo = new System.Windows.Forms.Label();
-			this.imageListResourceListing = new System.Windows.Forms.ImageList(this.components);
-			this.splitContainer1 = new System.Windows.Forms.SplitContainer();
-			this.splitContainer2 = new System.Windows.Forms.SplitContainer();
-			this.resourceListing = new Duality.Editor.Controls.ExplorerListView();
-			this.columnName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-			this.columnPath = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-			((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
-			this.splitContainer1.Panel1.SuspendLayout();
-			this.splitContainer1.Panel2.SuspendLayout();
-			this.splitContainer1.SuspendLayout();
-			((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
-			this.splitContainer2.Panel1.SuspendLayout();
-			this.splitContainer2.Panel2.SuspendLayout();
-			this.splitContainer2.SuspendLayout();
+			this.autoExpandTreeView1 = new Duality.Editor.Controls.AutoExpandTreeView();
 			this.SuspendLayout();
 			// 
 			// buttonCancel
 			// 
 			this.buttonCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
 			this.buttonCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-			this.buttonCancel.Location = new System.Drawing.Point(277, 13);
+			this.buttonCancel.Location = new System.Drawing.Point(277, 326);
 			this.buttonCancel.Name = "buttonCancel";
 			this.buttonCancel.Size = new System.Drawing.Size(75, 23);
 			this.buttonCancel.TabIndex = 2;
@@ -63,7 +49,7 @@
 			// 
 			this.buttonOk.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
 			this.buttonOk.DialogResult = System.Windows.Forms.DialogResult.OK;
-			this.buttonOk.Location = new System.Drawing.Point(196, 13);
+			this.buttonOk.Location = new System.Drawing.Point(196, 326);
 			this.buttonOk.Name = "buttonOk";
 			this.buttonOk.Size = new System.Drawing.Size(75, 23);
 			this.buttonOk.TabIndex = 1;
@@ -80,88 +66,29 @@
 			this.labelInfo.TabIndex = 6;
 			this.labelInfo.Text = "Please select the resource you would like to use.";
 			// 
-			// imageListResourceListing
+			// autoExpandTreeView1
 			// 
-			this.imageListResourceListing.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
-			this.imageListResourceListing.ImageSize = new System.Drawing.Size(16, 16);
-			this.imageListResourceListing.TransparentColor = System.Drawing.Color.Transparent;
+			this.autoExpandTreeView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.autoExpandTreeView1.AutoExpandDelay = 750;
+			this.autoExpandTreeView1.BackColor = System.Drawing.SystemColors.Window;
+			this.autoExpandTreeView1.ColumnHeaderHeight = 0;
+			this.autoExpandTreeView1.DefaultToolTipProvider = null;
+			this.autoExpandTreeView1.DragDropMarkColor = System.Drawing.Color.Black;
+			this.autoExpandTreeView1.FullRowSelectActiveColor = System.Drawing.Color.Empty;
+			this.autoExpandTreeView1.FullRowSelectInactiveColor = System.Drawing.Color.Empty;
+			this.autoExpandTreeView1.LineColor = System.Drawing.SystemColors.ControlDark;
+			this.autoExpandTreeView1.Location = new System.Drawing.Point(15, 36);
+			this.autoExpandTreeView1.Model = null;
+			this.autoExpandTreeView1.Name = "autoExpandTreeView1";
+			this.autoExpandTreeView1.NodeFilter = null;
+			this.autoExpandTreeView1.SelectedNode = null;
+			this.autoExpandTreeView1.Size = new System.Drawing.Size(337, 284);
+			this.autoExpandTreeView1.TabIndex = 7;
+			this.autoExpandTreeView1.Text = "autoExpandTreeView1";
 			// 
-			// splitContainer1
-			// 
-			this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel2;
-			this.splitContainer1.IsSplitterFixed = true;
-			this.splitContainer1.Location = new System.Drawing.Point(0, 0);
-			this.splitContainer1.Name = "splitContainer1";
-			this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
-			// 
-			// splitContainer1.Panel1
-			// 
-			this.splitContainer1.Panel1.Controls.Add(this.splitContainer2);
-			// 
-			// splitContainer1.Panel2
-			// 
-			this.splitContainer1.Panel2.Controls.Add(this.buttonCancel);
-			this.splitContainer1.Panel2.Controls.Add(this.buttonOk);
-			this.splitContainer1.Size = new System.Drawing.Size(364, 370);
-			this.splitContainer1.SplitterDistance = 318;
-			this.splitContainer1.TabIndex = 8;
-			// 
-			// splitContainer2
-			// 
-			this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.splitContainer2.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
-			this.splitContainer2.IsSplitterFixed = true;
-			this.splitContainer2.Location = new System.Drawing.Point(0, 0);
-			this.splitContainer2.Name = "splitContainer2";
-			this.splitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal;
-			// 
-			// splitContainer2.Panel1
-			// 
-			this.splitContainer2.Panel1.Controls.Add(this.labelInfo);
-			// 
-			// splitContainer2.Panel2
-			// 
-			this.splitContainer2.Panel2.Controls.Add(this.resourceListing);
-			this.splitContainer2.Size = new System.Drawing.Size(364, 318);
-			this.splitContainer2.SplitterDistance = 38;
-			this.splitContainer2.TabIndex = 0;
-			// 
-			// resourceListing
-			// 
-			this.resourceListing.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(196)))), ((int)(((byte)(196)))), ((int)(((byte)(196)))));
-			this.resourceListing.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-			this.resourceListing.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
-            this.columnName,
-            this.columnPath});
-			this.resourceListing.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.resourceListing.FullRowSelect = true;
-			this.resourceListing.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
-			this.resourceListing.Location = new System.Drawing.Point(0, 0);
-			this.resourceListing.MultiSelect = false;
-			this.resourceListing.Name = "resourceListing";
-			this.resourceListing.ShowGroups = false;
-			this.resourceListing.ShowItemToolTips = true;
-			this.resourceListing.Size = new System.Drawing.Size(364, 276);
-			this.resourceListing.Sorting = System.Windows.Forms.SortOrder.Ascending;
-			this.resourceListing.TabIndex = 0;
-			this.resourceListing.TileSize = new System.Drawing.Size(400, 38);
-			this.resourceListing.UseCompatibleStateImageBehavior = false;
-			this.resourceListing.View = System.Windows.Forms.View.Tile;
-			this.resourceListing.SelectedIndexChanged += new System.EventHandler(this.ResourceListingOnSelectedIndexChanged);
-			this.resourceListing.Resize += new System.EventHandler(this.resourceListing_Resize);
-			// 
-			// columnName
-			// 
-			this.columnName.DisplayIndex = 1;
-			this.columnName.Width = 400;
-			// 
-			// columnPath
-			// 
-			this.columnPath.DisplayIndex = 0;
-			this.columnPath.Width = 400;
-			// 
-			// ContentSelectionDialog
+			// ObjectRefSelectionDialog
 			// 
 			this.AcceptButton = this.buttonOk;
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -169,23 +96,18 @@
 			this.AutoValidate = System.Windows.Forms.AutoValidate.Disable;
 			this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(212)))), ((int)(((byte)(212)))), ((int)(((byte)(212)))));
 			this.CancelButton = this.buttonCancel;
-			this.ClientSize = new System.Drawing.Size(364, 370);
-			this.Controls.Add(this.splitContainer1);
+			this.ClientSize = new System.Drawing.Size(364, 361);
+			this.Controls.Add(this.autoExpandTreeView1);
+			this.Controls.Add(this.buttonCancel);
+			this.Controls.Add(this.labelInfo);
+			this.Controls.Add(this.buttonOk);
 			this.MaximizeBox = false;
 			this.MinimizeBox = false;
 			this.MinimumSize = new System.Drawing.Size(380, 400);
-			this.Name = "ContentSelectionDialog";
+			this.Name = "ObjectRefSelectionDialog";
 			this.ShowIcon = false;
 			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
 			this.Text = "Select a Resource...";
-			this.splitContainer1.Panel1.ResumeLayout(false);
-			this.splitContainer1.Panel2.ResumeLayout(false);
-			((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
-			this.splitContainer1.ResumeLayout(false);
-			this.splitContainer2.Panel1.ResumeLayout(false);
-			this.splitContainer2.Panel2.ResumeLayout(false);
-			((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
-			this.splitContainer2.ResumeLayout(false);
 			this.ResumeLayout(false);
 
 		}
@@ -195,11 +117,6 @@
 		private System.Windows.Forms.Button buttonCancel;
 		private System.Windows.Forms.Button buttonOk;
 		private System.Windows.Forms.Label labelInfo;
-		private Controls.ExplorerListView resourceListing;
-		private System.Windows.Forms.ImageList imageListResourceListing;
-		private System.Windows.Forms.ColumnHeader columnName;
-		private System.Windows.Forms.ColumnHeader columnPath;
-		private System.Windows.Forms.SplitContainer splitContainer1;
-		private System.Windows.Forms.SplitContainer splitContainer2;
+		private Controls.AutoExpandTreeView autoExpandTreeView1;
 	}
 }
--- Source/Editor/DualityEditor/Forms/ObjectRefSelectionDialog.cs
+++ Source/Editor/DualityEditor/Forms/ObjectRefSelectionDialog.cs
@@ -15,7 +15,7 @@ namespace Duality.Editor.Forms
 		{
 			InitializeComponent();
 
-			this.resourceListing.DoubleClick += this.ResourceListingOnDoubleClick;
+			// this.resourceListing.DoubleClick += this.ResourceListingOnDoubleClick;
 		}
 
 		protected override void OnShown(EventArgs e)
@@ -24,8 +24,7 @@ namespace Duality.Editor.Forms
 
 			this.Text = string.Format("Select a {0} Resource", this.FilteredType.Name);
 
-			this.imageListResourceListing.Images.Clear();
-			this.resourceListing.Items.Clear();
+			// this.resourceListing.Items.Clear();
 
 			foreach (var contentRef in ContentProvider.GetAvailableContent(this.FilteredType))
 			{
@@ -34,7 +33,7 @@ namespace Duality.Editor.Forms
 					Tag = contentRef
 				};
 
-				this.resourceListing.Items.Add(tmpLVI);
+				// this.resourceListing.Items.Add(tmpLVI);
 
 				if (contentRef.FullName == this.ResourcePath)
 				{
@@ -42,7 +41,7 @@ namespace Duality.Editor.Forms
 				}
 			}
 
-			this.resourceListing.Focus();
+			// this.resourceListing.Focus();
 		}
 
 		private void ResourceListingOnDoubleClick(object sender, EventArgs eventArgs)
@@ -53,7 +52,7 @@ namespace Duality.Editor.Forms
 
 		private void ResourceListingOnSelectedIndexChanged(object sender, EventArgs eventArgs)
 		{
-			foreach (ListViewItem resourceListingSelectedItem in this.resourceListing.SelectedItems)
+			/*foreach (ListViewItem resourceListingSelectedItem in this.resourceListing.SelectedItems)
 			{
 				var contentRef = resourceListingSelectedItem.Tag as IContentRef;
 
@@ -62,12 +61,12 @@ namespace Duality.Editor.Forms
 					this.ContentReference = contentRef;
 					this.ResourcePath = this.ContentReference.FullName;
 				}
-			}
+			}*/
 		}
 
 		private void resourceListing_Resize(object sender, EventArgs e)
 		{
-			this.resourceListing.TileSize = new Size(this.resourceListing.ClientSize.Width, this.resourceListing.TileSize.Height);
+			// this.resourceListing.TileSize = new Size(this.resourceListing.ClientSize.Width, this.resourceListing.TileSize.Height);
 		}
 
 		/// <summary>
