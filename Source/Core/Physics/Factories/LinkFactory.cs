4943fe308de367047d6e7068b4c135343abcf7d5
Jared Miller <1668550+Jared-Miller@users.noreply.github.com>
2018-01-02 09:30:48 -0500

    Switch branch base to develop-3.0

---
+++ Source/Core/Physics/Factories/LinkFactory.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using FarseerPhysics.Collision.Shapes;
+using FarseerPhysics.Common;
+using FarseerPhysics.Dynamics;
+using Duality;
+
+namespace FarseerPhysics.Factories
+{
+    public static class LinkFactory
+    {
+        /// <summary>
+        /// Creates a chain.
+        /// </summary>
+        /// <param name="world">The world.</param>
+        /// <param name="start">The start.</param>
+        /// <param name="end">The end.</param>
+        /// <param name="linkWidth">The width.</param>
+        /// <param name="linkHeight">The height.</param>
+        /// <param name="numberOfLinks">The number of links.</param>
+        /// <param name="linkDensity">The link density.</param>
+        /// <returns></returns>
+        public static Path CreateChain(World world, Vector2 start, Vector2 end, float linkWidth, float linkHeight, int numberOfLinks, float linkDensity)
+        {
+            //Chain start / end
+            Path path = new Path();
+            path.Add(start);
+            path.Add(end);
+
+            //A single chainlink
+            PolygonShape shape = new PolygonShape(PolygonTools.CreateRectangle(linkWidth, linkHeight), linkDensity);
+
+            //Use PathManager to create all the chainlinks based on the chainlink created before.
+            List<Body> chainLinks = PathManager.EvenlyDistributeShapesAlongPath(world, path, shape, BodyType.Dynamic,
+                                                                                numberOfLinks);
+
+            //Attach all the chainlinks together with a revolute joint
+            PathManager.AttachBodiesWithRevoluteJoint(world, chainLinks, new Vector2(0, -linkHeight),
+                                                      new Vector2(0, linkHeight),
+                                                      false, false);
+
+            return (path);
+        }
+    }
+}
---
+++ Source/Core/Physics/Properties/AssemblyInfo.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+
+[assembly: AssemblyTitle("Farseer Physics Duality")]
+[assembly: AssemblyProduct("Farseer Physics Duality")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCompany("Farseer Physics Engine")]
+[assembly: AssemblyCopyright("Copyright © Farseer Physics 2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+
+[assembly: AssemblyVersion("4.1.4")]
