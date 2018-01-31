851218d42dea3c48208c7f035fdb9becb0531969
Patrick Adesso <patrickadesso@gmail.com>
2018-01-31 17:16:04 -0500

    Added undo history module

---
+++ Source/Plugins/EditorModules/UndoHistoryView/Modules/UndoHistoryView.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WeifenLuo.WinFormsUI.Docking;
+
+namespace Duality.Editor.Plugins.UndoHistoryView
+{
+    public partial class UndoHistoryView : DockContent
+    {      
+        private BindingSource _undoStackSource;
+
+        public UndoHistoryView()
+        {
+            InitializeComponent();
+
+            //Can't bind directly to a IReadOnlyList, so we create a binding source
+            _undoStackSource = new BindingSource();
+            _undoStackSource.DataSource = UndoRedoManager.ActionStack;
+
+            //Setup data binding and display 
+            undoRedoListBox.DataSource = _undoStackSource;
+            undoRedoListBox.DisplayMember = "Name";
+
+            //handle clicks to undo/redo
+            undoRedoListBox.Click += _undoRedoListBox_Click;
+
+            //Make sure the UI stays in sync with any undo/redo changes
+            UndoRedoManager.StackChanged += UndoRedoManager_StackChanged;     
+        }
+
+        private void _undoRedoListBox_Click(object sender, EventArgs e)
+        {
+            //Where in the stack we need to end up
+            int selectedIndex = ((ListBox)sender).SelectedIndex;
+
+            //Walk the stack until the selected index and position in undo/redo stack is equal
+            while (selectedIndex != UndoRedoManager.ActionIndex)
+            {
+                if (selectedIndex < UndoRedoManager.ActionIndex)
+                {
+                    UndoRedoManager.Undo();
+                }
+                else if (selectedIndex > UndoRedoManager.ActionIndex)
+                {
+                    UndoRedoManager.Redo();
+                }
+            }
+        }
+
+        private void UndoRedoManager_StackChanged(object sender, EventArgs e)
+        {
+            //update the list
+            _undoStackSource.ResetBindings(false);
+
+            //update the selection
+            undoRedoListBox.SelectedIndex = UndoRedoManager.ActionIndex;
+        }
+
+        private void undoButton_Click(object sender, EventArgs e)
+        {
+            if (UndoRedoManager.CanUndo)
+                UndoRedoManager.Undo();
+        }
+
+        private void redoButton_Click(object sender, EventArgs e)
+        {
+            if (UndoRedoManager.CanRedo)
+                UndoRedoManager.Redo();
+        }
+    }
+}
---
+++ Source/Plugins/EditorModules/UndoHistoryView/UndoHistoryViewPlugin.cs
@@ -0,0 +1,73 @@
+﻿using AdamsLair.WinForms.ItemModels;
+using Duality.Editor.Forms;
+using Duality.Editor.Plugins.UndoHistoryView.Properties;
+using Duality.Editor.Properties;
+using System;
+using System.Windows.Forms;
+using WeifenLuo.WinFormsUI.Docking;
+
+namespace Duality.Editor.Plugins.UndoHistoryView
+{
+    public class UndoHistoryViewPlugin : EditorPlugin
+    {        
+        UndoHistoryView _undoHistoryView;
+        private bool isLoading = false;
+
+        public override string Id
+        {
+            get { return "UndoHistoryView"; }
+        }
+
+        protected override IDockContent DeserializeDockContent(Type dockContentType)
+        {
+            this.isLoading = true;
+            IDockContent result;
+            if (dockContentType == typeof(UndoHistoryView))
+                result = this.RequestUndoHistoryView();
+            else
+                result = base.DeserializeDockContent(dockContentType);
+            this.isLoading = false;
+            return result;
+        }
+
+        protected override void InitPlugin(MainForm main)
+        {
+            base.InitPlugin(main);
+
+            // Request menu
+            MenuModelItem viewItem = main.MainMenu.RequestItem(GeneralRes.MenuName_View);
+            viewItem.AddItem(new MenuModelItem
+            {
+                Name = UndoHistoryViewRes.MenuItemName_UndoHistoryView,
+                Icon = UndoHistoryViewRes.IconUndoHistory,//.ToBitmap(),
+                ActionHandler = this.undoHistoryViewItemView_Click
+            });
+        }
+
+        private void undoHistoryViewItemView_Click(object sender, EventArgs e)
+        {            
+            this.RequestUndoHistoryView();
+        }
+
+        public UndoHistoryView RequestUndoHistoryView()
+        {
+            if (this._undoHistoryView == null || this._undoHistoryView.IsDisposed)
+            {
+                this._undoHistoryView = new UndoHistoryView();
+                this._undoHistoryView.FormClosed += delegate (object sender, FormClosedEventArgs e) { this._undoHistoryView = null; };
+            }
+
+            if (!this.isLoading)
+            {
+                this._undoHistoryView.Show(DualityEditorApp.MainForm.MainDockPanel);
+                if (this._undoHistoryView.Pane != null)
+                {
+                    this._undoHistoryView.Pane.Activate();
+                    this._undoHistoryView.Focus();
+                }
+            }
+
+            return this._undoHistoryView;
+        }
+    }
+}
