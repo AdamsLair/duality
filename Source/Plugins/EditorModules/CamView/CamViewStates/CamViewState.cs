3cd30b7c656a3239f4fc5c0313ecfd0bee7070ed
TheBoJaN <bojan4464@gmail.com>
2018-04-23 15:16:42 -0500

    Update CamViewState.cs
    Fixed issue with scrolling back from 0 RefDist

--- Source/Plugins/EditorModules/CamView/CamViewStates/CamViewState.cs
+++ Source/Plugins/EditorModules/CamView/CamViewStates/CamViewState.cs
@@ -812,7 +812,7 @@ namespace Duality.Editor.Plugins.CamView.CamViewStates
 				}
 				else
 				{
-					this.View.FocusDist += ((float)e.Delta / 1000f) * this.View.FocusDist;
+					this.View.FocusDist += ((float)e.Delta / 1000f) * Math.Max(this.View.FocusDist, 0.1f);
 				}
 			}
 
