67a62c6754cb24ea5742bb578cf736998a08bbb7
deanljohnson <dean@deanljohnson.com>
2018-05-13 16:25:23 -0700

    Tests now perform checks before and after clipboard use

--- Test/Editor/Utility/DataObjectExtensionsTest.cs
+++ Test/Editor/Utility/DataObjectExtensionsTest.cs
@@ -15,12 +15,19 @@ namespace Duality.Editor.Tests
 		{
 		}
 
-		[Test] public void GetGameObjectAfterClipboard()
+		[Test] public void GetGameObjects()
 		{
 			DataObject dataIn = new DataObject();
 			GameObject gameObject = new GameObject();
 
 			dataIn.SetGameObjects(new [] { gameObject });
+
+			Assert.IsTrue(dataIn.ContainsGameObjects(DataFormat.Reference));
+			Assert.IsTrue(dataIn.ContainsGameObjects(DataFormat.Value));
+
+			Assert.AreSame(gameObject, dataIn.GetGameObjects(DataFormat.Reference)[0]);
+			Assert.AreNotSame(gameObject, dataIn.GetGameObjects(DataFormat.Value)[0]);
+
 			Clipboard.SetDataObject(dataIn);
 
 			DataObject dataOut = (DataObject) Clipboard.GetDataObject();
@@ -32,12 +39,19 @@ namespace Duality.Editor.Tests
 			Assert.AreNotSame(gameObject, dataOut.GetGameObjects(DataFormat.Value)[0]);
 		}
 
-		[Test] public void GetComponentAfterClipboard()
+		[Test] public void GetComponents()
 		{
 			DataObject dataIn = new DataObject();
 			TestComponent comp = new TestComponent();
 
 			dataIn.SetComponents(new[] { comp });
+
+			Assert.IsTrue(dataIn.ContainsComponents(DataFormat.Reference));
+			Assert.IsTrue(dataIn.ContainsComponents(DataFormat.Value));
+
+			Assert.AreSame(comp, dataIn.GetComponents(DataFormat.Reference)[0]);
+			Assert.AreNotSame(comp, dataIn.GetComponents(DataFormat.Value)[0]);
+
 			Clipboard.SetDataObject(dataIn);
 
 			DataObject dataOut = (DataObject)Clipboard.GetDataObject();
@@ -49,12 +63,19 @@ namespace Duality.Editor.Tests
 			Assert.AreNotSame(comp, dataOut.GetComponents(DataFormat.Value)[0]);
 		}
 
-		[Test] public void GetComponentAfterClipboardStronglyTyped()
+		[Test] public void GetComponentStronglyTyped()
 		{
 			DataObject dataIn = new DataObject();
 			TestComponent comp = new TestComponent();
 
 			dataIn.SetComponents(new[] { comp });
+
+			Assert.IsTrue(dataIn.ContainsComponents<TestComponent>(DataFormat.Reference));
+			Assert.IsTrue(dataIn.ContainsComponents<TestComponent>(DataFormat.Value));
+
+			Assert.AreSame(comp, dataIn.GetComponents<TestComponent>(DataFormat.Reference)[0]);
+			Assert.AreNotSame(comp, dataIn.GetComponents<TestComponent>(DataFormat.Value)[0]);
+
 			Clipboard.SetDataObject(dataIn);
 
 			DataObject dataOut = (DataObject)Clipboard.GetDataObject();
