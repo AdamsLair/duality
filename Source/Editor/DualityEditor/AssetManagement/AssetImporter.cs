15f0c821fd2b1b2964aa696ae444f0ea080e2888
deanljohnson <dean@deanljohnson.com>
2018-05-17 15:24:35 -0700

    Documentation

--- Source/Editor/DualityEditor/AssetManagement/AssetImporter.cs
+++ Source/Editor/DualityEditor/AssetManagement/AssetImporter.cs
@@ -4,22 +4,96 @@ using System.Linq;
 
 namespace Duality.Editor.AssetManagement
 {
+	/// <summary>
+	/// A helper class that implements <see cref="IAssetImporter"/> and simplifies
+	/// the implementation of new importers that can only generate a single resource type.
+	/// </summary>
+	/// <typeparam name="T">The type of resource that this importer works with.</typeparam>
 	public abstract class AssetImporter<T> : IAssetImporter where T : Resource, new()
 	{
 		public const int PriorityGeneral = 0;
 		public const int PrioritySpecialized = 20;
 		public const int PriorityOverride = 50;
 
+		/// <summary>
+		/// [GET] The main file extension that this importer handles.
+		/// Used when determining the file extension of exported resources.
+		/// </summary>
 		protected abstract string SourceFileExtPrimary { get; }
+		/// <summary>
+		/// [GET] All file extensions that this importer handles.
+		/// Used when determining whether or not this importer
+		/// can handle a given input file.
+		/// </summary>
 		protected abstract string[] SourceFileExts { get; }
 
+		/// <summary>
+		/// [GET] A fixed system ID that represents this importer.
+		/// </summary>
 		public abstract string Id { get; }
+		/// <summary>
+		/// [GET] The user-friendly name of this importer.
+		/// </summary>
 		public abstract string Name { get; }
-
+		/// <summary>
+		/// [GET] The relative priority of this importer over others.
+		/// </summary>
 		public virtual int Priority
 		{
 			get { return PriorityGeneral; }
 		}
+
+		/// <summary>
+		/// Returns the name of the resource that this importer
+		/// would generate for the given input. Defaults to the name
+		/// of the input file, without extension.
+		/// </summary>
+		protected virtual string ResourceNameFromInput(AssetImportInput input)
+		{
+			return input.AssetName;
+		}
+		/// <summary>
+		/// Returns whether or not this importer accepts the given input.
+		/// By default, compares the input files extension to <see cref="SourceFileExts"/>.
+		/// </summary>
+		protected virtual bool AcceptsInput(AssetImportInput input)
+		{
+			string inputFileExt = Path.GetExtension(input.Path);
+			bool matchingFileExt = this.SourceFileExts.Any(acceptedExt =>
+				string.Equals(inputFileExt, acceptedExt, StringComparison.InvariantCultureIgnoreCase));
+			return matchingFileExt;
+		}
+		/// <summary>
+		/// Returns the of the file that would be exported for the given resource.
+		/// Defaults to the name of the resource with the extension <see cref="SourceFileExtPrimary"/>
+		/// </summary>
+		protected virtual string OutputNameFromResource(T resource)
+		{
+			return resource.Name + this.SourceFileExtPrimary;
+		}
+		/// <summary>
+		/// Returns whether or not the given resource can be exported.
+		/// By default, returns true for all non-null resources.
+		/// </summary>
+		protected virtual bool CanExport(T resource)
+		{
+			return resource != null;
+		}
+
+		/// <summary>
+		/// Performs the import operation for a resource.
+		/// </summary>
+		/// <param name="resourceRef">A <see cref="ContentRef{T}"/> pointing to the resource being imported.</param>
+		/// <param name="input">The input informatin for the import operation.</param>
+		/// <param name="env">The input environment in which the import is taking place.</param>
+		protected abstract void ImportResource(ContentRef<T> resourceRef, AssetImportInput input, IAssetImportEnvironment env);
+		/// <summary>
+		/// Performs the export operation for a resource.
+		/// </summary>
+		/// <param name="resourceRef">A <see cref="ContentRef{T}"/> pointing to the resource being imported.</param>
+		/// <param name="input">The input informatin for the import operation.</param>
+		/// <param name="env">The input environment in which the import is taking place.</param>
+		protected abstract void ExportResource(ContentRef<T> resourceRef, string path, IAssetExportEnvironment env);
 
 		void IAssetImporter.PrepareImport(IAssetImportEnvironment env)
 		{
@@ -30,7 +104,6 @@ namespace Duality.Editor.AssetManagement
 				env.AddOutput<T>(this.ResourceNameFromInput(input), input.Path);
 			}
 		}
-
 		void IAssetImporter.Import(IAssetImportEnvironment env)
 		{
 			// Handle all available input. No need to filter or ask for this anymore, as
@@ -51,7 +124,6 @@ namespace Duality.Editor.AssetManagement
 				}
 			}
 		}
-
 		void IAssetImporter.PrepareExport(IAssetExportEnvironment env)
 		{
 			T resource = env.Input as T;
@@ -60,35 +132,11 @@ namespace Duality.Editor.AssetManagement
 				env.AddOutputPath(this.OutputNameFromResource(resource));
 			}
 		}
-
 		void IAssetImporter.Export(IAssetExportEnvironment env)
 		{
 			T resource = env.Input as T;
 			string outputPath = env.AddOutputPath(this.OutputNameFromResource(resource));
 			this.ExportResource(resource, outputPath, env);
 		}
-
-		protected virtual string ResourceNameFromInput(AssetImportInput input)
-		{
-			return input.AssetName;
-		}
-		protected virtual bool AcceptsInput(AssetImportInput input)
-		{
-			string inputFileExt = Path.GetExtension(input.Path);
-			bool matchingFileExt = this.SourceFileExts.Any(acceptedExt =>
-				string.Equals(inputFileExt, acceptedExt, StringComparison.InvariantCultureIgnoreCase));
-			return matchingFileExt;
-		}
-		protected virtual string OutputNameFromResource(T resource)
-		{
-			return resource.Name + this.SourceFileExtPrimary;
-		}
-		protected virtual bool CanExport(T resource)
-		{
-			return true;
-		}
-
-		protected abstract void ImportResource(ContentRef<T> resourceRef, AssetImportInput input, IAssetImportEnvironment env);
-		protected abstract void ExportResource(ContentRef<T> resourceRef, string path, IAssetExportEnvironment env);
 	}
 }
