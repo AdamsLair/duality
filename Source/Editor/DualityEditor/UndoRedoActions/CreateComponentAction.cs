0d998705a977ab59cae1eda868c095dc78f10ba9
deanljohnson <dean@deanljohnson.com>
2018-05-19 15:26:39 -0700

    Previously resolved requirements can now resolve future requirements

--- Source/Editor/DualityEditor/UndoRedoActions/CreateComponentAction.cs
+++ Source/Editor/DualityEditor/UndoRedoActions/CreateComponentAction.cs
@@ -81,7 +81,9 @@ namespace Duality.Editor.UndoRedoActions
 
 						// Get additional requirements for the concrete type
 						IEnumerable<Type> additionalRequirements =
-							Component.RequireMap.GetRequirementsToCreate(this.targetParentObj, required);
+							Component.RequireMap.GetRequirementsToCreate(this.targetParentObj, required)
+								// Except types that are already covered by a previously identified requirement
+								.Where(newReq => !createRequirements.Any(newReq.IsAssignableFrom));
 						createRequirements.AddRange(additionalRequirements);
 					}
 
