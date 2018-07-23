dc7aa3f32de476382f6938e4e6b32c29f45b54a3
LAPTOP-NN5PFFMD\Work <yamailbox@gmail.com>
2018-07-23 21:14:57 +0200

    Added automatic update of CoroutineManager

--- Source/Core/Duality/DualityApp.cs
+++ Source/Core/Duality/DualityApp.cs
@@ -525,6 +525,7 @@ namespace Duality
 			VisualLogs.UpdateLogEntries();
 			pluginManager.InvokeBeforeUpdate();
 			UpdateUserInput();
+			CoroutineManager.Update();
 			Scene.Current.Update();
 			sound.Update();
 			pluginManager.InvokeAfterUpdate();
@@ -555,6 +556,7 @@ namespace Duality
 				pluginManager.InvokeBeforeUpdate();
 
 				UpdateUserInput();
+				CoroutineManager.Update();
 				Scene.Current.Update();
 
 				List<ICmpUpdatable> updatables = new List<ICmpUpdatable>();
