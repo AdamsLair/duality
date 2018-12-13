6a91c7f405c0f9ddb1172f3494faa5e207e99ece
deanljohnson <dean@deanljohnson.com>
2018-12-12 17:52:08 -0800

    Removed unused cache reflection value

--- Source/Core/Duality/Utility/ReflectionInfo.cs
+++ Source/Core/Duality/Utility/ReflectionInfo.cs
@@ -47,7 +47,6 @@ namespace Duality
 		public static readonly PropertyInfo	Property_Resource_AssetInfo;
 
 		public static readonly PropertyInfo	Property_Pixmap_Atlas;
-		public static readonly PropertyInfo	Property_RectAtlas_Items;
 
 		public static readonly PropertyInfo	Property_BatchInfo_Technique;
 		public static readonly PropertyInfo	Property_BatchInfo_MainColor;
@@ -98,8 +97,6 @@ namespace Duality
 
 			Type pixmap = typeof(Pixmap);
 			Property_Pixmap_Atlas			= GetProperty(pixmap, "Atlas");
-			Type rectAtlas = typeof(RectAtlas);
-			Property_RectAtlas_Items		= GetProperty(rectAtlas, "Items");
 
 			Type batchInfo = typeof(BatchInfo);
 			Property_BatchInfo_Technique	= GetProperty(batchInfo, "Technique");
