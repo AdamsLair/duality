993ccefc8857736ed9853b11f3d0c8a50f11c2b0
Rick van Dam <Barsonax@users.noreply.github.com>
2020-04-26 13:18:01 +0200

    #719 Fixed vertex locking test failing in debug mode (#768)
    * Added a utlity method to properly test if memory is released
    
    * added xml docs
    
    * moved and renamed garbagecollectiontestset
    
    * fix missing include

---
+++ Test/Core/Utility/GarbageCollectionUtils.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using NUnit.Framework;
+
+namespace Duality.Tests.Utility
+{
+	/// <summary>
+	/// Contains utilities to help test garbage collection related code.
+	/// </summary>
+	public class GarbageCollectionUtils
+	{
+		/// <summary>
+		/// Asserts if the <see cref="WeakReference"/> in <see cref="GarbageCollectionTestSet"/> are collected after a garbage collection.
+		/// </summary>
+		/// <param name="func"></param>
+		// No inlining is important here to defeat any optimizations the compiler can make that could cause instances to live longer.
+		[MethodImpl(MethodImplOptions.NoInlining)]
+		public static void CheckIfCleanedUp(Func<GarbageCollectionTestSet> func)
+		{
+			GarbageCollectionTestSet cleanupTestSet = func();
+
+			// In some cases unloading something happens async and can take some time such as with AssemblyLoadContext. Workaround this by retrying a couple of times..
+			for (int i = 0; cleanupTestSet.WeakReferences.Any(x => x.IsAlive) && i < 10; i++)
+			{
+				GC.Collect(2, GCCollectionMode.Forced, true);
+				GC.WaitForPendingFinalizers();
+			}
+			foreach (var item in cleanupTestSet.WeakReferences)
+			{
+				Assert.False(item.IsAlive);
+			}
+		}
+	}
+}
