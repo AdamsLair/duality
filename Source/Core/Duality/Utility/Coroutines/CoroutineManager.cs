72775e1a19cd4e36afc3a9036044343e18f27a0e
SirePi <yamailbox@gmail.com>
2020-05-10 18:42:15 +0200

    Removed exception tracking

--- Source/Core/Duality/Utility/Coroutines/CoroutineManager.cs
+++ Source/Core/Duality/Utility/Coroutines/CoroutineManager.cs
@@ -14,21 +14,12 @@ namespace Duality.Utility.Coroutines
 		private Queue<Coroutine> currentCycle = new Queue<Coroutine>(256);
 		private Queue<Coroutine> nextCycle = new Queue<Coroutine>(256);
 
-		private readonly Dictionary<Coroutine, Exception> lastFrameErrors = new Dictionary<Coroutine, Exception>();
-
 		/// <summary>
 		/// Returns an IEnumerable of all currently active and scheduled Coroutines
 		/// </summary>
 		public IEnumerable<Coroutine> Coroutines
 		{
 			get { return this.currentCycle.Concat(this.nextCycle); }
-		}
-		/// <summary>
-		/// Returns an IEnumerable of the Exception encountered in the last cycle, with the relative Coroutine
-		/// </summary>
-		public IReadOnlyDictionary<Coroutine, Exception> LastFrameErrors
-		{
-			get { return this.lastFrameErrors; }
 		}
 
 		/// <summary>
@@ -77,8 +68,6 @@ namespace Duality.Utility.Coroutines
 		/// </summary>
 		public void Update()
 		{
-			this.lastFrameErrors.Clear();
-
 			// swap around the queues
 			Queue<Coroutine> swap = this.currentCycle;
 			this.currentCycle = this.nextCycle;
@@ -89,9 +78,6 @@ namespace Duality.Utility.Coroutines
 			{
 				Coroutine c = this.currentCycle.Dequeue();
 				c.Update();
-
-				if (c.Status == CoroutineStatus.Error)
-					this.lastFrameErrors.Add(c, c.LastException);
 
 				if (c.Status == CoroutineStatus.Running || c.Status == CoroutineStatus.Paused)
 					this.nextCycle.Enqueue(c);
