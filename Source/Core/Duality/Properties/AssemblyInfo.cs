8b8bec00361ec23f5565859f21fda643d0bb7743
SirePi <yamailbox@gmail.com>
2020-05-16 20:03:30 +0200

    Package Update #CHANGE: Updated Package Specs to AdamsLair.Duality 3.2.2

--- Source/Core/Duality/Properties/AssemblyInfo.cs
+++ Source/Core/Duality/Properties/AssemblyInfo.cs
@@ -32,7 +32,7 @@ using System.Runtime.InteropServices;
 //
 // Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
 // übernehmen, indem Sie "*" eingeben:
-// [assembly: AssemblyVersion("3.2.1")]
-[assembly: AssemblyVersion("3.2.1")]
+// [assembly: AssemblyVersion("3.2.2")]
+[assembly: AssemblyVersion("3.2.2")]
 [assembly: InternalsVisibleTo("DualityEditor")]
 [assembly: InternalsVisibleTo("DualityTests")]
