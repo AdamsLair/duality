6eaccc10191f22bf349740519c05c33b9c4db217
Rick van Dam <Barsonax@users.noreply.github.com>
2020-05-22 13:00:33 +0200

    updated paths

--- Source/Editor/DualityEditor/Utility/EditorHelper.cs
+++ Source/Editor/DualityEditor/Utility/EditorHelper.cs
@@ -23,8 +23,8 @@ namespace Duality.Editor
 		public static readonly string BackupDirectory					= "Backup";
 		public static readonly string SourceDirectory					= "..";
 
-		public static readonly string AssetsDirectory				= Path.Combine(SourceDirectory, "assets");
-		public static readonly string SourceCodeDirectory				= Path.Combine(SourceDirectory, "src");
+		public static readonly string AssetsDirectory				= Path.Combine(SourceDirectory, "Import");
+		public static readonly string SourceCodeDirectory				= Path.Combine(SourceDirectory, "Source");
 
 		public static readonly string GlobalUserDirectory = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "Duality");
 
