c710a96501b1bc4d534969bc9e991381f06ee460
Rick van Dam <Barsonax@users.noreply.github.com>
2020-05-23 17:38:34 +0200

    added template project

---
+++ Source/DualityTemplates/Templates/ProjectTemplate/Source/Plugins/GamePlugin/CorePlugin.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Duality;
+
+namespace __Namespace__
+{
+	/// <summary>
+	/// Defines a Duality core plugin.
+	/// </summary>
+	public class __CorePluginClassName__ : CorePlugin
+	{
+		// Override methods here for global logic
+	}
+}
