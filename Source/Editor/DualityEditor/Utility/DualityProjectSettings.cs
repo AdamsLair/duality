c21bd930e64b9aa4d9a0b623f0b263da89eb900d
Rick van Dam <Barsonax@users.noreply.github.com>
2020-06-29 17:49:52 +0200

    cleanup

--- Source/Editor/DualityEditor/Utility/DualityProjectSettings.cs
+++ Source/Editor/DualityEditor/Utility/DualityProjectSettings.cs
@@ -1,26 +1,12 @@
-﻿using Duality.Serialization;
-
-namespace Duality.Editor
+﻿namespace Duality.Editor
 {
 	public class DualityProjectSettings
 	{
-		private static readonly string ProjectSettingsPath = "ProjectSettings.dat";
-
 		private string launcherPath = "DualityGame.exe";
 		public string LauncherPath
 		{
 			get { return this.launcherPath; }
 			set { this.launcherPath = value; }
-		}
-
-		public void Save()
-		{
-			Serializer.WriteObject(this, ProjectSettingsPath, typeof(XmlSerializer));
-		}
-
-		public static DualityProjectSettings Load()
-		{
-			return Serializer.TryReadObject<DualityProjectSettings>(ProjectSettingsPath) ?? new DualityProjectSettings();
 		}
 	}
 }
