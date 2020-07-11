40eaa61fca260362d6b1fda9486cb533ade3c2f9
Rick van Dam <Barsonax@users.noreply.github.com>
2020-07-11 20:22:59 +0200

    refactored projectview to the new userdata api

---
+++ Source/Plugins/EditorModules/ProjectView/ProjectViewSettings.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Duality.Editor.Plugins.ProjectView
+{
+	public class ProjectViewSettings
+	{
+		private string importSourcePath;
+
+		public string ImportSourcePath
+		{
+			get { return this.importSourcePath; }
+			set { this.importSourcePath = value; }
+		}
+	}
+}
