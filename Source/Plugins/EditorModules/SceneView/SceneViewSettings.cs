b506227b5962205c1060e5133d00a37cbe3d35dd
Rick van Dam <Barsonax@users.noreply.github.com>
2020-07-13 19:47:27 +0200

    update sceneViewsettings

---
+++ Source/Plugins/EditorModules/SceneView/SceneViewSettings.cs
@@ -0,0 +1,13 @@
+﻿namespace Duality.Editor.Plugins.SceneView
+{
+	public class SceneViewSettings
+	{
+		private bool showComponents = true;
+
+		public bool ShowComponents
+		{
+			get { return this.showComponents; }
+			set { this.showComponents = value; }
+		}
+	}
+}
