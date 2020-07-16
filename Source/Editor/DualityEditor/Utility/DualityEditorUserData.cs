87e1103dc8512364765443192463eee8a8e20bc4
Adam <adam@adamslair.net>
2020-07-16 21:17:27 +0200

    Reordered DualityEditorUserData and PluginSettings members to match style guide

--- Source/Editor/DualityEditor/Utility/DualityEditorUserData.cs
+++ Source/Editor/DualityEditor/Utility/DualityEditorUserData.cs
@@ -9,62 +9,56 @@ namespace Duality.Editor
 	public class DualityEditorUserData
 	{
 		private DesignTimeObjectDataManager perObjectData = new DesignTimeObjectDataManager();
+		private XElement dockPanelState = XElement.Parse(Properties.GeneralRes.DefaultDockPanelData);
+		private bool backups = false;
+		private AutosaveFrequency autoSaves = AutosaveFrequency.ThirtyMinutes;
+		private bool firstSession = false;
+		private int activeDocumentIndex = 0;
+		private bool startWithLastScene = false;
+		private ContentRef<Scene> lastOpenScene = null;
+		private PluginSettings pluginSettings = new PluginSettings();
+
+
 		internal DesignTimeObjectDataManager PerObjectData
 		{
 			get { return this.perObjectData; }
 			set { this.perObjectData = value; }
 		}
-
-		private XElement dockPanelState = XElement.Parse(Properties.GeneralRes.DefaultDockPanelData);
 		public XElement DockPanelState
 		{
 			get { return this.dockPanelState; }
 			set { this.dockPanelState = value; }
 		}
-
-		private bool backups = false;
 		public bool Backups
 		{
 			get { return this.backups; }
 			set { this.backups = value; }
 		}
-
-		private AutosaveFrequency autoSaves = AutosaveFrequency.ThirtyMinutes;
 		public AutosaveFrequency AutoSaves
 		{
 			get { return this.autoSaves; }
 			set { this.autoSaves = value; }
 		}
-
-		private bool firstSession = false;
 		public bool FirstSession
 		{
 			get { return this.firstSession; }
 			set { this.firstSession = value; }
 		}
-
-		private int activeDocumentIndex = 0;
 		public int ActiveDocumentIndex
 		{
 			get { return this.activeDocumentIndex; }
 			set { this.activeDocumentIndex = value; }
 		}
-
-		private bool startWithLastScene = false;
 		public bool StartWithLastScene
 		{
 			get { return this.startWithLastScene; }
 			set { this.startWithLastScene = value; }
 		}
-
-		private ContentRef<Scene> lastOpenScene = null;
 		public ContentRef<Scene> LastOpenScene
 		{
 			get { return this.lastOpenScene; }
 			set { this.lastOpenScene = value; }
 		}
-
-		private PluginSettings pluginSettings = new PluginSettings();
 		public PluginSettings PluginSettings
 		{
 			get { return this.pluginSettings; }
@@ -75,6 +69,7 @@ namespace Duality.Editor
 	public class PluginSettings
 	{
 		private XElement oldStyleSettings = XElement.Parse("<Plugins></Plugins>");
+		private List<object> plugins = new List<object>();
 
 		[Obsolete("Use the model based api")]
 		public XElement OldStyleSettings
@@ -83,16 +78,7 @@ namespace Duality.Editor
 			set { this.oldStyleSettings = value; }
 		}
 
-		private List<object> plugins = new List<object>();
-
-		internal void Clear()
-		{
-			this.plugins.Clear();
-			this.oldStyleSettings = XElement.Parse("<Plugins></Plugins>");
-		}
-
-		public T GetSettings<T>()
-		where T : class, new()
+		public T GetSettings<T>() where T : class, new()
 		{
 			T setting = this.plugins.OfType<T>().FirstOrDefault();
 			if (setting == null)
@@ -102,6 +88,11 @@ namespace Duality.Editor
 			}
 
 			return setting;
+		}
+		internal void Clear()
+		{
+			this.plugins.Clear();
+			this.oldStyleSettings = XElement.Parse("<Plugins></Plugins>");
 		}
 	}
 }
