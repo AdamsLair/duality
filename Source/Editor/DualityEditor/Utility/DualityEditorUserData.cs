7e9906bb37f1a2d5c75c067f01ae86e75a0db22d
Adam <adam@adamslair.net>
2020-07-16 21:34:00 +0200

    Added XML docs to DualityEditorUserData and PluginSettings

--- Source/Editor/DualityEditor/Utility/DualityEditorUserData.cs
+++ Source/Editor/DualityEditor/Utility/DualityEditorUserData.cs
@@ -6,6 +6,9 @@ using Duality.Resources;
 
 namespace Duality.Editor
 {
+	/// <summary>
+	/// Contains user settings for the Duality editor application, such UI layout or preferences.
+	/// </summary>
 	public class DualityEditorUserData
 	{
 		private DesignTimeObjectDataManager perObjectData = new DesignTimeObjectDataManager();
@@ -19,46 +22,73 @@ namespace Duality.Editor
 		private PluginSettings pluginSettings = new PluginSettings();
 
 
+		/// <summary>
+		/// Per-object user settings, such as global object visibility or lock states.
+		/// </summary>
 		internal DesignTimeObjectDataManager PerObjectData
 		{
 			get { return this.perObjectData; }
 			set { this.perObjectData = value; }
 		}
+		/// <summary>
+		/// A serialized XML blob representing the UI dock panel layout to use when starting the editor.
+		/// </summary>
 		public XElement DockPanelState
 		{
 			get { return this.dockPanelState; }
 			set { this.dockPanelState = value; }
 		}
+		/// <summary>
+		/// Specifies whether the editor will copy previous versions of saved resources to a local Backups folder. 
+		/// </summary>
 		public bool Backups
 		{
 			get { return this.backups; }
 			set { this.backups = value; }
 		}
+		/// <summary>
+		/// Specifies auto-save behavior in the editor, e.g. the frequency at which unsaved Resources are saved.
+		/// </summary>
 		public AutosaveFrequency AutoSaves
 		{
 			get { return this.autoSaves; }
 			set { this.autoSaves = value; }
 		}
+		/// <summary>
+		/// Whether this is the first time the editor has been started in this project.
+		/// </summary>
 		public bool FirstSession
 		{
 			get { return this.firstSession; }
 			set { this.firstSession = value; }
 		}
+		/// <summary>
+		/// The index of the dock panel main area's active document, to be applied when starting the editor.
+		/// </summary>
 		public int ActiveDocumentIndex
 		{
 			get { return this.activeDocumentIndex; }
 			set { this.activeDocumentIndex = value; }
 		}
+		/// <summary>
+		/// Whether the editor should open the <see cref="LastOpenScene"/> on startup.
+		/// </summary>
 		public bool StartWithLastScene
 		{
 			get { return this.startWithLastScene; }
 			set { this.startWithLastScene = value; }
 		}
+		/// <summary>
+		/// A reference to the most recently opened scene. Used in combination with <see cref="StartWithLastScene"/>.
+		/// </summary>
 		public ContentRef<Scene> LastOpenScene
 		{
 			get { return this.lastOpenScene; }
 			set { this.lastOpenScene = value; }
 		}
+		/// <summary>
+		/// Container object for each editor plugin's specific user settings.
+		/// </summary>
 		public PluginSettings PluginSettings
 		{
 			get { return this.pluginSettings; }
--- Source/Editor/DualityEditor/Utility/PluginSettings.cs
+++ Source/Editor/DualityEditor/Utility/PluginSettings.cs
@@ -6,11 +6,18 @@ using Duality.Resources;
 
 namespace Duality.Editor
 {
+	/// <summary>
+	/// Container class for <see cref="DualityEditorUserData"/> that is defined by editor plugins.
+	/// </summary>
 	public class PluginSettings
 	{
 		private XElement oldStyleSettings = XElement.Parse("<Plugins></Plugins>");
 		private List<object> plugins = new List<object>();
 
+		/// <summary>
+		/// Serialized XML blob that provides access to user data that is still serialized manually, as opposed to
+		/// the new way via Duality serializers.
+		/// </summary>
 		[Obsolete("Use the model based api")]
 		public XElement OldStyleSettings
 		{
@@ -18,6 +25,12 @@ namespace Duality.Editor
 			set { this.oldStyleSettings = value; }
 		}
 
+		/// <summary>
+		/// Returns existing editor plugin settings of type <typeparamref name="T"/>, or creates and registers a new 
+		/// instance if none existed yet.
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <returns></returns>
 		public T GetSettings<T>() where T : class, new()
 		{
 			T setting = this.plugins.OfType<T>().FirstOrDefault();
@@ -29,6 +42,9 @@ namespace Duality.Editor
 
 			return setting;
 		}
+		/// <summary>
+		/// Clears all editor plugin settings from this container, and resets it to an empty state.
+		/// </summary>
 		internal void Clear()
 		{
 			this.plugins.Clear();
