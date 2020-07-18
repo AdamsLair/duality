7f5f8feeea355846dc7efe33a5afb49e48705c96
Adam <adam@adamslair.net>
2020-07-18 15:54:46 +0200

    Tweaked SettingsContainer member ordering (style guide) and XML docs slightly

--- Source/Core/Duality/Utility/SettingsContainer.cs
+++ Source/Core/Duality/Utility/SettingsContainer.cs
@@ -7,23 +7,25 @@ namespace Duality
 	/// A container class to abstract all serialization logic away from settings classes.
 	/// </summary>
 	/// <typeparam name="TSettings"></typeparam>
-	public class SettingsContainer<TSettings> : ISettingsContainer
-		where TSettings : class, new()
+	public class SettingsContainer<TSettings> : ISettingsContainer where TSettings : class, new()
 	{
 		public string Path { get; }
+		/// <summary>
+		/// The settings data. This is null until <see cref="Load"/> is called for the first time, 
+		/// after which it can never be null again.
+		/// </summary>
+		public TSettings Instance { get; private set; }
+
 
 		/// <summary>
 		/// Fired when <see cref="Instance"/> has changed.
 		/// </summary>
 		public event EventHandler Changed;
 
-		/// <summary>
-		/// The settings data. This is null till <see cref="Load"/> is called after which it can never be null again.
-		/// </summary>
-		public TSettings Instance { get; private set; }
 
 		/// <summary>
-		/// Creates a new settings container where the data for the settings will be saved and loaded from <paramref name="path"/>.
+		/// Creates a new settings container where the data for the settings will be saved and loaded 
+		/// from <paramref name="path"/>.
 		/// </summary>
 		/// <param name="path"></param>
 		public SettingsContainer(string path)
@@ -39,7 +41,6 @@ namespace Duality
 			this.Instance = Serializer.TryReadObject<TSettings>(this.Path, typeof(XmlSerializer)) ?? new TSettings();
 			this.Changed?.Invoke(this, EventArgs.Empty);
 		}
-
 		/// <summary>
 		/// Saves the data of <typeparamref name="TSettings"/> to file.
 		/// </summary>
