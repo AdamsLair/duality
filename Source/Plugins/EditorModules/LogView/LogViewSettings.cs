986d71a86b4de4e328577a75b1622af9c265969a
Adam <adam@ilexp.net>
2020-07-22 12:35:46 +0200

    Reordered LogViewSettings members to match style guide

--- Source/Plugins/EditorModules/LogView/LogViewSettings.cs
+++ Source/Plugins/EditorModules/LogView/LogViewSettings.cs
@@ -3,55 +3,49 @@
 	public class LogViewSettings
 	{
 		private bool showMessages = true;
+		private bool showWarnings = true;
+		private bool showErrors = true;
+		private bool showCore = true;
+		private bool showEditor = true;
+		private bool showGame = true;
+		private bool autoClear = true;
+		private bool pauseOnError = true;
+
 		public bool ShowMessages
 		{
 			get { return this.showMessages; }
 			set { this.showMessages = value; }
 		}
-
-		private bool showWarnings = true;
 		public bool ShowWarnings
 		{
 			get { return this.showWarnings; }
 			set { this.showWarnings = value; }
 		}
-
-		private bool showErrors = true;
 		public bool ShowErrors
 		{
 			get { return this.showErrors; }
 			set { this.showErrors = value; }
 		}
-
-		private bool showCore = true;
 		public bool ShowCore
 		{
 			get { return this.showCore; }
 			set { this.showCore = value; }
 		}
-
-		private bool showEditor = true;
 		public bool ShowEditor
 		{
 			get { return this.showEditor; }
 			set { this.showEditor = value; }
 		}
-
-		private bool showGame = true;
 		public bool ShowGame
 		{
 			get { return this.showGame; }
 			set { this.showGame = value; }
 		}
-
-		private bool autoClear = true;
 		public bool AutoClear
 		{
 			get { return this.autoClear; }
 			set { this.autoClear = value; }
 		}
-
-		private bool pauseOnError = true;
 		public bool PauseOnError
 		{
 			get { return this.pauseOnError; }
