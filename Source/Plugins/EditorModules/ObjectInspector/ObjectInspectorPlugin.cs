b45a25c1ddd7e81c813ae01ffd3ac526a6cadd14
Rick van Dam <Barsonax@users.noreply.github.com>
2020-09-06 11:24:11 +0200

    Ported Object Inspector settings to the new settings serialization model (#871)
    Co-authored-by: Adam <adam@ilexp.net>

--- Source/Plugins/EditorModules/ObjectInspector/ObjectInspectorPlugin.cs
+++ Source/Plugins/EditorModules/ObjectInspector/ObjectInspectorPlugin.cs
@@ -1,15 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Windows.Forms;
-using System.IO;
-using System.Xml.Linq;
 
-using Duality;
-using Duality.Editor;
 using Duality.Editor.Forms;
 using Duality.Editor.Properties;
-using Duality.Editor.UndoRedoActions;
 using Duality.Editor.Plugins.ObjectInspector.Properties;
 
 using WeifenLuo.WinFormsUI.Docking;
@@ -21,15 +15,16 @@ namespace Duality.Editor.Plugins.ObjectInspector
 {
 	public class ObjectInspectorPlugin : EditorPlugin
 	{
-		private	static	ObjectInspectorPlugin	instance	= null;
+		private static ObjectInspectorPlugin instance = null;
 		internal static ObjectInspectorPlugin Instance
 		{
 			get { return instance; }
 		}
 
 
-		private	List<ObjectInspector>	objViews		= new List<ObjectInspector>();
-		private	bool					isLoading		= false;
+		private List<ObjectInspector>   objViews     = new List<ObjectInspector>();
+		private bool                    isLoading    = false;
+		private ObjectInspectorPluginSettings userSettings = new ObjectInspectorPluginSettings();
 
 
 		public override string Id
@@ -46,58 +41,12 @@ namespace Duality.Editor.Plugins.ObjectInspector
 		{
 			instance = this;
 		}
-		protected override IDockContent DeserializeDockContent(Type dockContentType)
-		{
-			this.isLoading = true;
-			IDockContent result;
-			if (dockContentType == typeof(ObjectInspector))
-				result = this.RequestObjView();
-			else
-				result = base.DeserializeDockContent(dockContentType);
-			this.isLoading = false;
-			return result;
-		}
-		protected override void SaveUserData(XElement node)
-		{
-			for (int i = 0; i < this.objViews.Count; i++)
-			{
-				XElement elem = new XElement("ObjectInspector");
-				elem.SetAttributeValue("id", i);
-				node.Add(elem);
-				this.objViews[i].SaveUserData(elem);
-			}
-		}
-		protected override void LoadUserData(XElement node)
-		{
-			this.isLoading = true;
-			foreach (XElement elem in node.Elements("ObjectInspector"))
-			{
-				int i = elem.GetAttributeValue("id", 0);
-				if (i < 0 || i >= this.objViews.Count) continue;
 
-				this.objViews[i].LoadUserData(elem);
-			}
-			this.isLoading = false;
-		}
-		protected override void InitPlugin(MainForm main)
-		{
-			base.InitPlugin(main);
-			
-			// Request menu
-			MenuModelItem viewItem = main.MainMenu.RequestItem(GeneralRes.MenuName_View);
-			viewItem.AddItem(new MenuModelItem
-			{
-				Name = ObjectInspectorRes.MenuItemName_ObjView,
-				Icon = ObjectInspectorResCache.IconObjView.ToBitmap(),
-				ActionHandler = this.menuItemObjView_Click
-			});
-		}
-		
 		public ObjectInspector RequestObjView(bool dontShow = false)
 		{
 			ObjectInspector objView = new ObjectInspector(this.objViews.Count);
 			this.objViews.Add(objView);
-			objView.FormClosed += delegate(object sender, FormClosedEventArgs e) { this.objViews.Remove(sender as ObjectInspector); };
+			objView.FormClosed += delegate (object sender, FormClosedEventArgs e) { this.objViews.Remove(sender as ObjectInspector); };
 
 			if (!this.isLoading && !dontShow)
 			{
@@ -109,6 +58,54 @@ namespace Duality.Editor.Plugins.ObjectInspector
 				}
 			}
 			return objView;
+		}
+
+		protected override IDockContent DeserializeDockContent(Type dockContentType)
+		{
+			this.isLoading = true;
+			IDockContent result;
+			if (dockContentType == typeof(ObjectInspector))
+				result = this.RequestObjView();
+			else
+				result = base.DeserializeDockContent(dockContentType);
+			this.isLoading = false;
+			return result;
+		}
+		protected override void InitPlugin(MainForm main)
+		{
+			base.InitPlugin(main);
+
+			// Request menu
+			MenuModelItem viewItem = main.MainMenu.RequestItem(GeneralRes.MenuName_View);
+			viewItem.AddItem(new MenuModelItem
+			{
+				Name = ObjectInspectorRes.MenuItemName_ObjView,
+				Icon = ObjectInspectorResCache.IconObjView.ToBitmap(),
+				ActionHandler = this.menuItemObjView_Click
+			});
+		}
+
+		protected override void SaveUserData(PluginSettings pluginSettings)
+		{
+			this.userSettings.InspectorSettingsById.Clear();
+			foreach (ObjectInspector inspector in this.objViews)
+			{
+				inspector.SaveGridExpandState();
+				this.userSettings.InspectorSettingsById[inspector.Id] = inspector.UserSettings;
+			}
+			pluginSettings.Set(this.userSettings);
+		}
+		protected override void LoadUserData(PluginSettings pluginSettings)
+		{
+			this.isLoading = true;
+			this.userSettings = pluginSettings.Get<ObjectInspectorPluginSettings>();
+			foreach (var pair in this.userSettings.InspectorSettingsById)
+			{
+				if (pair.Key < 0 || pair.Key >= this.objViews.Count) continue;
+				this.objViews[pair.Key].UserSettings = pair.Value;
+				this.objViews[pair.Key].ApplyUserSettings();
+			}
+			this.isLoading = false;
 		}
 
 		private void menuItemObjView_Click(object sender, EventArgs e)
